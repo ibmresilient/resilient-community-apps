6d91be56e2ba389d3b3a77149f04ed941b0f4857
Steven Bade <sbade@resilientsystems.com>
2016-03-29 13:30:11 -0400

    Initial drop of the python web form

---
+++ python/PythonWebForm/WebForm/__init__.py
@@ -0,0 +1,50 @@
+# Resilient Systems, Inc. ("Resilient") is willing to license software
+# or access to software to the company or entity that will be using or
+# accessing the software and documentation and that you represent as
+# an employee or authorized agent ("you" or "your") only on the condition
+# that you accept all of the terms of this license agreement.
+#
+# The software and documentation within Resilient's Development Kit are
+# copyrighted by and contain confidential information of Resilient. By
+# accessing and/or using this software and documentation, you agree that
+# while you may make derivative works of them, you:
+#
+# 1)  will not use the software and documentation or any derivative
+#     works for anything but your internal business purposes in
+#     conjunction your licensed used of Resilient's software, nor
+# 2)  provide or disclose the software and documentation or any
+#     derivative works to any third party.
+#
+# THIS SOFTWARE AND DOCUMENTATION IS PROVIDED "AS IS" AND ANY EXPRESS
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL RESILIENT BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from flask import Flask
+from flask.ext.login import LoginManager
+from flask_bootstrap import Bootstrap
+import os
+from flask.ext.sqlalchemy import SQLAlchemy
+
+import logging
+
+LOG = logging.getLogger(__name__)
+
+app = Flask(__name__)   # Initialize the Flask application
+app.config.from_object('config')  # load the configuration file config.py
+db = SQLAlchemy(app)  # Setup the authentication database
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+from .models import User  # bring in the local db module
+
+LOG.debug("Before view Import")
+from WebForm import views
