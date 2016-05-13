3d71fe62d1464305579b2484ec1ecec746f8878e
ztaira14 <ztaira14@punahou.edu>
2016-05-13 17:07:33 -0400

    Adjusted a few comments in the circuits framework.

--- resilientsystems.com/python/framework/framework.py
+++ resilientsystems.com/python/framework/framework.py
@@ -30,7 +30,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 
-"""Action Module example framework""" # CHANGE inside """
+"""Action Module example framework""" # CHANGE inside """. No impact on functionality.
 
 from __future__ import print_function
 import logging
@@ -39,14 +39,14 @@ from resilient_circuits.actions_component import ResilientComponent
 
 LOG = logging.getLogger(__name__)
 
-CONFIG_DATA_SECTION = 'framework_section' # CHANGE inside '
+CONFIG_DATA_SECTION = "framework_section" # CHANGE inside ". Must match config [framework_section].
 
 
-class FrameworkComponent(ResilientComponent): # CHANGE FrameworkComponent
-    """Example Circuits framework to increase prototyping speed""" # CHANGE inside """
+class FrameworkComponent(ResilientComponent): # CHANGE FrameworkComponent. No impact on functionality.
+    """Example Circuits framework to increase prototyping speed""" # CHANGE inside """. No impact on functionality.
 
     def __init__(self, opts):
-        super(FrameworkComponent, self).__init__(opts) # CHANGE FrameworkComponent
+        super(FrameworkComponent, self).__init__(opts) # CHANGE FrameworkComponent. Must match class FrameworkComponent.
         self.options = opts.get(CONFIG_DATA_SECTION, {})
         LOG.debug(self.options)
 
@@ -54,25 +54,23 @@ class FrameworkComponent(ResilientComponent): # CHANGE FrameworkComponent
         # or default to 'framework_default_queue'
         self.channel = "actions." + self.options.get("queue", "framework_default_queue") # CHANGE framework_default_queue
 
-    @handler("framework_action_title") # CHANGE inside "
+    @handler("framework_action_title") # CHANGE inside ". Must match "Framework Action Title" as defined in the README. 
     def _framework_function(self, event, *args, **kwargs): # CHANGE _framework_function
         """FRAMEWORK FUNCTION DOCSTRING""" # CHANGE inside """
         # CHANGE everything after the following line
         # ====================================================================
         # Note: the code isn't meant to be functional.
         # It's just to give an example.
-        incident = event.message['incident']
-        inc_id = incident['id']
+        incident = event.message["incident"]
+        inc_id = incident["id"]
 
         def framework_change_function(inc):
             """function to pass to the get_put method"""
-            inc[self.options.get['framework_field']] = self.options.get("framework_field2")
+            inc[self.options.get("framework_field")] = self.options.get("framework_field2")
             return inc
 
-        # Update the incident owner
         self.rest_client().get_put("/incidents/"+str(inc_id), framework_change_function)
 
-        status = "Finished executing framework code!" # CHANGE inside "
+        status = "Finished executing framework code!" 
         yield status
-
 
