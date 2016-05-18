37356b33b2145c5b78346e77cb2687587a2c5260
ztaira14 <ztaira14@punahou.edu>
2016-05-18 15:28:47 -0400

    Changed framework stuff to fit actual script

--- resilientsystems.com/python/task_tracker/task_tracker.py
+++ resilientsystems.com/python/task_tracker/task_tracker.py
@@ -30,7 +30,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 
-"""Action Module example framework""" # CHANGE inside """. No impact on functionality.
+"""Action Module task logger, implemented via circuits""" 
 
 from __future__ import print_function
 import logging
@@ -39,23 +39,23 @@ from resilient_circuits.actions_component import ResilientComponent
 
 LOG = logging.getLogger(__name__)
 
-CONFIG_DATA_SECTION = "framework_section" # CHANGE inside ". Must match config [framework_section].
+CONFIG_DATA_SECTION = "task_tracker" 
 
 
-class FrameworkComponent(ResilientComponent): # CHANGE FrameworkComponent. No impact on functionality.
-    """Example Circuits framework to increase prototyping speed""" # CHANGE inside """. No impact on functionality.
+class TaskTrackerComponent(ResilientComponent):
+    """Example Circuits script to implement a data-table based task tracker"""
 
     def __init__(self, opts):
-        super(FrameworkComponent, self).__init__(opts) # CHANGE FrameworkComponent. Must match class FrameworkComponent.
+        super(TaskTrackerComponent, self).__init__(opts) 
         self.options = opts.get(CONFIG_DATA_SECTION, {})
         LOG.debug(self.options)
 
         # The queue name can be specified in the config file,
         # or default to 'framework_default_queue'
-        self.channel = "actions." + self.options.get("queue", "framework_default_queue") # CHANGE framework_default_queue
+        self.channel = "actions." + self.options.get("queue", "tasktrackerqueue")
 
-    @handler("framework_action_title") # CHANGE inside ". Must match "Framework Action Title" as defined in the README. 
-    def _framework_function(self, event, *args, **kwargs): # CHANGE _framework_function
+    @handler("task_tracker")
+    def _task_tracker_function(self, event, *args, **kwargs):
         """FRAMEWORK FUNCTION DOCSTRING""" # CHANGE inside """
         # CHANGE everything after the following line
         # ====================================================================
@@ -76,6 +76,9 @@ class FrameworkComponent(ResilientComponent): # CHANGE FrameworkComponent. No im
                # '210':{'id': '210', 'value':inc_id},                                                                                                               │ 7
                # '211':{'id': '211', 'value':note}}}                                                                                                                │2 
         # self.co3_client.post('/incidents/'+str(inc_id)+'/table_data/1000/row_data', row)  
+
+        for thing in event:
+            print(thing)
 
         status = "Finished executing framework code!" 
         yield status
