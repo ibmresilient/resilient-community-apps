76c91eecd4aa4f61087c649a9e62c6fdb7c3bcaf
ztaira14 <ztaira14@punahou.edu>
2016-05-20 14:28:14 -0400

    Making it prettier for pep8.

--- resilientsystems.com/python/task_tracker/setup.py
+++ resilientsystems.com/python/task_tracker/setup.py
@@ -8,6 +8,7 @@ import logging
 
 LOG = logging.getLogger(__name__)
 
+
 def get_queue_id(res_client, queue_name):
     """Returns the id of the queue_name in the res_client instance"""
     message_dest_dict = res_client.get('/message_destinations')
@@ -16,6 +17,112 @@ def get_queue_id(res_client, queue_name):
             return message_dest['id']
     return -1
 
+
+def create_table(resilient_client):
+    # Create a new table
+    data_table = {"type_id": 8,
+                  "display_name": "Task History",
+                  "type_name": "task_history",
+                  "parent_types": ["incident"],
+                  "fields": {"task_name": {"hide_notification": False,
+                                           "values": [],
+                                           "text": "Task Name",
+                                           "required": None,
+                                           "chosen": False,
+                                           "blank_option": False,
+                                           "input_type": "text",
+                                           "name": "task_name",
+                                           "tooltip": "Task Name",
+                                           "placeholder": "task name",
+                                           "order": 0},
+                            "task_notes": {"hide_notification": False,
+                                           "values": [],
+                                           "text": "task_notes",
+                                           "required": None,
+                                           "chosen": False,
+                                           "blank_option": False,
+                                           "input_type": "text",
+                                           "name": "task_notes",
+                                           "tooltip": "Task Notes",
+                                           "placeholder": "task changed",
+                                           "order": 1},
+                            "init_date": {"hide_notification": False,
+                                          "values": [],
+                                          "text": "Init Date",
+                                          "required": None,
+                                          "chosen": True,
+                                          "blank_option": True,
+                                          "input_type": "datetimepicker",
+                                          "name": "init_date",
+                                          "tooltip": "Date the task was created",
+                                          "placeholder": "init date",
+                                          "order": 2},
+                            "closed_date": {"hide_notification": False,
+                                            "values": [],
+                                            "text": "Closed Date",
+                                            "required": None,
+                                            "chosen": True,
+                                            "blank_option": True,
+                                            "input_type": "datetimepicker",
+                                            "name": "closed_date",
+                                            "tooltip": "Date the task was closed",
+                                            "placeholder": "closed date",
+                                            "order": 3},
+                            "time_to_close": {"hide_notification": False,
+                                              "values": [],
+                                              "text": "Time to Close",
+                                              "required": None,
+                                              "chosen": False,
+                                              "blank_option": False,
+                                              "input_type": "text",
+                                              "name": "time_to_close",
+                                              "tooltip": "How long the task took to close",
+                                              "placeholder": "time to close",
+                                              "order": 4},
+                            "task_id":  {"hide_notification": False,
+                                         "values":  [],
+                                         "text": "Task ID",
+                                         "required": None,
+                                         "chosen": False,
+                                         "blank_option": True,
+                                         "input_type": "text",
+                                         "name": "task_id",
+                                         "tooltip": "The task ID",
+                                         "placeholder": "task ID",
+                                         "order": 5}}} 
+    resilient_client.post('/types', data_table)
+
+def create_message_destination(resilient_client, credentials):
+    # Create a new message destination with the name as defined in the config
+    LOG.info("Creating new message destination...")
+    users = resilient_client.get('/users')
+    queuereaderid = ""
+    for user in users:
+        if user['email'] == credentials['queue_reader_email']:
+            queuereaderid = user['id']
+            print(queuereaderid)
+    message_destination={"programmatic_name": credentials["queue_prog_name"],
+                         "name": credentials["queue_name"],
+                         "expect_ack": True,
+                         "destination_type": 0,
+                         "users": [queuereaderid]}
+    resilient_client.post("/message_destinations", message_destination)
+
+def create_automatic_action(resilient_client, credentials):
+    # Create automatic action that triggers whenever the task is changed from
+    # active to closed or vice-versa. This automatic action will be
+    # pointed at the message destination we just created earlier.
+    LOG.info("Creating new action...")
+    LOG.info("Pointing action at message destination...")
+    queue_id = get_queue_id(resilient_client, credentials["queue_name"])
+    automatic_action={"object_type": 1,
+                      "name": credentials['action_name'],
+                      "message_destinations": [queue_id],
+                      "conditions":[{"method": "changed",
+                                    "field_name": "task.status"
+                                    }]
+                      }
+    resilient_client.post("/actions", automatic_action)
 
 def setup():
     """Logs into resilient and posts necessary fields/actions/message queues"""
@@ -36,108 +143,13 @@ def setup():
                                               verify=verify_cert)
     resilient_client.connect(credentials["username"], credentials["password"])
 
-    # Create a new table
-    data_table = {"type_id":8,
-                  "display_name":"Task History",
-                  "type_name":"task_history",
-                  "parent_types":["incident"],
-                  "fields":{"task_name":{"hide_notification":False,
-                                         "values":[],
-                                         "text":"Task Name",
-                                         "required":None,
-                                         "chosen":False,
-                                         "blank_option":False,
-                                         "input_type":"text",
-                                         "name":"task_name",
-                                         "tooltip":"Task Name",
-                                         "placeholder":"task name",
-                                         "order":0},
-                            "task_notes":{"hide_notification":False,
-                                     "values":[],
-                                     "text":"task_notes",
-                                     "required":None,
-                                     "chosen":False,
-                                     "blank_option":False,
-                                     "input_type":"text",
-                                     "name":"task_notes",
-                                     "tooltip":"Task Notes",
-                                     "placeholder":"task changed",
-                                     "order":1},
-                            "init_date":{"hide_notification":False,
-                                         "values":[],
-                                         "text":"Init Date",
-                                         "required":None,
-                                         "chosen":True,
-                                         "blank_option":True,
-                                         "input_type":"datetimepicker",
-                                         "name":"init_date",
-                                         "tooltip":"Date the task was created",
-                                         "placeholder":"init date",
-                                         "order":2},
-                            "closed_date":{"hide_notification":False,
-                                           "values":[],
-                                           "text":"Closed Date",
-                                           "required":None,
-                                           "chosen":True,
-                                           "blank_option":True,
-                                           "input_type":"datetimepicker",
-                                           "name":"closed_date",
-                                           "tooltip":"Date the task was closed",
-                                           "placeholder":"closed date",
-                                           "order":3},
-                            "time_to_close":{"hide_notification":False,
-                                             "values":[],
-                                             "text":"Time to Close",
-                                             "required":None,
-                                             "chosen":False,
-                                             "blank_option":False,
-                                             "input_type":"text",
-                                             "name":"time_to_close",
-                                             "tooltip":"How long the task took to close",
-                                             "placeholder":"time to close",
-                                             "order":4},
-                            "task_id": {"hide_notification":False,
-                                        "values": [],
-                                        "text":"Task ID",
-                                        "required":None,
-                                        "chosen":False,
-                                        "blank_option":True,
-                                        "input_type":"text",
-                                        "name":"task_id",
-                                        "tooltip":"The task ID",
-                                        "placeholder":"task ID",
-                                        "order":5}}} 
-    resilient_client.post('/types', data_table)
+    # Create table
+    create_table(resilient_client)
+    # Create message destination
+    create_message_destination(resilient_client, credentials)
+    # Create automatic action with the specified user in the setup.config file
+    create_automatic_action(resilient_client, credentials)
 
-    # Create a new message destination with the name as defined in the config
-    LOG.info("Creating new message destination...")
-    users = resilient_client.get('/users')
-    queuereaderid = ""
-    for user in users:
-        if user['email'] == credentials['queue_reader_email']:
-            queuereaderid = user['id']
-            print(queuereaderid)
-    message_destination={"programmatic_name": credentials["queue_prog_name"],
-                         "name": credentials["queue_name"],
-                         "expect_ack": True,
-                         "destination_type": 0,
-                         "users": [queuereaderid]}
-    resilient_client.post("/message_destinations", message_destination)
-
-    # Create automatic action that triggers whenever the task is changed from
-    # active to closed or vice-versa. This automatic action will be
-    # pointed at the message destination we just created earlier.
-    LOG.info("Creating new action...")
-    LOG.info("Pointing action at message destination...")
-    queue_id = get_queue_id(resilient_client, credentials["queue_name"])
-    automatic_action={"object_type": 1,
-                      "name": credentials['action_name'],
-                      "message_destinations": [queue_id],
-                      "conditions":[{"method": "changed",
-                                    "field_name": "task.status"
-                                    }]
-                      }
-    resilient_client.post("/actions", automatic_action)
 
 if __name__ == "__main__":
     setup()
