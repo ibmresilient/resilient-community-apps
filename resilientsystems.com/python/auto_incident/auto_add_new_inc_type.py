965d4d90905bbd86d27da5a186b8ace7c4a16931
agamota <agamota@resilientsystems.com>
2016-08-17 14:05:19 -0400

    Debugged and Cleaned Code with Pylint
    Fixed the _update_inc function
    Miscellaneous code corrections (whitespace, line too long, etc)

--- resilientsystems.com/python/auto_incident/auto_add_new_inc_type.py
+++ resilientsystems.com/python/auto_incident/auto_add_new_inc_type.py
@@ -40,7 +40,7 @@ from resilient_circuits.actions_component import ResilientComponent
 
 LOG = logging.getLogger(__name__)
 
-CONFIG_DATA_SECTION = "auto_add_new_inc_type"
+CONFIG_DATA_SECTION = 'auto_add_new_inc_type'
 
 class NewIncComponent(ResilientComponent):
     """Circuits framework to add a new incident type via the entry wizard"""
@@ -51,33 +51,32 @@ class NewIncComponent(ResilientComponent):
         self.options = opts.get(CONFIG_DATA_SECTION, {})
         LOG.debug(self.options)
 
-        #The queue name, "add_new_object", is specified in the app.config.fragment file
-        self.channel = "actions." + self.options.get("queue", add_new_object)
+        #The queue name, 'add_new_object', is specified in the app.config.fragment file
+        self.channel = 'actions.' + self.options.get('queue', 'add_new_object')
 
-    @handler("add_new_incident_type")
-    def _add_new_inc_type(self, event, source=None, headers=None, message=None):
-        """Function to automatically add new incident types to Incident Types and the Incident via the entry wizard"""
-        incident = event.message["incident"]
-        inc_id = incident["id"]
+    @handler('add_new_incident_type')
+    def _add_new_inc_type(self, event):
+        """Function to automatically add new incident types to Incident Types and the Incident
+          via the entry wizard"""
+        incident = event.message['incident']
+        inc_id = incident['id']
         results = []
 
         # Saves the name of the new incident type
-        new_inc_type = incident["properties"]["enter_new_incident_type"]
+        new_inc_type = incident['properties']['enter_new_incident_type']
 
         # New incident type skeleton
-        obj = {"system": False, "parent_id": None, "create_date": None, "name": "Example", "hidden": False, "enabled": True, "id": None, "description": None}
-        
-        # Adds all the entered incident types to Resilient
+        obj = {'system': False, 'parent_id': None, 'create_date': None,
+               'name': 'Example', 'hidden': False, 'enabled': True, 'id': None, 'description': None}
+        # Adds the entered incident types to Resilient
         LOG.info('Updated incident types with %s type', new_inc_type)
-        obj["name"] = new_inc_type
-        results.append(self.rest_client().post("/incident_types", obj))
-
-        # Function to add an incident type to an incident
-        def update_inc(incident):
+        obj['name'] = new_inc_type
+        results.append(self.rest_client().post('/incident_types', obj))
+        def _update_inc(incident):
+            """Function to add an incident type to an incident"""
             LOG.info('Updated incident with %s type', new_inc_type)
-            incident["incident_type_ids"].append(new_inc_type)
-        return incident
-	
+            incident['incident_type_ids'].append(new_inc_type)
+            return incident
         # Updates the incident types with the types defined in the new entry wiz
-        self.rest_client().get_put("/incidents/{}".format(inc_id), update_inc, co3_context_token=event.context)
-
+        self.rest_client().get_put('/incidents/{}'.format(inc_id),
+                                   _update_inc, co3_context_token=event.context)
