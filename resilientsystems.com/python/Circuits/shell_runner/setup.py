bf5dc9d4c50c60ecf2d4963a14c7a678f5530453
Katherine Church <kchurch@Katherines-MacBook-Pro.local>
2017-03-30 18:27:55 -0400

    install license to correct directory

--- resilientsystems.com/python/Circuits/shell_runner/setup.py
+++ resilientsystems.com/python/Circuits/shell_runner/setup.py
@@ -2,8 +2,6 @@ from __future__ import print_function
 import os
 from setuptools import setup, find_packages
 
-sample_scripts = [u"shell_runner/scripts/" + filename for filename in os.listdir("shell_runner/scripts")]
-
 setup(
     name='rc-shell-runner',
     version="27.0.0",
@@ -18,9 +16,6 @@ setup(
     long_description = "Resilient Circuits Component for Shell Script Runner",
     packages=find_packages(),
     include_package_data=True,
-    data_files = [("shell_runner", ["shell_runner/LICENSE"]),
-                  ("shell_runner/scripts", sample_scripts),
-                  ("shell_runner/data", ["shell_runner/data/app.config"])],
     platforms='any',
     classifiers=[
         'Programming Language :: Python',
