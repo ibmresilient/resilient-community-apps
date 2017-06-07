435279db11ac9967571c6311cdb335c75caa6b6f
Kathy Church <kchurch@us.ibm.com>
2017-06-07 17:43:14 -0400

    Fix py2.7 bug in rc-webserver and fix broken test

--- packages/rc-webserver/tests/test_webserver.py
+++ packages/rc-webserver/tests/test_webserver.py
@@ -2,6 +2,29 @@
 from __future__ import print_function
 import pytest
 import requests
+from circuits.web import BaseController
+from rc_webserver.web import exposeWeb
+
+
+HTML = """
+<html>
+ <head>
+  <title>A Test</title>
+ </head>
+ <body>Test</body>
+</html>"""
+
+
+class WebTest(BaseController):
+    """ Test Web Component """
+
+    def __init__(self, opts):
+        super(WebTest, self).__init__(opts)
+        self.channel = "/test"
+
+    @exposeWeb("index")
+    def _index(self, status=""):
+        return HTML
 
 
 @pytest.mark.usefixtures("configure_resilient")
@@ -10,6 +33,7 @@ class TestWebserverTests:
 
     def test_server_up(self, circuits_app, new_incident):
         """ Verify Webserver is Up """
-        response = requests.get("http://localhost:9000")
+        WebTest({}).register(circuits_app.app.component_loader)
+        response = requests.get("http://localhost:9000/test")
         assert response.status_code == 200
-        assert response.text == "<HTML><BODY>Not Found</BODY></HTML>"
+        assert response.text == HTML
