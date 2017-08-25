21402788b3d6003356ecab4b53bcff1b39d52885
Hugh Pyle <hpyle@resilientsystems.com>
2017-08-25 10:05:59 -0400

    reorg

--- examples/README.md
+++
@@ -1,9 +0,0 @@
-# Community-driven examples using Resilient API
-
-This repository contains integrations, both simple and more advanced,
-that demonstrate the capabilities of the Resilient API.  They are
-intended to be used as examples, not as production-ready code.
-
-Contributions from the community are welcomed and encouraged.
-
-
---
+++ rc-query-runner/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+
+"""setup"""
+
+from __future__ import print_function
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    """test"""
+    user_options = [('pytestargs=', 'a', "Resilient Environment Arguments")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytestargs = []
+        self.test_suite = True
+
+    def finalize_options(self):
+        import shlex
+        TestCommand.finalize_options(self)
+        self.test_args = ["-s", ] + shlex.split(self.pytestargs)
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+setup(
+    name='rc-query-runner',
+    namespace_packages=['query_runner', 'query_runner.components', "query_runner.lib"],
+    use_scm_version={"root": "../", "relative_to": __file__},
+    setup_requires=['setuptools_scm'],
+    url='https://www.resilientsystems.com/',
+    license='Resilient License',
+    author='IBM Resilient',
+    install_requires=[
+        'resilient_circuits>=28.0.0'
+    ],
+    tests_require=["pytest",
+                   "pytest_resilient_circuits"],
+    extras_require={"qradar": ["rc-qradar-search"],
+                    "splunk": ["rc-splunk-search"],
+                    "ldap": ["rc-ldap-search"],
+                    "rest": ["rc-query-rest"]},
+    cmdclass={"test": PyTest},
+    author_email='support@resilientsystems.com',
+    description="Resilient Circuits base pkg for running queries and updating incidents from the results",
+    long_description="Resilient Circuits base pkg for running queries and updating incidents from the results",
+    packages=find_packages(),
+    include_package_data=True,
+    data_files=[("query_runner", ["query_runner/LICENSE"])],
+    platforms='any',
+    classifiers=[
+        'Programming Language :: Python',
+    ]
+)
