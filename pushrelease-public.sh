c3d5ee1c12cdb427c5e0f4eedf9e5377babacdef
Nacho Sarnaglia <ignacio.sarnaglia@ibm.com>
2017-12-15 13:22:07 -0300

    scripts for uploading releases to private and public github

---
+++ pushrelease-public.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+function getJsonVal() {
+   releaseID=`python -c "import json,sys;print json.dumps(json.loads('''$1''')$2); sys.exit(0);"` ; 
+   echo $releaseID
+}
+
+echo Getting Git Tag...
+latestTag=$(git tag --list | tail -n 1)
+
+echo Creating release...
+echo '{"tag_name": "'$latestTag'","target_commitish": "master","name": "'$latestTag'","body": "Resilient Python API '$latestTag'","draft": false,"prerelease": false}' > json.json
+output=$(curl -X POST -H 'Content-Type:application/json' -H 'Accept:application/json' --data-binary @json.json https://api.github.com/repos/ibmresilient/resilient-community-apps/releases?access_token=$GIT_HUB_AUTH_TOKEN)
+rm json.json
+
+id=$(getJsonVal "$output" "['id']")
+echo Release Id: $id
+for file in ./rc-*.tar.gz; do
+  echo Uploading file... ${file##*/}
+  fileUpload=$(curl --data-binary @./${file##*/} -H "Authorization: token $GIT_HUB_AUTH_TOKEN" -H "Content-Type: application/octet-stream" https://uploads.github.com/repos/ibmresilient/resilient-community-apps/releases/"$id"/assets?name=${file##*/} )
+  echo $fileUpload
+done
+
+
+
+
+
+
