3c1f3c14af2c28ac97c291bd45ea652e6e532446
Hugh Pyle <hpyle@us.ibm.com>
2018-01-08 17:32:58 -0500

    Make the execption class visible

--- rc-cts/rc_cts/__init__.py
+++ rc-cts/rc_cts/__init__.py
@@ -3,3 +3,4 @@ __import__('pkg_resources').declare_namespace(__name__)
 
 from rc_cts.lib.threat_models import *
 from rc_cts.components.threat_webservice import ThreatServiceLookupEvent
+from rc_cts.components.threat_webservice import ThreatLookupIncompleteException
