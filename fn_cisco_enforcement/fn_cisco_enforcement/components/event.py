90fce350515f7c4b4ef4d3f85bf6fec00c7e7583
Mark-Scherfling <mark.scherfling@ibm.com>
2018-04-18 21:44:01 -0400

    Updates fixes to event.py

--- fn_cisco_enforcement/fn_cisco_enforcement/components/event.py
+++ fn_cisco_enforcement/fn_cisco_enforcement/components/event.py
@@ -3,8 +3,9 @@
 # (c) Copyright IBM Corp. 2010, 2018. All Rights Reserved.
 """Function implementation"""
 import logging
-import requests
 import datetime
+import re
+import requests
 from resilient_circuits import ResilientComponent, function, handler, StatusMessage, FunctionResult, FunctionError
 try:
     from urlparse import urlparse
@@ -34,28 +35,25 @@ class FunctionComponent(ResilientComponent):
     def _event_function(self, event, *args, **kwargs):
         """Function: This is a function implementation that uses the Cisco API to post a Malware event"""
         try:
-            data=self.createdataobject(kwargs)
             apikey=self.options.get('api_token')
-
             url = '/'.join((self.options['url'], 'events?customerKey={}'))
             url = url.format(apikey)
             self.log.debug(url)
 
-            response=requests.post(url, json=data, verify=False, headers=HEADERS)
+            data=self.createdataobject(kwargs)
+
+            response = requests.post(url, json=data, verify=False, headers=HEADERS)
 
             if not response or response.status_code >= 300 or not response.content:
-                self.log.info(response.status_code)
-                yield FunctionError('api call failure: '+ str(response.status_code))
+                yield FunctionError('api call failure: {} on {}'.format(response.status_code, data['dstDomain']))
             else:
-                results = {
-                    "value": response.content.decode('latin1')
-                }
-                self.log.debug(results)
+                result = response.content.decode('latin1')
 
-                # Produce a FunctionResult with the results
-                yield FunctionResult(results)
-        except Exception:
-            yield FunctionError()
+            self.log.debug(result)
+            # Produce a FunctionResult with the results
+            yield FunctionResult({ "value": result})
+        except Exception as err:
+            yield FunctionError(err)
 
     # Creates the data object to send
     def createdataobject(self,kwargs):
@@ -72,16 +70,16 @@ class FunctionComponent(ResilientComponent):
 
 
         # Convert timestamps from miliseconds to seconds
-        cisco_deviceid = str(kwargs.get("cisco_deviceid"))  # text
-        cisco_deviceversion = str(kwargs.get("cisco_deviceversion"))  # text
+        cisco_deviceid = kwargs.get("cisco_deviceid")  # text
+        cisco_deviceversion = kwargs.get("cisco_deviceversion")  # text
 
         cisco_eventtime = datetime.datetime.utcfromtimestamp(kwargs.get("cisco_eventtime")/1000).strftime('%Y-%m-%dT%H:%M:%SZ')  # datetimepicker
         cisco_alerttime = datetime.datetime.utcfromtimestamp(kwargs.get("cisco_alerttime")/1000).strftime('%Y-%m-%dT%H:%M:%SZ')  # datetimepicker
 
-        cisco_dsturl, cisco_dstdomain = self._parseUrl(kwargs.get("cisco_dsturl"), kwargs.get("cisco_dstdomain"))
+        domain = kwargs.get("cisco_dstdomain")
+        cisco_dsturl, cisco_dstdomain = self._parseUrl(domain)     # split url and domain
 
         cisco_disabledstsafeguards = self.get_select_param(kwargs.get("cisco_disabledstsafeguards"))  # text
-
         cisco_dstip = kwargs.get("cisco_dstip")  # text
         cisco_eventseverity = kwargs.get("cisco_eventseverity")  # text
         cisco_eventtype = kwargs.get("cisco_eventtype")  # text
@@ -92,11 +90,12 @@ class FunctionComponent(ResilientComponent):
         cisco_externalurl = kwargs.get("cisco_externalurl")  # text
         cisco_src = kwargs.get("cisco_src")  # text
 
+        self.log.info("dstDomain: %s", cisco_dstdomain)
+        self.log.info("dstUrl: %s", cisco_dsturl)
         self.log.info("cisco_deviceid: %s", cisco_deviceid)
         self.log.info("cisco_deviceversion: %s", cisco_deviceversion)
         self.log.info("cisco_eventtime: %s", cisco_eventtime)
         self.log.info("cisco_alerttime: %s", cisco_alerttime)
-        self.log.info("cisco_dstdomain: %s", cisco_dstdomain)
         self.log.info("cisco_protocolversion: %s", cisco_protocolversion)
         self.log.info("cisco_providername: %s", cisco_providername)
         self.log.info("cisco_disabledstsafeguards: %s", cisco_disabledstsafeguards)
@@ -110,45 +109,27 @@ class FunctionComponent(ResilientComponent):
         self.log.info("cisco_externalurl: %s", cisco_externalurl)
         self.log.info("cisco_src: %s", cisco_src)
 
-        basicdata = {"alertTime": cisco_alerttime,
+        basicdata = {
+                "alertTime": cisco_alerttime,
                 "deviceId": cisco_deviceid,
                 "deviceVersion": cisco_deviceversion,
-                "dstDomain": cisco_dstdomain.strip(),
-                "dstUrl": cisco_dsturl.strip(),
                 "eventTime": cisco_eventtime,
                 "protocolVersion": cisco_protocolversion,
-                "providerName": cisco_providername}
+                "providerName": cisco_providername,
+                "dstDomain": cisco_dstdomain,
+                "dstUrl": cisco_dsturl
+                     }
 
         data=self.addothers(basicdata, cisco_dstip, cisco_eventseverity, cisco_eventtype, cisco_eventdescription, cisco_eventhash,
                   cisco_filename, cisco_filehash, cisco_externalurl, cisco_src)
 
         return data
 
-    def _parseUrl(self, url, domain):
-        if url and domain:
-            d = urlparse(domain)
+    def _parseUrl(self, url_domain):
+        """ split a url to also capture the domain """
+        d = urlparse(url_domain)
 
-            return url, self._getDomain(d.hostname if d.hostname else d.path)
-        elif not url and domain:
-            d = urlparse(domain)
-            hostname = d.hostname if d.hostname else d.path
-
-            return 'http://'+hostname, self._getDomain(hostname)
-        elif url and not domain:
-            u = urlparse(url)
-
-            return url, self._getDomain(u.hostname if u.hostname else u.path)
-
-        raise ValueError('either cisco_dsturl or cisco_dstdomain required')
-
-
-    def _getDomain(self, hostname):
-        """ return a domain from a url. ex: www.abc.com returns abc.com """
-        parts = hostname.split('.')
-        if len(parts) > 2:
-            return '.'.join((parts[len(parts)-2], parts[len(parts)-1]))
-        else:
-            return hostname
+        return d.geturl(), d.hostname if d.hostname else d.path
 
     # Adds the non mandatory parameters to the data object
     def addothers(self,data,cisco_dstip,cisco_eventseverity,cisco_eventtype,cisco_eventdescription,cisco_eventhash,cisco_filename,cisco_filehash,cisco_externalurl,cisco_src):
