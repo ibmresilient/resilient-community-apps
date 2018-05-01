feae6f11edd203d5fa87f256ca3bb7f459a0d337
Mark-Scherfling <mark.scherfling@ibm.com>
2018-05-01 16:54:25 -0400

    pull request changes

--- fn_cisco_enforcement/fn_cisco_enforcement/lib/resilient_common.py
+++ fn_cisco_enforcement/fn_cisco_enforcement/lib/resilient_common.py
@@ -13,12 +13,9 @@ def validateFields(fieldList, kwargs):
             raise ValueError('Required field is missing or empty: '+field)
 
 def readableDateTime(timestamp, milliseconds=True):
-    try:
-        if milliseconds:
-            ts = timestamp/1000
-        else:
-            ts = timestamp
+    if milliseconds:
+        ts = timestamp/1000
+    else:
+        ts = timestamp
 
-        return datetime.datetime.utcfromtimestamp(ts).strftime('%Y-%m-%dT%H:%M:%SZ')
-    except:
-        return ''
+    return datetime.datetime.utcfromtimestamp(ts).strftime('%Y-%m-%dT%H:%M:%SZ')
