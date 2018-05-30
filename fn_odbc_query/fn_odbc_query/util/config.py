b19f2161817444991af4bcf5b2878e88d9cbf670
Tamara Zlender <tamara.zlender@ibm.com>
2018-05-30 10:26:20 -0400

    INT-266 Updated README.md, setup.py and config.py files.

--- fn_odbc_query/fn_odbc_query/util/config.py
+++ fn_odbc_query/fn_odbc_query/util/config.py
@@ -11,8 +11,7 @@ def config_section_data():
     """
     config_data = u"""[fn_odbc_query]
 # Define your connection string
-#sql_connection_string=DRIVER={MariaDB ODBC 3.0 Driver};SERVER=127.0.0.1;PORT=3306;UID=root;PWD=password;Connection Timeout=60;
-sql_connection_string=Driver={PostgreSQL};Server=localhost;Port=5432;Database=postgres_test_database;Uid=postgres;Pwd=password;Timeout=60;
+sql_connection_string=Driver={PostgreSQL};Server=IP Adress;Port=5432; Database=myDataBase;Uid=myUserName;Pwd=myPassword;
 
 # Optional setting
 # Define a list of restricted SQL statements, separated by a comma.
--- fn_odbc_query/setup.py
+++ fn_odbc_query/setup.py
@@ -6,13 +6,14 @@ from setuptools import setup, find_packages
 setup(
     name='fn_odbc_query',
     version='1.0.0',
-    license='<<insert here>>',
-    author='<<your name here>>',
-    author_email='you@example.com',
+    license='MIT',
+    author='IBM Resilient',
+    author_email='support@resilientsystems.com',
     description="Resilient Circuits Components for 'fn_odbc_query'",
     long_description="Resilient Circuits Components for 'fn_odbc_query'",
     install_requires=[
-        'resilient_circuits>=30.0.0'
+        'resilient_circuits>=30.0.0',
+        'pyodbc'
     ],
     packages=find_packages(),
     include_package_data=True,
