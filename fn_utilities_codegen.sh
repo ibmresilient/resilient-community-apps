f7ad0c84de01ddacfaf76641247aad818387dd85
Ihor-Husar <ihor.husar@ibm.com>
2018-06-15 09:27:53 -0400

    Added Excel data to the codegen script

--- fn_utilities_codegen.sh
+++ fn_utilities_codegen.sh
@@ -11,4 +11,5 @@ resilient-circuits codegen -p fn_utilities -m fn_utilities \
     "Example: String to Attachment" \
     "Example: Zip Extract" \
     "Example: Zip List" \
-    "Example: Expand URL"
+    "Example: Expand URL" \
+    "Example: Use Excel Data"
