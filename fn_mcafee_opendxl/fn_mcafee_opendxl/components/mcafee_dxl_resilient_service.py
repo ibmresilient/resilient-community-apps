87c7f2c934bacf48158611ee89f5d39bca22f0ad
Brian Walsh <brianwal@Brians-MacBook-Pro.local>
2018-06-29 09:50:31 -0400

    Changed some logic to ResilientComponent subclass added more helper methods and a default template file

--- fn_mcafee_opendxl/fn_mcafee_opendxl/components/mcafee_dxl_resilient_service.py
+++ fn_mcafee_opendxl/fn_mcafee_opendxl/components/mcafee_dxl_resilient_service.py
@@ -3,13 +3,27 @@
 """Service listener implementation"""
 
 import logging
+import json
 from resilient_circuits import ResilientComponent
+from resilient_circuits.rest_helper import get_resilient_client
+from dxlclient.client import DxlClient
+from dxlclient import EventCallback
+from dxlclient.client_config import DxlClientConfig
 try:
-    from fn_mcafee_opendxl.util.helper import verify_config, event_subscriber
+    from fn_mcafee_opendxl.util.helper import verify_config, get_topic_template_dict, map_values, \
+        create_incident
 except ModuleNotFoundError:
-    from fn_mcafee_opendxl.fn_mcafee_opendxl.util.helper import verify_config, event_subscriber
+    from fn_mcafee_opendxl.fn_mcafee_opendxl.util.helper import verify_config, get_topic_template_dict, \
+        map_values, create_incident
 
 log = logging.getLogger(__name__)
+
+
+def get_connected_resilient_client(opts):
+    client = get_resilient_client(opts)
+    client.connect(opts.email, opts.password)
+
+    return client
 
 
 class DxlComponentListener(ResilientComponent):
@@ -17,13 +31,74 @@ class DxlComponentListener(ResilientComponent):
     def __init__(self, opts):
         super(DxlComponentListener, self).__init__(opts)
         self.config = verify_config(opts)
+
+        # Create and connect DXL client
+        config_client_file = self.config.get("config_client")
+        dxl_config = DxlClientConfig.create_dxl_config_from_file(config_client_file)
+        self.client = DxlClient(dxl_config)
+        self.client.connect()
+
         self.main()
 
     def main(self):
         if self.config["topic_listener_on"] == "True":
-            res_client = self.rest_client()
+#            res_client = self.rest_client()
             log.info("Service Listener called")
 
-            event_subscriber(res_client, self.config)
+            self.event_subscriber(self.config)
         else:
             log.info("Event subscriber not listening. To turn on set topic_listener_on to True")
+
+    def event_subscriber(self, config):
+#        config_client_file = config.get("config_client")
+#        dxl_config = DxlClientConfig.create_dxl_config_from_file(config_client_file)
+
+#        # Create the client
+#        with DxlClient(dxl_config) as client:
+
+#            # Connect to the fabric
+#            client.connect()
+
+            #
+            # Register the Event
+            #
+#            EVENT_TOPIC = config.get("topic_name")
+#            template = config.get("incident_template")
+        try:
+            mapping_dict = config.get("incident_mapping")
+
+            topic_template_dict = get_topic_template_dict(config.get("custom_template_dir"))
+
+            class ResilientEventSubscriber(EventCallback):
+
+                def __init__(self, template):
+                    super(ResilientEventSubscriber, self).__init__()
+                    self.temp = template
+
+                def on_event(self, event):
+                    message = event.payload.decode(encoding="UTF-8")
+                    log.info("Event received payload: " + message)
+
+                    message_dict = json.loads(message)
+
+                    # Map values from topic to incident template to create new incident
+#                    inc_temp = map_values_old(self.temp, mapping_dict, message)
+                    inc_temp = map_values(self.temp, message_dict)
+
+                    # Create new Incident in Resilient
+                    response = create_incident(get_connected_resilient_client(config), inc_temp)
+                    log.info("Created incident {}".format(str(response.get("id"))))
+
+            for event_topic, template in topic_template_dict.iteritems():
+                self.client.add_event_callback(event_topic, ResilientEventSubscriber(template))
+#                        client.add_event_callback(EVENT_TOPIC, ResilientEventSubscriber)
+                log.info("Resilient DXL Subscriber listening on {} ...".format(event_topic))
+#                        log.info("Listening on {}".format(EVENT_TOPIC))
+        except Exception as e:
+            log.error(e)
+            self.client.destroy()
+
+            # Wait forever
+#            while True:
+#                continue
+
--- fn_mcafee_opendxl/fn_mcafee_opendxl/util/helper.py
+++ fn_mcafee_opendxl/fn_mcafee_opendxl/util/helper.py
@@ -3,6 +3,8 @@
 
 import logging
 import json
+import os
+from os.path import join, pardir
 from dxlclient.client import DxlClient
 from dxlclient import EventCallback
 from dxlclient.client_config import DxlClientConfig
@@ -23,6 +25,7 @@ def verify_config(opts):
     topic_listener_on = config_opts.get("topic_listener_on")
     incident_template = config_opts.get("incident_template")
     incident_mapping = config_opts.get("incident_template_mapping")
+    custom_template_dir = config_opts.get("custom_template_dir")
 
     if config_client is None:
         log.error("dxlclient_config is not set. You must set this path to run this service")
@@ -49,13 +52,15 @@ def verify_config(opts):
         "topic_name": topic_name,
         "topic_listener_on": topic_listener_on,
         "incident_template": incident_template,
-        "incident_mapping": incident_mapping
+        "incident_mapping": incident_mapping,
+        "custom_template_dir": custom_template_dir,
+        "opts": opts
     }
 
     return config
 
 
-def _create_incident(resilient_client, payload):
+def create_incident(resilient_client, payload):
     uri = "/incidents"
     payload_dict = json.loads(payload)
     log.info("Creating incident with payload: {}".format(json.dumps(payload_dict)))
@@ -65,7 +70,7 @@ def _create_incident(resilient_client, payload):
     return response
 
 
-def _map_values(template_file, mapping_template_file, message):
+def map_values_old(template_file, mapping_template_file, message):
 
     with open(template_file, 'r') as template, open(mapping_template_file, 'r') as map_temp:
         mapping_template = Template(map_temp.read())
@@ -79,39 +84,140 @@ def _map_values(template_file, mapping_template_file, message):
         return incident_template
 
 
-def event_subscriber(res_client, config):
-    config_client_file = config.get("config_client")
-    dxl_config = DxlClientConfig.create_dxl_config_from_file(config_client_file)
+def map_values(template_file, message_dict):
 
-    # Create the client
-    with DxlClient(dxl_config) as client:
+    with open(template_file, 'r') as template:
 
-        # Connect to the fabric
-        client.connect()
+        incident_template = Template(template.read())
+        incident_template = incident_template.render(message_dict)
 
-        #
-        # Register the Event
-        #
-        EVENT_TOPIC = config.get("topic_name")
-        template = config.get("incident_template")
-        mapping_dict = config.get("incident_mapping")
+        return incident_template
 
-        class ResilientEventSubscriber(EventCallback):
-            def on_event(self, event):
-                message = event.payload.decode(encoding="UTF-8")
-                log.info("Event received payload: " + message)
 
-                # Map values from topic to incident template to create new incident
-                inc_temp = _map_values(template, mapping_dict, message)
+# Adds Incident Type to message dict
+def add_incident_type(message):
+    category = message.get("event").get("category")
+    log.debug("Category is {}".format(category))
 
-                # Create new Incident in Resilient
-                response = _create_incident(res_client, inc_temp)
-                log.info("Created incident {}".format(str(response.get("id"))))
+    incident_type_lookup = {
+        "Malware detected": "Malware"
+    }
+    incident_type = incident_type_lookup.get(category)
 
-        client.add_event_callback(EVENT_TOPIC, ResilientEventSubscriber)
+    if incident_type is not None:
+        message["incident_type"] = incident_type
 
-        log.info("Resilient DXL Listener running...")
+    return message
 
-        # Wait forever
-        while True:
-            continue
+
+def _merge_two_dicts(a, b):
+    c = a.copy()
+    c.update(b)  # modifies c with b's keys and values
+    return c
+
+
+def _get_topic_from_file_name(f):
+    file_no_extention = f.split('.')[0]
+    topic = file_no_extention.replace('_', '/')
+
+    return topic
+
+
+def _get_jinja_files(dir):
+    # return [f for f in os.listdir(dir) if f.endswith(".jinja2") or f.endswith(".jinja")]
+    dict = {}
+    for f in os.listdir(dir):
+        if f.endswith(".jinja2") or f.endswith(".jinja"):
+            dict[f] = join(dir, f)  # dictionary with key=filename and values=path to file
+    return dict
+
+
+def _get_template_files(overrides_dir=None):
+    # Get custom templates
+    custom_templates = []
+    if overrides_dir:
+        custom_templates = _get_jinja_files(overrides_dir)
+
+    # Get default templates
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    default_dir = join(current_path, pardir, "data/templates")
+    default_templates = _get_jinja_files(default_dir)
+
+    return _merge_two_dicts(default_templates, custom_templates)
+
+
+def get_topic_template_dict(overrides_dir=None):
+    template_files = _get_template_files(overrides_dir)
+
+    topic_template_dict = {}
+    for k, v in template_files.iteritems():
+        topic = _get_topic_from_file_name(k)
+        topic_template_dict[topic] = v
+
+    return topic_template_dict
+
+
+# def _get_values_from_event(message):
+#     message = json.loads(message) #ast.literal_eval(message)
+#     dict = {}
+#     dict["target_artifact"] = message.get("event").get("target")
+#     dict["source_artifact"] = message.get("event").get("source")
+#     dict["category"] = message.get("event").get("category")
+#     dict["event_desc"] = message.get("event").get("eventDesc")
+#     dict["threat_type"] = message.get("event").get("threatType")
+#     dict["threat_name"] = message.get("event").get("threatName")
+#
+#     # Verify only usable values are being added to the dict
+#     for k, v in dict.iteritems():
+#         if v == "null" or v is None or v == "":
+#             del dict[k]
+#
+#     return dict
+
+
+# def event_subscriber(res_client, config):
+#     config_client_file = config.get("config_client")
+#     dxl_config = DxlClientConfig.create_dxl_config_from_file(config_client_file)
+#
+#     # Create the client
+#     with DxlClient(dxl_config) as client:
+#
+#         # Connect to the fabric
+#         client.connect()
+#
+#         #
+#         # Register the Event
+#         #
+#         EVENT_TOPIC = config.get("topic_name")
+#         template = config.get("incident_template")
+#         mapping_dict = config.get("incident_mapping")
+#
+#         topic_template_dict = _get_topic_template_dict(config.get("custom_template_dir"))
+#
+#         class ResilientEventSubscriber(EventCallback):
+#
+#             def __init__(self, template):
+#                 super(ResilientEventSubscriber, self).__init__()
+#                 self.temp = template
+#
+#             def on_event(self, event):
+#                 message = event.payload.decode(encoding="UTF-8")
+#                 log.info("Event received payload: " + message)
+#
+#
+#                 # Map values from topic to incident template to create new incident
+#                 inc_temp = _map_values(template, mapping_dict, message)
+#
+#                 # Create new Incident in Resilient
+#                 response = _create_incident(res_client, inc_temp)
+#                 log.info("Created incident {}".format(str(response.get("id"))))
+#
+#         for event_topic, template in topic_template_dict.iteritems():
+#             client.add_event_callback(event_topic, ResilientEventSubscriber(template))
+# #        client.add_event_callback(EVENT_TOPIC, ResilientEventSubscriber)
+#             log.info("Resilient DXL Subscriber listening on {} ...".format(event_topic))
+# #            log.info("Listening on {}".format(EVENT_TOPIC))
+#
+#         # Wait forever
+#         while True:
+#             continue
