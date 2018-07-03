53b134aac7925c3ada0889b87514ca97c738ee93
Ryan Gordon <ryan.gordon1@ibm.com>
2018-07-03 16:06:06 +0100

    Added function logic from previous branch
    Also ran it through pylint to check code quality, landed a 8.14
    
    Still some logging statements to remove when DEVComplete

--- fn_utilities/fn_utilities/components/utilities_parse_ssl_cert.py
+++ fn_utilities/fn_utilities/components/utilities_parse_ssl_cert.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 # (c) Copyright IBM Corp. 2010, 2018. All Rights Reserved.
-# pragma pylint: disable=unused-argument, no-self-use
+# pragma pylint: disable=unused-argument, no-self-use, broad-except
 """Function implementation"""
 
 import logging
+import json
+import datetime
+import OpenSSL # Used for certificates
+from cryptography import x509 # Used for certificates
+from cryptography.hazmat.backends import default_backend
 from resilient_circuits import ResilientComponent, function, handler, StatusMessage, FunctionResult, FunctionError
 
 
@@ -32,14 +37,57 @@ Attempts to parse information from this certificate and save it as a note"""
             log.info("certificate: %s", certificate)
 
             # PUT YOUR FUNCTION IMPLEMENTATION CODE HERE
-            #  yield StatusMessage("starting...")
-            #  yield StatusMessage("done...")
+            yield StatusMessage("starting...")
 
-            results = {
-                "value": "xyz"
-            }
+            try:  # Try catch inside a try catch ?
+                yield StatusMessage("Attempting to parse the cert as JSON")
+                parsed_cert_json = json.loads(certificate)
+                # Load the cert into PyOpenSSL; 
+                parsed_cert_openssl = OpenSSL.crypto.load_certificate(
+                    OpenSSL.crypto.FILETYPE_PEM, parsed_cert_json)
+                # Load cert also with cryptography; this package has better date attributes
+                parsed_cert_crypto = x509.load_pem_x509_certificate(
+                    str(parsed_cert_json), default_backend())
 
-            # Produce a FunctionResult with the results
-            yield FunctionResult(results)
+                log.info(parsed_cert_crypto.serial_number)
+                log.info(parsed_cert_crypto.not_valid_before)
+                log.info(parsed_cert_crypto.not_valid_after)
+                log.info(parsed_cert_crypto.subject)
+
+                yield StatusMessage("Returning results")
+                # Loop over all available extensions and log them
+                # Most of this is handled by the get_issuer() function; 
+                for extension in range(parsed_cert_openssl.get_extension_count()):
+                    log.info(parsed_cert_openssl.get_extension(extension))
+
+                #  Prepare results for return; many fields need to be wrapped as strings or as JSON.
+                results = {
+
+                    "subject": json.dumps(parsed_cert_openssl.get_subject().get_components()),
+                    "notBefore": str(parsed_cert_crypto.not_valid_before),
+                    "notAfter": str(parsed_cert_crypto.not_valid_after),
+                    "issuer": json.dumps(parsed_cert_openssl.get_issuer().get_components()),
+                    "version": parsed_cert_openssl.get_version(),
+                    "expiration_status": ('Valid' if self._date_within_range(parsed_cert_crypto.not_valid_before, parsed_cert_crypto.not_valid_after, datetime.datetime.today()) is True else 'Expired'),
+                    "local_time": str(datetime.datetime.now())  # Local time is used to save when the function was run
+                }
+
+                yield StatusMessage("Function finish at local time "+str(datetime.datetime.today()))
+                # Produce a FunctionResult with the results
+                yield FunctionResult(results)
+            except ValueError:
+                yield StatusMessage('Problem loading the cert as JSON; Perhaps a file was provided?')
+
+                raise FunctionError('Problem loading the certificate as JSON')
+
+
         except Exception:
             yield FunctionError()
+
+    @staticmethod
+    def _date_within_range(lower_bound, upper_bound, the_date):
+
+        if lower_bound <= the_date <= upper_bound:
+            return True
+        else:
+            return False
