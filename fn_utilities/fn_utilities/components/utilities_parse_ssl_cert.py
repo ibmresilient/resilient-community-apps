b8912cd0066b5053dbc80ca22e728e6c7d2d98f1
Ryan Gordon <ryan.gordon1@ibm.com>
2018-07-06 14:25:02 +0100

    Parsing artifacts via the REST API
    Have both a text artifact and a  file working
    In the case of a file it uses the REST API to gather the file and then parses it for information

--- fn_utilities/fn_utilities/components/utilities_parse_ssl_cert.py
+++ fn_utilities/fn_utilities/components/utilities_parse_ssl_cert.py
@@ -82,7 +82,48 @@ class FunctionComponent(ResilientComponent):
                     }
                 except ValueError:
                     yield StatusMessage('Problem encountered loading the certificate as JSON;')
-                    
+                    '''
+                        2 Possible causes for reaching this ValueError
+                        -- Non valid JSON was provided; Maybe malformed cert
+                        -- A file was provided and the filename thew exception
+
+                        In the case of cause 2 try to load the file using the rest API
+
+                    '''
+                    artifact_data = self.get_binary_data_from_file(client, incident_id, artifact_id)
+
+
+                    # Create a temporary file to write the binary data to.
+                    with tempfile.NamedTemporaryFile('w+b', bufsize=0) as temp_file:
+                        # Write binary data to a temporary file.
+                        temp_file.write(artifact_data)
+                        st_cert=open(temp_file.name, 'rt').read()
+
+                        c=OpenSSL.crypto
+                        parsed_cert_openssl=c.load_certificate(c.FILETYPE_PEM, st_cert)
+
+                        # Load cert also with cryptography; this package has better date attributes
+                        parsed_cert_crypto = x509.load_pem_x509_certificate(
+                            st_cert, default_backend())
+
+                        yield("File parsed from rest API")
+                        log.info("Info gathered from cert files subject")
+                        log.info(json.dumps(parsed_cert_openssl.get_subject().get_components()))
+
+
+                        
+                        #  Prepare results for return; many fields need to be wrapped as strings or as JSON.
+                        results = {
+
+                            "subject": json.dumps(parsed_cert_openssl.get_subject().get_components()),
+                            "notBefore": str(parsed_cert_crypto.not_valid_before),
+                            "notAfter": str(parsed_cert_crypto.not_valid_after),
+                            "issuer": json.dumps(parsed_cert_openssl.get_issuer().get_components()),
+                            "version": parsed_cert_openssl.get_version(),
+                            "expiration_status": ('Valid' if self._date_within_range(parsed_cert_crypto.not_valid_before, parsed_cert_crypto.not_valid_after, datetime.datetime.today()) is True else 'Expired')
+                        }
+                
+                        yield FunctionResult(results)
                         
                 else:
                     # Produce a FunctionResult with the results if no exceptions happened
