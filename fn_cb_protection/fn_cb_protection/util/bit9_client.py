211d1343b09f99cd7744e179d317949ed4ba0d15
Hugh Pyle <hpyle@us.ibm.com>
2018-07-09 08:35:08 -0400

    Tidy

--- fn_cb_protection/fn_cb_protection/util/bit9_client.py
+++ fn_cb_protection/fn_cb_protection/util/bit9_client.py
@@ -9,38 +9,8 @@ import json
 URI_PATH = "api/bit9platform/v1"
 
 
-class SimpleHTTPException(Exception):
-    """Exception for HTTP errors."""
-    def __init__(self, response):
-        """
-        Args:
-          response - the Response object from the get/put/etc.
-        """
-        if isinstance(response, str):
-            text = response
-            status = 500
-        else:
-            text = response.text
-            status = response.status_code
-
-        logging.getLogger(__name__).error("SimpleHTTPException %s", status)
-        self.response = response
-
-
-def _raise_if_error(response):
-    """raise error for failed GET request"""
-    if response.status_code != 200:
-        raise SimpleHTTPException(response)
-
-
-def _raise_if_posterror(response):
-    """raise error for failed POST request"""
-    if response.status_code not in [200, 201]:
-        logging.getLogger(__name__).error(response.text)
-        raise SimpleHTTPException(response)
-
-
 def _is_true(flag):
+    """helper for the 'verify' flag"""
     return str(flag).lower()[:1] not in ["f", "n", "0"]
 
 
@@ -65,28 +35,34 @@ class CbProtectClient(object):
         }
 
     def get(self, specific_uri):
+        """generic get"""
         uri = "https://{}/{}/{}".format(self.server, URI_PATH, specific_uri)
         response = requests.get(uri, headers=self._headers(), verify=self.verify)
         response.raise_for_status()
         return response.json()
 
     def put(self, specific_uri, payload):
+        """generic update"""
         uri = "https://{}/{}/{}".format(self.server, URI_PATH, specific_uri)
         response = requests.put(uri, json.dumps(payload), headers=self._headers(), verify=self.verify)
         response.raise_for_status()
         return response.json()
 
     def post(self, specific_uri, payload):
+        """generic post"""
         uri = "https://{}/{}/{}".format(self.server, URI_PATH, specific_uri)
         response = requests.post(uri, json=payload, headers=self._headers(), verify=self.verify)
         response.raise_for_status()
         return response.json()
 
     def get_approval_request(self, id):
+        """get an approval request"""
         return self.get("approvalRequest/{}".format(id))
 
     def query_approval_request(self, query):
+        """get approval requests that match the query string"""
         return self.get("approvalRequest?q={}".format(query))
 
     def update_approval_request(self, id, payload):
+        """update an approval request"""
         return self.put("approvalRequest/{}".format(id), payload=payload)
