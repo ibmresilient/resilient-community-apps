8c05ccb4a766a1031b27ba89280bd986778143b5
John Prendergast <johnpren@ie.ibm.com>
2018-07-17 17:20:38 +0100

    INT-534 - Uses params dict for params.

--- fn_bigfix/fn_bigfix/components/fn_bigfix_artifact.py
+++ fn_bigfix/fn_bigfix/components/fn_bigfix_artifact.py
@@ -94,17 +94,17 @@ class FunctionComponent(ResilientComponent):
             yield StatusMessage("Running Query BigFix for Artifact ...")
             bigfix_client = BigFixClient(self.options)
 
-            if bigfix_incident_plan_status != 'C':
+            if params["incident_plan_status"] != 'C':
                 # If incident isn't closed
-                if bigfix_artifact_type == "IP Address":
+                if params["artifact_type"] == "IP Address":
                     artifact_data = bigfix_client.get_bf_computer_by_ip(bigfix_artifact_value)
-                elif bigfix_artifact_type == "File Path":
+                elif params["artifact_type"] == "File Path":
                     artifact_data = bigfix_client.get_bf_computer_by_file_path(bigfix_artifact_value)
-                elif bigfix_artifact_type == "Process Name":
+                elif params["artifact_type"] == "Process Name":
                     artifact_data = bigfix_client.get_bf_computer_by_process_name(bigfix_artifact_value)
-                elif bigfix_artifact_type == "Service":
+                elif params["artifact_type"] == "Service":
                     artifact_data = bigfix_client.get_bf_computer_by_service_name(bigfix_artifact_value)
-                elif bigfix_artifact_type == "Registry Key":
+                elif params["artifact_type"] == "Registry Key":
                     artifact_data = bigfix_client.get_bf_computer_by_registry_key_name_value(bigfix_artifact_value,
                                                                                            params["artifact_properties_name"],
                                                                                            params["artifact_properties_value"])
