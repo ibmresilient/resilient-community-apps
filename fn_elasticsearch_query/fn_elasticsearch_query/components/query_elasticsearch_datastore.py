5c55f2776891ea2bc86ea8187da984beea109839
Ryan Gordon <ryan.gordon1@ibm.com>
2018-07-20 10:53:18 +0100

    INT-575 -- Working with certs + check if results has 'hits'
    Have a config value called ELASTICSEARCH_CERT
    We check if this value is present when setting up ES and if it is then use it.
    Also when connecting we check if the ES result has a hits attribute

--- fn_elasticsearch_query/fn_elasticsearch_query/components/query_elasticsearch_datastore.py
+++ fn_elasticsearch_query/fn_elasticsearch_query/components/query_elasticsearch_datastore.py
@@ -7,6 +7,7 @@ import json
 from resilient_circuits import ResilientComponent, function, handler, StatusMessage, FunctionResult, FunctionError
 from fn_elasticsearch_query.util.helper import ElasticSearchHelper
 from elasticsearch import Elasticsearch
+from ssl import create_default_context
 
 class FunctionComponent(ResilientComponent):
     """Component that implements Resilient function 'query_elasticsearch_datastore"""
@@ -30,43 +31,60 @@ class FunctionComponent(ResilientComponent):
             helper = ElasticSearchHelper(self.options)
             # Get Elasticsearch params 
             ELASTICSEARCH_URL = helper.get_config_option("es_datastore_url")
+            ELASTICSEARCH_CERT = helper.get_config_option("es_cafile", True)
             # Get the function parameters:
             index = kwargs.get("index")  # text
             doc_type = kwargs.get("doc_type")  # text
             es_query = self.get_textarea_param(kwargs.get("es_query"))  # textarea
 
+            if not ELASTICSEARCH_CERT:
+                yield StatusMessage("No Cafile found in app.config. Attempting connection without")
             
             log = logging.getLogger(__name__)
             log.info("index: %s", index)
             log.info("doc_type: %s", doc_type)
             log.info("es_query: %s", es_query)
+
+            # TODO: Remove these logs before finishing
             log.info("es_datastore_url: %s", ELASTICSEARCH_URL)
+            log.info("es_cafile: %s", ELASTICSEARCH_CERT)
 
             if not es_query:
                 raise ValueError("An elasticsearch query is required")
 
             yield StatusMessage("Connecting to ElasticSearch...")
-            es = Elasticsearch([ELASTICSEARCH_URL])
+
+            '''
+
+            verify_certs is set to false so self-signed certs won't throw an error
+            cafile is the path to the cert; optional
+            '''
+            es = Elasticsearch([ELASTICSEARCH_URL], 
+                verify_certs=False,
+                cafile=ELASTICSEARCH_CERT)
+
+            # Optional, Yield the cluster name
             cluster_name = es.info()['cluster_name']
             yield StatusMessage("ElasticSearch cluster_name:"+cluster_name)
 
-            es_results = es.search(index=index, doc_type=doc_type,body=es_query)
-            log.info(es_results)
+            es_results = es.search(index=index, doc_type=doc_type,body=es_query, ignore=[400, 404])
+            #log.info(es_results)
+
             # If our results has a 'hits' attribute; inform the user
-            if es_results["hits"]:
+            if 'hits' in es_results:
                 yield StatusMessage("Call to elasticsearch was successful. Returning results")
             
                 
-            # PUT YOUR FUNCTION IMPLEMENTATION CODE HERE
-            #  yield StatusMessage("starting...")
-            #  yield StatusMessage("done...")
+            
             query_results = json.dumps(es_results["hits"]["hits"])
-            yield StatusMessage(query_results)
 
+            # TODO: Remove before finishing
+            yield StatusMessage(query_results)
+            # Prepare the results object
             results = {
                 "query_results": query_results
             }
-
+            yield StatusMessage("done...")
             # Produce a FunctionResult with the results
             yield FunctionResult(results)
         except Exception:
