d08f787dfb3a3e88647b4f8092cd6b95a43f67dc
Shane Curtin <shane.curtin@ie.ibm.com>
2018-07-24 16:58:28 +0100

    INT-509: Reuse test with diff inputs/outputs

--- fn_ldap_utilities/tests/test_ldap_utilities_toggle_access.py
+++ fn_ldap_utilities/tests/test_ldap_utilities_toggle_access.py
@@ -11,9 +11,7 @@ from mock import patch
 PACKAGE_NAME = "fn_ldap_utilities"
 FUNCTION_NAME = "ldap_utilities_toggle_access"
 
-# Read the default configuration-data section from the package
-# config_data = get_config_data(PACKAGE_NAME)
-
+#Set mock config_data
 config_data="""[fn_ldap_utilities]
 ldap_server=xxx.xxx.xxx.xxx
 ldap_port=389
@@ -49,47 +47,35 @@ class TestLdapUtilitiesToggleAccess:
         func = get_function_definition(PACKAGE_NAME, FUNCTION_NAME)
         assert func is not None
 
-    inputs = {
+    inputs1 = {
       "ldap_dn": "CN=Test User8,CN=Users,DC=dev,DC=co3sys,DC=com",
       "ldap_toggle_access": {"name": "Enable"}
     }
 
-    outputs = {
+    outputs1 = {
       "success": True,
       "user_dn": "CN=Test User8,CN=Users,DC=dev,DC=co3sys,DC=com",
       "user_status": "Enabled"
     }
-
-    @patch('fn_ldap_utilities.util.helper.Connection', helper.mocked_connection())
-    @patch('fn_ldap_utilities.util.helper.Server', helper.mocked_server())
-    @pytest.mark.parametrize("ldap_dn, ldap_toggle_access, expected_results", [
-        (inputs["ldap_dn"], inputs["ldap_toggle_access"], outputs)])
-    def test_enable(self, circuits_app, ldap_dn, ldap_toggle_access, expected_results):
-        """ Test enabling a user """
-        function_params = { 
-            "ldap_dn": ldap_dn,
-            "ldap_toggle_access": ldap_toggle_access
-        }
-        results = call_ldap_utilities_toggle_access_function(circuits_app, function_params)
-        assert(expected_results == results)
     
-    inputs = {
+    inputs2 = {
       "ldap_dn": "CN=Test User8,CN=Users,DC=dev,DC=co3sys,DC=com",
       "ldap_toggle_access": {"name": "Disable"}
     }
 
-    outputs = {
+    outputs2 = {
       "success": True,
       "user_dn": "CN=Test User8,CN=Users,DC=dev,DC=co3sys,DC=com",
       "user_status": "Disabled"
     }
-
+    
     @patch('fn_ldap_utilities.util.helper.Connection', helper.mocked_connection())
     @patch('fn_ldap_utilities.util.helper.Server', helper.mocked_server())
     @pytest.mark.parametrize("ldap_dn, ldap_toggle_access, expected_results", [
-        (inputs["ldap_dn"], inputs["ldap_toggle_access"], outputs)])
-    def test_disable(self, circuits_app, ldap_dn, ldap_toggle_access, expected_results):
-        """ Test calling with sample values for the parameters """
+        (inputs1["ldap_dn"], inputs1["ldap_toggle_access"], outputs1),
+        (inputs2["ldap_dn"], inputs2["ldap_toggle_access"], outputs2)])
+    def test_enable(self, circuits_app, ldap_dn, ldap_toggle_access, expected_results):
+        """ Test enabling/disabling a user """
         function_params = { 
             "ldap_dn": ldap_dn,
             "ldap_toggle_access": ldap_toggle_access
