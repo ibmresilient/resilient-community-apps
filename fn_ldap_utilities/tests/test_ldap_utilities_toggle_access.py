8ebdcd359dd5c41e51d5d700a70d48ac7ec60d4c
Shane Curtin <shane.curtin@ie.ibm.com>
2018-07-24 16:51:49 +0100

    INT-509: Add unit test for toggle_access fn

--- fn_ldap_utilities/tests/test_ldap_utilities_toggle_access.py
+++ fn_ldap_utilities/tests/test_ldap_utilities_toggle_access.py
@@ -5,12 +5,24 @@ from __future__ import print_function
 import pytest
 from resilient_circuits.util import get_config_data, get_function_definition
 from resilient_circuits import SubmitTestFunction, FunctionResult
+from helper import TestingHelper
+from mock import patch
 
 PACKAGE_NAME = "fn_ldap_utilities"
 FUNCTION_NAME = "ldap_utilities_toggle_access"
 
 # Read the default configuration-data section from the package
-config_data = get_config_data(PACKAGE_NAME)
+# config_data = get_config_data(PACKAGE_NAME)
+
+config_data="""[fn_ldap_utilities]
+ldap_server=xxx.xxx.xxx.xxx
+ldap_port=389
+ldap_use_ssl=False
+ldap_auth=SIMPLE
+ldap_user_dn=CN=Username,CN=Users,DC=example,DC=com
+ldap_password=password
+ldap_is_active_directory=True
+ldap_connect_timeout=10"""
 
 # Provide a simulation of the Resilient REST API (uncomment to connect to a real appliance)
 resilient_mock = "pytest_resilient_circuits.BasicResilientMock"
@@ -30,16 +42,53 @@ def call_ldap_utilities_toggle_access_function(circuits, function_params, timeou
 class TestLdapUtilitiesToggleAccess:
     """ Tests for the ldap_utilities_toggle_access function"""
 
+    helper = TestingHelper()
+
     def test_function_definition(self):
         """ Test that the package provides customization_data that defines the function """
         func = get_function_definition(PACKAGE_NAME, FUNCTION_NAME)
         assert func is not None
 
+    inputs = {
+      "ldap_dn": "CN=Test User8,CN=Users,DC=dev,DC=co3sys,DC=com",
+      "ldap_toggle_access": {"name": "Enable"}
+    }
+
+    outputs = {
+      "success": True,
+      "user_dn": "CN=Test User8,CN=Users,DC=dev,DC=co3sys,DC=com",
+      "user_status": "Enabled"
+    }
+
+    @patch('fn_ldap_utilities.util.helper.Connection', helper.mocked_connection())
+    @patch('fn_ldap_utilities.util.helper.Server', helper.mocked_server())
     @pytest.mark.parametrize("ldap_dn, ldap_toggle_access, expected_results", [
-        ("text", 'Disable', {"value": "xyz"}),
-        ("text", 'Disable', {"value": "xyz"})
-    ])
-    def test_success(self, circuits_app, ldap_dn, ldap_toggle_access, expected_results):
+        (inputs["ldap_dn"], inputs["ldap_toggle_access"], outputs)])
+    def test_enable(self, circuits_app, ldap_dn, ldap_toggle_access, expected_results):
+        """ Test enabling a user """
+        function_params = { 
+            "ldap_dn": ldap_dn,
+            "ldap_toggle_access": ldap_toggle_access
+        }
+        results = call_ldap_utilities_toggle_access_function(circuits_app, function_params)
+        assert(expected_results == results)
+    
+    inputs = {
+      "ldap_dn": "CN=Test User8,CN=Users,DC=dev,DC=co3sys,DC=com",
+      "ldap_toggle_access": {"name": "Disable"}
+    }
+
+    outputs = {
+      "success": True,
+      "user_dn": "CN=Test User8,CN=Users,DC=dev,DC=co3sys,DC=com",
+      "user_status": "Disabled"
+    }
+
+    @patch('fn_ldap_utilities.util.helper.Connection', helper.mocked_connection())
+    @patch('fn_ldap_utilities.util.helper.Server', helper.mocked_server())
+    @pytest.mark.parametrize("ldap_dn, ldap_toggle_access, expected_results", [
+        (inputs["ldap_dn"], inputs["ldap_toggle_access"], outputs)])
+    def test_disable(self, circuits_app, ldap_dn, ldap_toggle_access, expected_results):
         """ Test calling with sample values for the parameters """
         function_params = { 
             "ldap_dn": ldap_dn,
