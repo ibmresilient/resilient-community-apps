7bbbbab4ffbf90ee39768e6a6409c9bfc2a33d7e
Keenan Mach <keenan.mach@ibm.com>
2018-07-27 15:54:52 -0400

    updated config.py

--- fn_exchange/fn_exchange/util/config.py
+++ fn_exchange/fn_exchange/util/config.py
@@ -9,8 +9,12 @@ def config_section_data():
     """Produce the default configuration section for app.config,
        when called by `resilient-circuits config [-c|-u]`
     """
-#    config_data = u"""[fn_exchange]
-#setting=xxx
+    config_data = u"""[fn_exchange]
+cert_verify=[True|False]
+server=example.com
+username=domain\\username
+password=password
+default_folder_path=Some folder path after root i.e. Top of Information Store/Inbox
+default_timeozne=Some Microsoft timezone i.e. America/New_York
 #"""
-#    return config_data
-    return None
+    return config_data
