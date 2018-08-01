370b980ffaa065e6b57205a64d9fca3b26c9f51a
yongjian feng <yj@debian64Bit.debian64.irsys.com>
2018-08-01 10:43:17 -0400

    INT-611: mapping between observables and artifacts in the scripts

--- fn_qradar_advisor/scripts/full_search_datatable_script.py
+++ fn_qradar_advisor/scripts/full_search_datatable_script.py
@@ -3,13 +3,16 @@
 # Resilient default artifacts. If user has custom artifacts, and wants
 # to map them as well, please modify the following mapping dict. 
 #
-# All the other observables will be created as String type artifacts
+# All the other observables without direct mapping, try to make decision depending
+# on the qradar_advisor_description of them. If not decision can be made, then 
+# a String type artifact will be created.
 #
 mapping = {
   "domain-name":"DNS Name",
+  "domain":"DNS Name",
   "EmailContent":"Email Body",
   "ipv4-addr":"IP Address",
-  "malware":"Malware MD5 Hash",
+  "malware":"Malware Family/Variant",
   "url":"URL"
 }
 
@@ -18,19 +21,21 @@ type = row.qradar_advisor_type
 if type in mapping:
   incident.addArtifact(mapping[type], row.qradar_advisor_description, artifact_description)
 else:
+  artifact_type = "String"
   #
-  # if the type is "file", the description could be MD5 hash, SHA-256 hash. Distinguish them according to the length
+  # if the type is "file", the description could be MD5 hash, SHA-256 hash, SHA-1. 
+  # Distinguish them according to the lengthself.
   #
-  bcreated = False
+  # Anything else is considered "File Name"
+  #
   if type == "file":
     if len(row.qradar_advisor_description) == 32:
-      incident.addArtifact("Malware MD5 Hash", row.qradar_advisor_description, artifact_description)
-      bcreated = True
+      artifact_type = "Malware MD5 Hash"
     elif len(row.qradar_advisor_description) == 64:
-      incident.addArtifact("Malware SHA-256 Hash", row.qradar_advisor_description, artifact_description)
-      bcreated = True
-  else:
-    bcreated = False
-  # We don't know what type this observable should be mapped to. Just create a String type
-  if not bcreated:
-    incident.addArtifact("String", row.qradar_advisor_description, artifact_description)
+      artifact_type = "Malware SHA-256 Hash"
+    elif len(row.qradar_advisor_description) == 40:
+      artifact_type = "Malware SHA-1 Hash"
+    else:
+      artifact_type = "File Name"
+      
+  incident.addArtifact(artifact_type, row.qradar_advisor_description, artifact_description)
