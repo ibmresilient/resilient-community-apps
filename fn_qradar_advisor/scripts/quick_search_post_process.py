87aa76e01afa90a24a10bde0e20de3600fc8efca
yongjian feng <yj@debian64Bit.debian64.irsys.com>
2018-08-08 09:09:04 -0400

    Add the post process script for quick search here as well.

--- fn_qradar_advisor/scripts/quick_search_post_process.py
+++ fn_qradar_advisor/scripts/quick_search_post_process.py
@@ -5,8 +5,15 @@
 #     if the type of the observable can be mapped to an default artifact type.
 #
 return_search = results.search
+
+api_version = 2
+try:
+  if return_search.search_results["suspicious_observables"] is None:
+    api_version = 1
+except:
+  api_version = 1
 #
-# Sample return json dict
+# Sample return json dict for v2.0
 #
 '''{
       "search": {
@@ -70,11 +77,6 @@ return_search = results.search
       }
 '''
 #
-# Note that in this example workflow, we only extract the suspicious_observables
-#
-suspicious_observables = return_search.search_results.suspicious_observables
-
-#
 # We ONLY create artifacts for those observables that can be mapped to 
 # default Resilient artifacts. If customer has custom artifacts, and wants
 # to map them as well, please modify the following mapping dict. 
@@ -86,35 +88,87 @@ mapping = {
   "IpAddress":"IP Address",
   "Hash":"Malware MD5 Hash"
 }
-
-new_artifact_count = 0
-summary_string = "This artifact is not a suspicious observable"
-
-for observable in suspicious_observables:
-  #
-  # We support only those defined in mapping dict above
-  # 
-  if observable.type in mapping:
+#
+# Note that in this example workflow, we only extract the suspicious_observables
+#
+if api_version == 2:
+  #v2.0
+  suspicious_observables = return_search.search_results.suspicious_observables
+  
+  new_artifact_count = 0
+  summary_string = "This artifact is not a suspicious observable"
+  
+  for observable in suspicious_observables:
     #
-    # Note sometimes QRadar Advisor return the artifact itself as a suspicious observable. We don't want to
-    # duplicate here.
-    #
-    if mapping[observable.type] != artifact.type or observable.label != artifact.value:
-      new_artifact_count = new_artifact_count + 1
-      incident.addArtifact(mapping[observable.type], observable.label, "Watson Search result")  
-    else:
+    # We support only those defined in mapping dict above
+    # 
+    if observable.type in mapping:
       #
-      # The artifact itself is a suspicious observable. We don't create new (duplicated) artifact. But we show this info
+      # Note sometimes QRadar Advisor return the artifact itself as a suspicious observable. We don't want to
+      # duplicate here.
       #
-      summary_string = "This artifact is a suspicous observable"
-
-# Add a note about number of suspicious_observables
-note_string = "<h3>Watson Search Result Summary</h3><hr>"
-note_string = note_string + "<br><p><span style=\"font-weight:bold\">" + summary_string + "</span></p><br>"
-note_string = note_string + "<p>Search Value: " + return_search.search_value + "</p>"
-note_string = note_string + "<p>Search Type:  <span style=\"color:white; border-bottom-left-radius: 2.96667px;background-color:#808080\">&nbsp " + return_search.search_value_type + "&nbsp</span></p>"
-note_string = note_string + "<p style=\"color:#FF00FF;\">Suspicious observables: " + str(return_search.suspicious_count) + "</p>" 
-note_string = note_string + "<p style=\"color:red;\">New artifacts mapped from suspicious observables: " + str(new_artifact_count) + "</p>" 
-note_string = note_string + "<p>Other observables: " + str(return_search.other_count) + "</p>"
-html_note = helper.createRichText(note_string)
-incident.addNote(html_note)
+      if mapping[observable.type] != artifact.type or observable.label != artifact.value:
+        new_artifact_count = new_artifact_count + 1
+        incident.addArtifact(mapping[observable.type], observable.label, "Watson Search result")  
+      else:
+        #
+        # The artifact itself is a suspicious observable. We don't create new (duplicated) artifact. But we show this info
+        #
+        summary_string = "This artifact is a suspicous observable"
+  
+  # Add a note about number of suspicious_observables
+  note_string = "<h3>Watson Search Result Summary</h3><hr>"
+  note_string = note_string + "<br><p><span style=\"font-weight:bold\">" + summary_string + "</span></p><br>"
+  note_string = note_string + "<p>Search Value: " + return_search.search_value + "</p>"
+  note_string = note_string + "<p>Search Type:  <span style=\"color:white; border-bottom-left-radius: 2.96667px;background-color:#808080\">&nbsp " + return_search.search_value_type + "&nbsp</span></p>"
+  note_string = note_string + "<p style=\"color:#FF00FF;\">Suspicious observables: " + str(return_search.suspicious_count) + "</p>" 
+  note_string = note_string + "<p style=\"color:red;\">New artifacts mapped from suspicious observables: " + str(new_artifact_count) + "</p>" 
+  note_string = note_string + "<p>Other observables: " + str(return_search.other_count) + "</p>"
+  html_note = helper.createRichText(note_string)
+  incident.addNote(html_note)
+else:
+  #v1.0?
+  new_artifact_count = 0
+  toxic_count = 0
+  non_toxic_count = 0
+  summary_string = "This artifact is not a toxic observable"
+  for result in return_search.search_results:
+    value_type = result["type"]
+    if value_type in mapping:
+      #
+      # We know what artifact type corresponds to this value type
+      #
+      for val in result["values"]:
+        #
+        # We care about the toxic ones only
+        #
+        if val["is_toxic"]:
+          toxic_count = toxic_count + 1
+          #
+          # Note sometimes QRadar Advisor return the artifact itself as a suspicious observable. We don't want to
+          # duplicate here.
+          #
+          if mapping[value_type] != artifact.type or val.label != artifact.value:
+            new_artifact_count = new_artifact_count + 1
+            incident.addArtifact(mapping[value_type], val.label, "Watson Search result")  
+          else:
+            #
+            # The artifact itself is a suspicious observable. We don't create new (duplicated) artifact. But we show this info
+            #
+            summary_string = "This artifact is a toxic observable"
+        else:
+          non_toxic_count = non_toxic_count + 1
+      
+  # Add a note about number of suspicious_observables
+  note_string = "<h3>Watson Search Result Summary</h3><hr>"
+  note_string = note_string + "<br><p><span style=\"font-weight:bold\">" + summary_string + "</span></p><br>"
+  note_string = note_string + "<p>Search Value: " + return_search.search_value + "</p>"
+  note_string = note_string + "<p>Search Type:  <span style=\"color:white; border-bottom-left-radius: 2.96667px;background-color:#808080\">&nbsp " + return_search.search_value_type + "&nbsp</span></p>"
+  note_string = note_string + "<p style=\"color:#FF00FF;\">Toxic observables: " + str(toxic_count) + "</p>" 
+  note_string = note_string + "<p style=\"color:red;\">New artifacts mapped from toxic observables: " + str(new_artifact_count) + "</p>" 
+  note_string = note_string + "<p>Non toxic observables: " + str(non_toxic_count) + "</p>"
+  html_note = helper.createRichText(note_string)
+  incident.addNote(html_note)    
+   
+        
+      
