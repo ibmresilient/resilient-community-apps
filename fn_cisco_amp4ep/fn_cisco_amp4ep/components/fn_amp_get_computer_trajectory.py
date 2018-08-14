76ad03445d588626846b999e89fef95de384efb5
John Prendergast <johnpren@ie.ibm.com>
2018-08-14 10:21:06 +0100

    INT-565 - Code review - Added copywrite IBM notice.

--- fn_cisco_amp4ep/fn_cisco_amp4ep/components/fn_amp_get_computer_trajectory.py
+++ fn_cisco_amp4ep/fn_cisco_amp4ep/components/fn_amp_get_computer_trajectory.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 # pragma pylint: disable=unused-argument, no-self-use
-"""Function implementation"""
 
+# (c) Copyright IBM Corp. 2010, 2018. All Rights Reserved.
+
+""" Resilient functions component to run a Cisco AMP for endpoints query - get computer trajectory """
+
+# Set up:
+# Destination: a Queue named "amp_computers".
+# Manual Action: Execute a REST query against a Cisco AMP for endpoints server.
 import logging
 import json
 from datetime import datetime
