488c6da60beb31abf5beda82812ea51524798b61
Tamara Zlender <tamara.zlender@ibm.com>
2018-08-24 13:08:02 -0400

    INT-629 - adding more info about tags to the app.config file

--- rc-cts-passivetotal/rc_cts_passivetotal/util/config.py
+++ rc-cts-passivetotal/rc_cts_passivetotal/util/config.py
@@ -25,6 +25,8 @@ passivetotal_enrich_subdom_api_url=/v2/enrichment/subdomains
 passivetotal_community_url=https://community.riskiq.com/search/
 
 # Define the tags you classify as "hits", separated by a comma
-passivetotal_tags=compromised,ransomeware
+# Global tasks defined by RiskIQ Passive total include
+# apt32, oceanlotus, compromised, espionage, bad rabbit, ransomware and more
+passivetotal_tags=compromised,ransomware
     """
     return config_data
