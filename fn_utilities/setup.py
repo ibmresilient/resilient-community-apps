b6a2aea16193ff398abc62bf849ec0af6267567b
Keenan Mach <keenan.mach@ibm.com>
2018-09-12 16:48:17 -0400

    Revert "update utilities to master"
    This reverts commit b3a9f0d11516589026eab8a017b8ed4b9e310012.

--- fn_utilities/setup.py
+++ fn_utilities/setup.py
@@ -13,12 +13,7 @@ setup(
     long_description="Resilient Circuits Utility Functions",
     install_requires=[
         'resilient_circuits>=30.0.0',
-        'openpyxl>=2.5.3',
-        'pyOpenSSL>=18.0.0',
-        'cryptography>=2.2.2',
-        'weasyprint',
-        'json2html',
-        'lxml'
+        'openpyxl>=2.5.3'
     ],
     packages=find_packages(),
     include_package_data=True,
@@ -42,13 +37,7 @@ setup(
             "ResilientSearchFunctionComponent = fn_utilities.components.utilities_resilient_search:FunctionComponent",
             "ShellCommandFunctionComponent = fn_utilities.components.utilities_shell_command:FunctionComponent",
             "StringToAttachmentFunctionComponent = fn_utilities.components.utilities_string_to_attachment:FunctionComponent",
-            "ExpandURLFunctionComponent = fn_utilities.components.utilities_expand_url:FunctionComponent",
-            "UtilitiesExtractSslCertFromUrlFunctionComponent = fn_utilities.components.utilities_extract_ssl_cert_from_url:FunctionComponent",
-            "UtilitiesParseSslCertificateFunctionComponent = fn_utilities.components.utilities_parse_ssl_certificate:FunctionComponent",
-            "Html2PdfFunctionComponent = fn_utilities.components.utilities_html2pdf:FunctionComponent",
-            "Json2HtmlFunctionComponent = fn_utilities.components.utilities_json2html:FunctionComponent",
-            "GetContactInfoFunctionComponent = fn_utilities.components.utilities_get_contact_info:FunctionComponent",
-            "XMLTransformationFunctionComponent = fn_utilities.components.utilities_xml_transformation:FunctionComponent"
+            "ExpandURLFunctionComponent = fn_utilities.components.utilities_expand_url:FunctionComponent"
         ],
         "resilient.circuits.configsection": ["gen_config = fn_utilities.util.config:config_section_data"],
         "resilient.circuits.customize": ["customize = fn_utilities.util.customize:customization_data"]
