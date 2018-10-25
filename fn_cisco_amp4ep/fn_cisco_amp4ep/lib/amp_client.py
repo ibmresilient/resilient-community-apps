a631ddf5eca6c8691b5ef9ea9c7d1d8d7d825b38
John Prendergast <johnpren@ie.ibm.com>
2018-10-25 12:22:26 +0100

    INT-561 INT-563 - Added support for more good return codes also raise error for unexpected return code.

--- fn_cisco_amp4ep/fn_cisco_amp4ep/lib/amp_client.py
+++ fn_cisco_amp4ep/fn_cisco_amp4ep/lib/amp_client.py
@@ -114,8 +114,11 @@ class Ampclient(object):
                 else:
                     raise e
 
-            if r.status_code == 200:
+            if r.status_code in range(200, 202):
                 break
+            else:
+                LOG.error("Unexpected response '%s' received." % r.status_code)
+                raise HTTPError("Unexpected response '{}' received.").format(r.status_code)
 
             retry_attempts += 1
             if (retry_attempts > self.max_retries):
