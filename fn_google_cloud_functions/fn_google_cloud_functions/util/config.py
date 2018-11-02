a7a00fc35cdd1904a9b874e532f362e3c271eae4
Ryan Gordon <ryan.gordon1@ibm.com>
2018-11-02 11:54:40 +0000

    INT-643: Added config to config.py

--- fn_google_cloud_functions/fn_google_cloud_functions/util/config.py
+++ fn_google_cloud_functions/fn_google_cloud_functions/util/config.py
@@ -10,8 +10,12 @@ def config_section_data():
     """Produce the default configuration section for app.config,
        when called by `resilient-circuits config [-c|-u]`
     """
-#    config_data = u"""[fn_google_cloud_functions]
-#setting=xxx
-#"""
-#    return config_data
-    return None
+    config_data = u"""[fn_google_cloud_functions]
+gcp_region = <GCP_REGION_ID>
+gcp_project_id = <GCP_PROJECT_ID>
+gcp_function_name = <NAME_OF_CLOUD_FUNCTION>
+# Optional Config values
+gcp_http_proxy = None
+gcp_https_proxy = None
+"""
+    return config_data
