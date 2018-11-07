a0b98af0f2a6249c24dd05f18936b7e42aae0757
Mark R. Scherfling <Mark.Scherfling@ibm.com>
2018-11-07 14:11:53 -0500

    Update setup.py
    changes made to fn_utilities to ensure rules do not also reference message destinations.

--- fn_utilities/setup.py
+++ fn_utilities/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fn_utilities',
-    version='1.0.4',
+    version='1.0.5',
     license='MIT',
     author='IBM Resilient',
     author_email='support@resilientsystems.com',
