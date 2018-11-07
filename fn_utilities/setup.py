04a8b781f6038b7e7e4a39147f88181a82db1051
RYAN GORDON <Ryan.Gordon1@ibm.com>
2018-11-07 20:11:55 +0000

    Updated customize py for fn_utilities (#216)
    * Updated customize py for fn_utilities
    
    Removed a destination on 2 rules which caused the Actions to fire twice.
    
    * Incremented version

--- fn_utilities/setup.py
+++ fn_utilities/setup.py
@@ -15,7 +15,7 @@ setup(
         'resilient_circuits>=30.0.0',
         'openpyxl>=2.5.3',
         'pyOpenSSL>=18.0.0',
-        'cryptography>2.3',
+        'cryptography>=2.3',
         'pywinrm>=0.3.0',
         'json2html',
         'lxml'
