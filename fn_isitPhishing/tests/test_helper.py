5b7f556d00e2e7b3aca1db59692b573c5dd65889
AnnMarie Norcross <annmarie.meier.norcross@ibm.com>
2018-12-05 15:47:35 -0500

    INT-791 adding unit test

---
+++ fn_isitPhishing/tests/test_helper.py
@@ -0,0 +1,6 @@
+def get_mock_config():
+    return u"""[fn_isitphishing]
+isitPhishing_api_url=https://ws.isitphishing.org/api/v2/url
+isitPhishing_name=IBM
+isitPhishing_license=A4C5E6G1I7KCXN5V
+"""
--- fn_isitPhishing/tests/test_isitphishing.py
+++ fn_isitPhishing/tests/test_isitphishing.py
@@ -5,16 +5,36 @@ from __future__ import print_function
 import pytest
 from resilient_circuits.util import get_config_data, get_function_definition
 from resilient_circuits import SubmitTestFunction, FunctionResult
+from test_helper import get_mock_config
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
 
 PACKAGE_NAME = "fn_isitPhishing"
 FUNCTION_NAME = "isitphishing"
 
 # Read the default configuration-data section from the package
-config_data = get_config_data(PACKAGE_NAME)
+config_data = get_mock_config()
 
 # Provide a simulation of the Resilient REST API (uncomment to connect to a real appliance)
 resilient_mock = "pytest_resilient_circuits.BasicResilientMock"
 
+
+def mocked_requests_get(*args, **kwargs):
+    class MockResponse:
+        """
+        A mock class intended to mock the entire Response attribute for a requests.
+        """
+        def __init__(self, status_code, content):
+          self.content = content
+          self.status_code = status_code
+
+    if kwargs.get('success') == True:
+        return MockResponse(200, "\n status:  PHISHING \n")
+
+    else:
+        return MockResponse(400, "Bad API request")
 
 def call_isitphishing_function(circuits, function_params, timeout=10):
     # Fire a message to the function
@@ -29,20 +49,25 @@ def call_isitphishing_function(circuits, function_params, timeout=10):
 
 class TestIsitphishing:
     """ Tests for the isitphishing function"""
-
+    inputs = {
+        "isitphishing_url": "http://www.thisisaphishingurl.com"
+    }
+    output = {"analysis": {"status": "PHISHING"},
+              "inputs": {"URL": inputs["isitphishing_url"]}
+    }
     def test_function_definition(self):
         """ Test that the package provides customization_data that defines the function """
         func = get_function_definition(PACKAGE_NAME, FUNCTION_NAME)
         assert func is not None
 
-    @pytest.mark.parametrize("isitphishing_url, expected_results", [
-        ("text", {"value": "xyz"}),
-        ("text", {"value": "xyz"})
-    ])
-    def test_success(self, circuits_app, isitphishing_url, expected_results):
+    @pytest.mark.parametrize("inputs, expected_results", [(inputs, output)])
+    def test_success(self, circuits_app, inputs, expected_results):
         """ Test calling with sample values for the parameters """
-        function_params = { 
-            "isitphishing_url": isitphishing_url
-        }
-        results = call_isitphishing_function(circuits_app, function_params)
-        assert(expected_results == results)
+        function_params = inputs
+
+        with patch("fn_isitphishing.components.isitphishing.requests.post") as mock_requests_get:
+            # Replace the return value of our mock_session with a custom function
+            mock_requests_get.return_value = mocked_requests_get(success=True)
+
+            results = call_isitphishing_function(circuits_app, function_params)
+            assert(expected_results == results)
