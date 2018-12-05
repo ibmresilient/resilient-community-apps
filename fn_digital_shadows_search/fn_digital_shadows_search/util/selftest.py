db32a231cac2399395d8700add54d24ebed936a0
Shane Curtin <shane.curtin@ie.ibm.com>
2018-12-05 13:59:28 +0000

    INT-178: Add selftest

--- fn_digital_shadows_search/fn_digital_shadows_search/util/selftest.py
+++ fn_digital_shadows_search/fn_digital_shadows_search/util/selftest.py
@@ -5,11 +5,24 @@
 """
 
 import logging
+import requests
+from requests.auth import HTTPBasicAuth
+from requests.adapters import HTTPAdapter
+import json
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 log.addHandler(logging.StreamHandler())
 
+def get_config_option(option_name, opts, optional=False):
+  """Given option_name, checks if it is in appconfig. Raises ValueError if a mandatory option is missing"""
+  option = opts.get(option_name)
+
+  if not option and optional is False:
+    err = "'{0}' is mandatory and is not set in the app.config file. You must set this value to run this function".format(option_name)
+    raise ValueError(err)
+  else:
+    return option
 
 def selftest_function(opts):
     """
@@ -17,4 +30,38 @@ def selftest_function(opts):
     Suggested return values are be unimplemented, success, or failure.
     """
     options = opts.get("fn_digital_shadows_search", {})
-    return {"state": "unimplemented"}
+
+    api_key = get_config_option("ds_api_key", options)
+    api_secret = get_config_option("ds_api_secret", options)
+    base_url = get_config_option("ds_base_url", options)
+
+    headers = {'content-type': 'application/json; charset=utf-8', 'Accept': 'application/json'}
+    basic_auth = HTTPBasicAuth(api_key, api_secret)
+
+    url = "{0}{1}".format(base_url, "/search/find")
+    
+    try:
+      res = requests.post(
+        url,
+        json.dumps({"query": "8.8.8.8"}),
+        auth=basic_auth,
+        headers=headers,
+        verify=True)
+
+      res.raise_for_status()
+      
+      if res.status_code == 200:
+        return {"state": "success"}
+
+      else:
+        return {
+          "state": "failure",
+          "reason": "Status Code {0}".format(res.status_code)
+        }
+
+    except Exception as e:
+      log and log.error(e)
+      return {
+        "state": "failure",
+        "reason": e
+      }
