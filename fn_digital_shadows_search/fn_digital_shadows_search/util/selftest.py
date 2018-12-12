db346fcd87c978cd9114d6f54e4ff3d20b3fda6a
Shane Curtin <shane.curtin@ie.ibm.com>
2018-12-12 17:23:46 +0000

    INT-178: Fix Pylint selftest

--- fn_digital_shadows_search/fn_digital_shadows_search/util/selftest.py
+++ fn_digital_shadows_search/fn_digital_shadows_search/util/selftest.py
@@ -1,29 +1,30 @@
 # (c) Copyright IBM Corp. 2010, 2018. All Rights Reserved.
 # -*- coding: utf-8 -*-
-# pragma pylint: disable=unused-argument, no-self-use
+# pragma pylint: disable=unused-argument, no-self-use, line-too-long
 """Function implementation
    test with: resilient-circuits selftest -l fn_digital_shadows_search
 """
 
 import logging
+import json
 import requests
 from requests.auth import HTTPBasicAuth
-from requests.adapters import HTTPAdapter
-import json
 
-log = logging.getLogger(__name__)
-log.setLevel(logging.INFO)
-log.addHandler(logging.StreamHandler())
+LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.INFO)
+LOG.addHandler(logging.StreamHandler())
+
 
 def get_config_option(option_name, opts, optional=False):
-  """Given option_name, checks if it is in appconfig. Raises ValueError if a mandatory option is missing"""
-  option = opts.get(option_name)
+    """Given option_name, checks if it is in appconfig. Raises ValueError if a mandatory option is missing"""
+    option = opts.get(option_name)
 
-  if not option and optional is False:
-    err = "'{0}' is mandatory and is not set in the app.config file. You must set this value to run this function".format(option_name)
-    raise ValueError(err)
-  else:
-    return option
+    if not option and optional is False:
+        err = "'{0}' is mandatory and is not set in the app.config file. You must set this value to run this function".format(option_name)
+        raise ValueError(err)
+    else:
+        return option
+
 
 def selftest_function(opts):
     """
@@ -39,30 +40,29 @@ def selftest_function(opts):
     headers = {'content-type': 'application/json; charset=utf-8', 'Accept': 'application/json'}
     basic_auth = HTTPBasicAuth(api_key, api_secret)
 
-    url = "{0}{1}".format(base_url, "/search/find")
-    
+    url = "{0}{1}".format(base_url, "/api/search/find")
+
     try:
-      res = requests.post(
-        url,
-        json.dumps({"query": "8.8.8.8"}),
-        auth=basic_auth,
-        headers=headers,
-        verify=True)
+        res = requests.post(
+            url,
+            json.dumps({"query": "8.8.8.8"}),
+            auth=basic_auth,
+            headers=headers,
+            verify=True)
 
-      res.raise_for_status()
-      
-      if res.status_code == 200:
-        return {"state": "success"}
+        res.raise_for_status()
 
-      else:
+        if res.status_code == 200:
+            return {"state": "success"}
+
         return {
-          "state": "failure",
-          "reason": "Status Code {0}".format(res.status_code)
+            "state": "failure",
+            "reason": "Status Code {0}".format(res.status_code)
         }
 
-    except Exception as e:
-      log and log.error(e)
-      return {
-        "state": "failure",
-        "reason": e
-      }
+    except Exception as exp:
+        LOG.error(exp)
+        return {
+            "state": "failure",
+            "reason": exp
+        }
