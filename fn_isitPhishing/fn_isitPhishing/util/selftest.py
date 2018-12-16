55103d3acab095a7f35d2268e65b4dd0207838c4
AnnMarie Norcross <annmarie.meier.norcross@ibm.com>
2018-12-16 14:14:49 -0500

    INT-791 implement selftest_function

--- fn_isitPhishing/fn_isitPhishing/util/selftest.py
+++ fn_isitPhishing/fn_isitPhishing/util/selftest.py
@@ -4,7 +4,9 @@
    test with: resilient-circuits selftest -l fn_isitPhishing
 """
 
+import requests
 import logging
+from fn_isitPhishing.lib.isitphishing_util import get_license_key
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
@@ -17,4 +19,37 @@ def selftest_function(opts):
     Suggested return values are be unimplemented, success, or failure.
     """
     options = opts.get("fn_isitPhishing", {})
-    return {"state": "unimplemented"}
+
+    # Get the license key to access the API endpoint.
+    auth_token = get_license_key(options["isitphishing_name"], options["isitphishing_license"])
+
+    # Build the header and the data payload.
+    headers = {
+        "Authorization": u'Bearer {}'.format(auth_token),
+        "Content-type": "application/json",
+    }
+    api_url = u"{0}/url".format(options['isitphishing_api_url'])
+
+    payload = {"url": "http://www.thisisaphishingurl.com", "force": False, "smart": True, "timeout": 8000}
+
+    state, reason = "", ""
+    try:
+        response = requests.post(api_url, headers=headers, json=payload)
+
+        # Check the results
+        if response.status_code == 200:
+            state = "success"
+        else:
+            state = "failure"
+            reason = response.error
+    except Exception as ex:
+        state = "failure"
+        reason = str(ex)
+
+    result = {
+        "state": state,
+        "reason": reason
+    }
+
+    log.info(result)
+    return result
