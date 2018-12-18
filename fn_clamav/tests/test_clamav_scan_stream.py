f647e25129b5fb8a029775e962529b38f2fb3874
John Prendergast <johnpren@ie.ibm.com>
2018-12-18 10:58:04 +0000

    INT-777 - Added Unit/Function tests.

--- fn_clamav/tests/test_clamav_scan_stream.py
+++ fn_clamav/tests/test_clamav_scan_stream.py
@@ -3,8 +3,10 @@
 
 from __future__ import print_function
 import pytest
+from mock import patch
 from resilient_circuits.util import get_config_data, get_function_definition
 from resilient_circuits import SubmitTestFunction, FunctionResult
+from mock_artifacts import mocked_pyclamd_client, mocked_res_client
 
 PACKAGE_NAME = "fn_clamav"
 FUNCTION_NAME = "clamav_scan_stream"
@@ -35,15 +37,22 @@ class TestClamavScanStream:
         func = get_function_definition(PACKAGE_NAME, FUNCTION_NAME)
         assert func is not None
 
-    @pytest.mark.parametrize("base64content, file_name, expected_results", [
-        ("text", "text", {"value": "xyz"}),
-        ("text", "text", {"value": "xyz"})
+    @patch('fn_clamav.components.clamav_scan_stream.pyclamd.ClamdNetworkSocket', side_effect=mocked_pyclamd_client)
+    @patch('resilient_circuits.actions_component.ResilientComponent.rest_client', side_effect=mocked_res_client)
+    @pytest.mark.parametrize("incident_id, artifact_id, attachment_id, task_id, expected_results_1, expected_results_2", [
+        (2095, None, 25, None, "incident_eicar.txt", {u'stream': [u'FOUND', u'Eicar-Test-Signature']}),
+        (2095, 10, None, None, "artifact_eicar.txt", {u'stream': [u'FOUND', u'Eicar-Test-Signature']}),
+        (2095, None, 25, 2251251, "task_eicar.txt", {u'stream': [u'FOUND', u'Eicar-Test-Signature']})
     ])
-    def test_success(self, circuits_app, base64content, file_name, expected_results):
+    def test_success(self, mock_get_1, mock_get_2, circuits_app, incident_id, artifact_id, attachment_id, task_id,
+                     expected_results_1, expected_results_2):
         """ Test calling with sample values for the parameters """
         function_params = { 
-            "base64content": base64content,
-            "file_name": file_name
+            "incident_id": incident_id,
+            "artifact_id": artifact_id,
+            "attachment_id": attachment_id,
+            "task_id": task_id
         }
         results = call_clamav_scan_stream_function(circuits_app, function_params)
-        assert(expected_results == results)
+        assert (expected_results_1 == results["file_name"])
+        assert(expected_results_2 == results["response"])
