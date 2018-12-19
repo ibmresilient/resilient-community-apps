45b6ed8a3d8391309257c054e48726e136614eb6
Neetin <neetin@oc3777881733.ibm.com>
2018-12-19 17:47:16 +0530

    1. New Function for Querying TOR Network for Specified IP Address or Domain Names

---
+++ fn_query_tar_network/fn_query_tar_network/util/config.py
@@ -0,0 +1,18 @@
+# (c) Copyright IBM Corp. 2010, 2018. All Rights Reserved.
+# -*- coding: utf-8 -*-
+
+"""Generate a default configuration-file section for fn_query_tar_network"""
+
+from __future__ import print_function
+
+
+def config_section_data():
+    """Produce the default configuration section for app.config,
+       when called by `resilient-circuits config [-c|-u]`
+    """
+    config_data = u"""[fn_query_tar_network]
+base_url = https://onionoo.torproject.org/details
+flag = Exit  
+data_fields = exit_addresses,or_addresses,host_name
+    """
+    return config_data
---
+++ fn_query_tar_network/fn_query_tar_network/util/selftest.py
@@ -0,0 +1,31 @@
+# (c) Copyright IBM Corp. 2010, 2018. All Rights Reserved.
+# -*- coding: utf-8 -*-
+# pragma pylint: disable=unused-argument, no-self-use
+"""Function implementation
+   test with: resilient-circuits selftest -l fn_query_tar_network
+"""
+
+import logging
+import requests
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+log.addHandler(logging.StreamHandler())
+
+
+def selftest_function(opts):
+    """
+    Placeholder for selftest function. An example use would be to test package api connectivity.
+    Suggested return values are be unimplemented, success, or failure.
+    """
+    options = opts.get("fn_query_tar_network", {})
+    try:
+        connection_status = requests.get(options.get('base_url'))
+        if not connection_status.status_code == 200:
+            raise requests.ConnectionError("The Status code {}".format(connection_status))
+        log.info("Successfully Established connection to TOR : {}".format(connection_status))
+        return {"state": "Success"}
+
+    except Exception as e:
+        log.info("Failed Connection to TOR Error - {}".format(e))
+        return {"state": "Failed"}
