aea320e782e4470536b8bf7aee22425abd1f6617
brianwal <brianwal@us.ibm.com>
2019-01-04 17:04:05 -0500

    Added support for mulitiple url void api calls.

--- fn_url_void/fn_url_void/components/url_void_retrive_information.py
+++ fn_url_void/fn_url_void/components/url_void_retrive_information.py
@@ -1,12 +1,23 @@
 # -*- coding: utf-8 -*-
+# (c) Copyright IBM Corp. 2010, 2019. All Rights Reserved.
 # pragma pylint: disable=unused-argument, no-self-use
 """Function implementation"""
 
 import logging
 import time
 import requests
+import xmltodict
 from resilient_circuits import ResilientComponent, function, handler, StatusMessage, FunctionResult, FunctionError
+from resilient_lib.components.resilient_common import validate_fields
+from resilient_lib import RequestsCommon
 import fn_url_void.util.selftest as selftest
+try:
+    from urlparse import urlparse  # Python 2 import
+except:
+    from urllib.parse import urlparse  # Python 3 import
+
+
+log = logging.getLogger(__name__)
 
 
 class FunctionComponent(ResilientComponent):
@@ -16,6 +27,8 @@ class FunctionComponent(ResilientComponent):
         """constructor provides access to the configuration options"""
         super(FunctionComponent, self).__init__(opts)
         self.options = opts.get("fn_url_void", {})
+
+        validate_fields(["api_key"], self.options)
         selftest.selftest_function(opts)
 
     @handler("reload")
@@ -23,26 +36,28 @@ class FunctionComponent(ResilientComponent):
         """Configuration options have changed, save new values"""
         self.options = opts.get("fn_url_void", {})
 
-    @function("url_void_retrive_information")
+    @function("url_void_function")
     def _url_void_retrive_information_function(self, event, *args, **kwargs):
         """Function: Retrieves information of a URL from the  URL Void database."""
         try:
             yield StatusMessage("starting...")
             start_time = time.time()
 
+            ## Add support for Requests Common
+            ## req_common = RequestsCommon(self.opts)
+
             api_key = self.options.get("api_key")
             identifier = self.options.get("identifier", "api1000")
 
             # Get the function parameters:
             artifact_value = kwargs.get("artifact_value")  # text
-            if artifact_value is None:
-                raise ValueError("artifact_value is required to run this function.")
+            url_void_api = self.get_select_param(kwargs.get("url_void_api", "Retrieve"))  # select
+            validate_fields([artifact_value, url_void_api], kwargs)
 
-            log = logging.getLogger(__name__)
             log.info("artifact_value: %s", artifact_value)
+            log.info("url_void_api: %s", url_void_api)
 
-            artifact_value = artifact_value.replace("https://", "")
-            res = requests.get("https://api.urlvoid.com/{}/{}/host/{}".format(identifier, api_key, artifact_value))
+            response = call_url_void_api(artifact_value, identifier, api_key, url_void_api)
 
             yield StatusMessage("done...")
             end_time = time.time()
@@ -54,3 +69,33 @@ class FunctionComponent(ResilientComponent):
             yield FunctionResult(results)
         except Exception:
             yield FunctionError()
+
+
+def get_netloc(url):
+    parsed_url = urlparse(url)
+
+    return parsed_url.netloc
+
+
+def get_endpoint(url_api):
+    if url_api == "Retrieve":
+        return ""
+    elif url_api == "Scan":
+        return "scan"
+    elif url_api == "Rescan":
+        return "rescan"
+    else:
+        raise ValueError("Update method, unexpected url_api value")
+
+
+def call_url_void_api(artifact_value, identifier, api_key, url_void_api):
+    netloc = get_netloc(artifact_value)
+    endpoint = get_endpoint(url_void_api)
+    response = requests.get("https://api.urlvoid.com/{}/{}/host/{}/".format(identifier, api_key, netloc, endpoint))
+
+    if response.status_code > 299 or response.status_code < 200:
+        log.debug(response.content)
+        raise ValueError("Request not successful. Status code: {}".format(str(response.status_code)))
+
+    # Convert and return response to a dictionary
+    return xmltodict.parse(response.content)
