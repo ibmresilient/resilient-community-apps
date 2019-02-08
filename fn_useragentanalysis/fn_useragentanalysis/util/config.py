00ac7c609aa7fe40b2d25ad53b9819b3b4699153
tmack <tmack@essextec.com>
2019-02-08 12:03:05 -0500

    Fixing config to be left aligned

--- fn_useragentanalysis/fn_useragentanalysis/util/config.py
+++ fn_useragentanalysis/fn_useragentanalysis/util/config.py
@@ -9,8 +9,9 @@ def config_section_data():
     """Produce the default configuration section for app.config,
        when called by `resilient-circuits config [-c|-u]`
     """
-    config_data = u"""[fn_useragentanalysis]
-                     url=https://api.whatismybrowser.com/api/v2/user_agent_parse
-                     api_key=KEY
-                """
+    config_data = u"""
+    [fn_useragentanalysis]
+    url=https://api.whatismybrowser.com/api/v2/user_agent_parse
+    api_key=KEY
+    """
     return config_data
