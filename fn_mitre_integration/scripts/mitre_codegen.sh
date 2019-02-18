e1c33115b795901810ab4f908cd9cbb2d3fbd676
yongjian feng <yj@debian64Bit.debian64.irsys.com>
2019-02-18 15:57:17 -0500

    Add codegen script.

---
+++ fn_mitre_integration/scripts/mitre_codegen.sh
@@ -0,0 +1,6 @@
+resilient-circuits codegen \
+-p fn_mitre_integration \
+-f mitre_tactic_information mitre_technique_information \
+-m fn_mitre_integration \
+--datatable mitre_attack_of_artifact mitre_attack_of_incident \
+--exportfile export.res
