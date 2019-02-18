1f008c891194aea4052c7505ddef4bd5281ac5ff
yongjian feng <yj@debian64Bit.debian64.irsys.com>
2019-02-18 16:04:13 -0500

    Added workflow comments

--- fn_mitre_integration/scripts/mitre_codegen.sh
+++ fn_mitre_integration/scripts/mitre_codegen.sh
@@ -2,5 +2,6 @@ resilient-circuits codegen \
 -p fn_mitre_integration \
 -f mitre_tactic_information mitre_technique_information \
 -m fn_mitre_integration \
+--workflow mitre_technique_task \
 --datatable mitre_attack_of_artifact mitre_attack_of_incident \
 --exportfile export.res
