a2370213fe7c40247064b8b5c350874b263b9755
yongjian feng <yj@debian64Bit.debian64.irsys.com>
2019-02-18 14:24:03 -0500

    Add tests to cover the new codes

--- fn_qradar_advisor/scripts/analysis_post_process_script.py
+++ fn_qradar_advisor/scripts/analysis_post_process_script.py
@@ -1,25 +1,32 @@
-import java.util.Date as Date
-
-# Return data of function Watson Search with Local Context:
+#
+# Result retured by the QRadar Advisor Offense Analysis function:
 #   * results.observables: observables and their details, used here to be output to Data table.
 #   * results.note: html representation of STIX data, used here to generate a Note.
-#   * results.summary: used here to create a Task.
-#   * results.stix: raw stix data (not used here), for any customized parsing.
-
-
+#   * results.insights: used here to create a Task.
+#   * results.stix: raw stix data, preserved for any customized parsing.
+#
+#
+#
 # We publish a data table according to the stix
-date_str = str(Date())
 for observable in results.observables:
-  qradar_obs = incident.addRow("qradar_advisor_observable_for_artifact")
+  qradar_obs = incident.addRow("qradar_advisor_observable")
   qradar_obs.qradar_advisor_toxicity = observable.toxicity 
   qradar_obs.qradar_advisor_relevance = observable.relevance
   qradar_obs.qradar_advisor_type = observable.type 
   qradar_obs.qradar_advisor_description = observable.description 
-  qradar_obs.artifact_related = artifact.value
-  qradar_obs.full_search_time = date_str
+
+# Pass insights data (with MITRE ATTACK tactics information) to following function
+# using workflow.properties.qraw_offense_insights. Refer to the Output tab please
+
 # Our STIX tree
 html = helper.createRichText(results.note)
 incident.addNote(html)
 
-# Create a task
-incident.addTask("Review Watson Search with Local Context of artifact: " + artifact.value, "Initial", results.summary)
+# Task
+task_title = "Review QRadar Advisor Analysis for Offense " + str(incident.properties.qradar_id)
+task_summary = results.insights.insights + "\n\n" + results.insights.stage3_insights
+incident.addTask(task_title, "Initial", task_summary)
+
+# Note that results.stix is the raw stix return from QRadar Advisor in stix 2 (json) format
+# Users can add their customize codes to handle the stix data here
+#
