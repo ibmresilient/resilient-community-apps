0606be1805c7c5ae9326b28bdf181a1e9fc2dce7
yongjian feng <yj@debian64Bit.debian64.irsys.com>
2019-02-19 14:52:44 -0500

    Dependency adjustment atain

--- fn_qradar_advisor/scripts/qraw_codegen.sh
+++ fn_qradar_advisor/scripts/qraw_codegen.sh
@@ -6,5 +6,5 @@ resilient-circuits codegen \
 --rule "QRadar Advisor Offense Analysis" "Watson Search" "Watson Search with Local Context" "Create Artifact (QRadar Advisor Analysis)" \
 "Create Artifact (Watson Search with Local Context)" "Map rule to MITRE tactic" \
 --script "Create Artifact for QRadar Advisor Analysis Observable" "Create Artifact for Watson Search with Local Context" \
---datatable qradar_advisor_observable qradar_advisor_observable_for_artifact mitre_attack_of_artifact mitre_attack_of_incident mitre_attack_techniques \
+--datatable qradar_advisor_observable qradar_advisor_observable_for_artifact mitre_attack_of_artifact mitre_attack_of_incident mitre_attack_techniques qradar_reference_set \
 --exportfile export.res
