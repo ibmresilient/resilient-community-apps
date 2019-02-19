2abe7cf6b599636871b7dd72f978d198f80f3f98
yongjian feng <yj@debian64Bit.debian64.irsys.com>
2019-02-19 10:39:03 -0500

    Clean up dependency

--- fn_mitre_integration/scripts/mitre_codegen.sh
+++ fn_mitre_integration/scripts/mitre_codegen.sh
@@ -3,5 +3,6 @@ resilient-circuits codegen \
 -f mitre_tactic_information mitre_technique_information \
 -m fn_mitre_integration \
 --workflow mitre_technique_task \
+--rule "Create Task For Technique" \
 --datatable mitre_attack_of_artifact mitre_attack_of_incident \
 --exportfile export.res
