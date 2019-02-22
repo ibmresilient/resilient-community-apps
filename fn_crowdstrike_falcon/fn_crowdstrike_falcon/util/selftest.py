15f4985df87c3a93e16fff0d9f519a9cf729ed2e
Shane Curtin <shane.curtin@ie.ibm.com>
2019-02-22 17:46:02 +0000

    INT-908: Implement selftest

--- fn_crowdstrike_falcon/fn_crowdstrike_falcon/util/selftest.py
+++ fn_crowdstrike_falcon/fn_crowdstrike_falcon/util/selftest.py
@@ -6,16 +6,66 @@
 """
 
 import logging
+from fn_crowdstrike_falcon.util.cs_helper import CrowdStrikeHelper
 
-log = logging.getLogger(__name__)
-log.setLevel(logging.INFO)
-log.addHandler(logging.StreamHandler())
+LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.INFO)
+LOG.addHandler(logging.StreamHandler())
 
 
 def selftest_function(opts):
     """
-    Placeholder for selftest function. An example use would be to test package api connectivity.
-    Suggested return values are be unimplemented, success, or failure.
+    Verify OAuth2 and Basic Auth Credentials for CrowdStrike
     """
-    options = opts.get("fn_crowdstrike_falcon", {})
-    return {"state": "unimplemented"}
+
+    try:
+
+        LOG.info("Verifying app.config values for %s", CrowdStrikeHelper.app_config_section)
+
+        app_configs = opts.get(CrowdStrikeHelper.app_config_section, {})
+
+        cs_helper = CrowdStrikeHelper(app_configs)
+        
+        LOG.info("Verifying OAuth2 Credentials...")
+
+        cs_helper.get_oauth2_token()
+
+        LOG.info("Success")
+
+        LOG.info("Verifying Basic Auth Credentials...")
+
+        res = cs_helper.get_device_status("x")
+
+        if res is not None and res.get("err_msg") == "Could not get device status for device_id x.":
+            LOG.info("Success")
+        else:
+            raise ValueError("Failed to verify Basic Auth Credentials")
+
+        LOG.info("Test was successful")
+
+        return {
+            "state": "success"
+        }
+
+    except Exception as err:
+        err_reason_msg = """Could not connect to CrowdStrike.
+            error: {0}
+            ---------
+            Current Configs in app.config file::
+            ---------
+            cs_falcon_oauth2_base_url: {1}
+            cs_falcon_oauth2_cid: {2}
+            cs_falcon_bauth_base_url: {3}
+            cs_falcon_bauth_api_uuid: {4}\n""".format(
+                err,
+                cs_helper.oauth2_base_url,
+                cs_helper.oauth2_cid,
+                cs_helper.bauth_base_url,
+                cs_helper.bauth_api_uuid)
+
+        LOG.error(err_reason_msg)
+
+        return {
+            "state": "failure",
+            "reason": err_reason_msg
+        }
