1f69f6da1ee30682eb513f8f7a3046908d9019b0
brianwal <brianwal@us.ibm.com>
2019-03-08 17:26:17 -0500

    INT-574 updated config file for seperate packet and log server configurations.

--- fn_rsa_netwitness/fn_rsa_netwitness/util/config.py
+++ fn_rsa_netwitness/fn_rsa_netwitness/util/config.py
@@ -10,10 +10,16 @@ def config_section_data():
        when called by `resilient-circuits config [-c|-u]`
     """
     config_data = u"""[fn_rsa_netwitness]
-nw_url=<https://test.nw_server.com>
-nw_port=<default port for communication>
-nw_user=<nw_username>
-nw_password=<nw_password>
-cafile=[true|false]
+nw_packet_server_url=<http://test.nw_packet_server.com:50104>
+nw_packet_server_port=<port for communication>
+nw_packet_server_user=<nw_packet_server_username>
+nw_packet_server_password=<nw_packet_server_password>
+nw_packet_server_cafile=[true|false]
+
+nw_log_server_url=<http://test.nw_log_server.com:50102>
+nw_log_server_port=<port for communication>
+nw_log_server_user=<nw_log_server_username>
+nw_log_server_password=<nw_log_server_password>
+nw_log_server_cafile=[true|false]
 """
     return config_data
