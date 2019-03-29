91747de88e733d60bc92879bde0e37009a697510
yongjian feng <yj@debian64Bit.debian64.irsys.com>
2019-03-29 10:48:42 -0400

    Regenerate customize.py using v31.

--- fn_qradar_advisor/scripts/qraw_codegen.sh
+++ fn_qradar_advisor/scripts/qraw_codegen.sh
@@ -2,7 +2,7 @@ resilient-circuits codegen \
 -p fn_qradar_advisor \
 -f qradar_advisor_offense_analysis qradar_advisor_quick_search qradar_advisor_full_search qradar_advisor_map_rule \
 -m fn_qradar_advisor \
---field mitre_tactic_name qradar_rule \
+--field mitre_tactic_name qradar_rule qradar_id \
 --workflow qradar_advisor_offense_analysis qradar_advisor_quick_search qradar_advisor_full_search qradar_advisor_map_rule \
 --rule "QRadar Advisor Offense Analysis" "Watson Search" "Watson Search with Local Context" "Create Artifact (QRadar Advisor Analysis)" \
 "Create Artifact (Watson Search with Local Context)" "Map QRadar rule" \
