6d31173aab4a87d8732a673b1273a18a717df5f1
neetin.k34@gmail.com <neetin.k34@gmail.com>
2019-04-02 22:05:01 +0530

    Backup ioc parser

--- fn_ioc_parser/fn_ioc_parser/util/ioc_parser_helper.py
+++ fn_ioc_parser/fn_ioc_parser/util/ioc_parser_helper.py
@@ -7,7 +7,39 @@ from pdfminer.pdfinterp import PDFPageInterpreter
 from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.pdfpage import PDFPage
 
+
+
 class IOCParserHelper(object):
+    def __init__(self):
+        # Resilient Rest Api calls url's
+        self.ARTIFACT_META_DATA_URL = "/incidents/{0}/artifacts/{1}"
+        self.ARTIFACT_DATA_URL = "/incidents/{0}/artifacts/{1}/contents"
+        self.ATTACHMENT_META_DATA_URL = "/incidents/{0}/attachments/{1}"
+        self.ATTACHMENT_DATA_URL = "/incidents/{0}/attachments/{1}/contents"
+        self.TASK_META_DATA_URL = "/tasks/{0}/attachments/{1}"
+        self.TASK_DATA_URL = "/tasks/{0}/attachments/{1}/contents"
+
+    @classmethod
+    def extract_text_from_bytes_data(cls, filename, file_data_bytes):
+        if filename.find('.pdf') != -1:
+            """
+            Converting .pdf file data to plain text format
+            """
+            file_string_data = IOCParserHelper.extract_text_from_pdf(file_data_bytes)
+        elif filename.find('.docx') != -1:
+            """
+            Converting .docx file data to plain text format
+            """
+            file_string_data = IOCParserHelper.extract_text_from_docx(file_data_bytes)
+        elif filename.split('.')[-1].strip() in ['doc', 'odt', 'ott', 'dot']:
+            file_string_data = None
+        else:
+            file_string_data = file_data_bytes
+
+        if file_string_data:
+            file_string_data = str(file_string_data)
+
+        return file_string_data
     @classmethod
     def extract_text_from_pdf(cls, attachment_input):
         """
@@ -33,11 +65,10 @@ class IOCParserHelper(object):
                     page_interpreter.process_page(page)
 
                 extracted_input = fake_file_handle.getvalue()
-
+            finally:
                 # close open handles
                 converter.close()
                 fake_file_handle.close()
-            finally:
                 os.unlink(temp_pdf_file.name)
         return extracted_input
 
@@ -54,12 +85,8 @@ class IOCParserHelper(object):
                 # Write and close tempfile
                 temp_doc_file.write(attachment_input)
                 read_doc = Document(temp_doc_file)
-                print("Docuement file name - ", temp_doc_file.name)
                 for paragraph in read_doc.paragraphs:
                     extracted_input += paragraph.text
-                print("#################", extracted_input)
-            except:
-                os.unlink(temp_doc_file.name)
             finally:
                 os.unlink(temp_doc_file.name)
         return extracted_input
