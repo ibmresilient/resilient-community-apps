69bcb5f30d454704826e4cd0cfbfbba3e31b2c45
neetin.k34@gmail.com <neetin.k34@gmail.com>
2019-04-17 16:02:38 +0530

    an update

--- fn_ioc_parser/fn_ioc_parser/__init__.py
+++
@@ -1,6 +0,0 @@
-# (c) Copyright IBM Corp. 2010, 2019. All Rights Reserved.
-import pkg_resources
-try:
-    __version__ = pkg_resources.get_distribution(__name__).version
-except pkg_resources.DistributionNotFound:
-    pass
