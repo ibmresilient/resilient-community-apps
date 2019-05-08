a48ce613dbba5dadcc7dac932210a53d51bddcdb
brianwal <brianwal@us.ibm.com>
2019-05-08 13:22:12 -0400

    Removed rc-cts and rc-webserver from repo.

--- rc-cts/rc_cts/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-__import__('pkg_resources').declare_namespace(__name__)
-
-from rc_cts.lib.threat_models import *
-from rc_cts.components.threat_webservice import ThreatServiceLookupEvent
-from rc_cts.components.threat_webservice import ThreatLookupIncompleteException
