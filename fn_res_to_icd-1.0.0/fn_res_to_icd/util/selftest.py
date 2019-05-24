2a86ce5f46bcc652778179f3a749fbd822bf743c
Sean O Gorman <Sean.OGorman@ibm.com>
2019-05-24 13:33:50 +0100

    First commit of function code

---
+++ fn_res_to_icd-1.0.0/fn_res_to_icd/util/selftest.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# pragma pylint: disable=unused-argument, no-self-use
+"""Function implementation
+   test with: resilient-circuits selftest -l fn_res_to_icd
+"""
+
+import logging
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+log.addHandler(logging.StreamHandler())
+
+
+def selftest_function(opts):
+    """
+    Placeholder for selftest function. An example use would be to test package api connectivity.
+    Suggested return values are be unimplemented, success, or failure.
+    """
+    options = opts.get("fn_res_to_icd", {})
+    return {"state": "unimplemented"}
