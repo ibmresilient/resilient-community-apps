4f915504ed061fd624944ef30e0cd60c84ec9135
Shane Curtin <shane.curtin@ie.ibm.com>
2019-05-29 09:32:01 +0100

    INT-1315: Bump to v1.0.1. Update long_description

--- fn_service_now/setup.py
+++ fn_service_now/setup.py
@@ -5,13 +5,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='fn_service_now',
-    version='1.0.0',
+    version='1.0.1',
     license='MIT',
     author='IBM Resilient',
     author_email='support@resilientsystems.com',
     url='www.resilientsystems.com',
-    description="Resilient Circuits Components to Integrate with the ServiceNow Platform",
-    long_description="Contains Functions to sync Incidents, Tasks, Notes and Attachments between IBM Resilient and ServiceNow",
+    description="Bi-directional synchronization of Incidents, Tasks, Notes and Attachments with ServiceNow",
+    long_description="""Create an IBM Resilient Incident/Task from a ServiceNow Record in the Incident Table.
+        Create a ServiceNow Record in the Incident Table from an IBM Resilient Incident/Task.
+        Sync notes between a related IBM Resilient Incident/Task and a ServiceNow Record.
+        Send Attachments from an IBM Resilient Incident/Task to a related ServiceNow Record.""",
     install_requires=[
         'resilient_circuits>=31.0.0',
         'beautifulsoup4>=4.6.3'
