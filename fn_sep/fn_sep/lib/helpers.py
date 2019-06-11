02739cf6c319db4e8c59124aec4db9e9dde53365
John Prendergast <johnpren@ie.ibm.com>
2019-06-11 16:22:32 +0100

    INT-1002, INT-1003, INT-1004 - Fixed bug with csv name.

--- fn_sep/fn_sep/lib/helpers.py
+++ fn_sep/fn_sep/lib/helpers.py
@@ -90,7 +90,7 @@ def generate_result_cvs(rtn, sep_commandid):
     """
     file_content = ""
     file_name = "EOC_scan_results_for_commandid_{0}_{1}.csv" \
-        .format(sep_commandid, datetime.datetime.today().strftime('%Y%m%d%H%M%S"'))
+        .format(sep_commandid, datetime.datetime.today().strftime('%Y%m%d%H%M%S'))
     file_content = "Computer name,Computer id,Artifact type,Artifact value,Match type,Match value\n"
 
     eps = rtn["content"]
@@ -159,6 +159,7 @@ def add_non_compliant_ep_properties(rtn, non_compliant_endpoints, results):
             for f in READABLE_TIME_FIELDS:
                 ep[f] = eps[i][f]
             for f in EP_ENGINE_STATUS_FIELDS:
+
                 if eps[i][f]:
                     ep[f] = "On"
                 else:
