50f3e7910ad321d390fd78d27b7d266bb79d15d5
Sean O Gorman <Sean.OGorman@ibm.com>
2019-06-11 12:07:01 +0100

    Update fn_res_to_icd.py
    Remove hard coded input & and added payload

--- fn_res_to_icd/fn_res_to_icd/components/fn_res_to_icd.py
+++ fn_res_to_icd/fn_res_to_icd/components/fn_res_to_icd.py
@@ -10,7 +10,8 @@ import xmltodict
 import datetime as dt
 import pytz
 from resilient_circuits import ResilientComponent, function, handler, StatusMessage, FunctionResult, FunctionError
-import resilient_lib 
+import resilient_lib
+from resilient_lib import ResultPayload
 from functools import reduce  # forward compatibility for Python 3
 import operator
 
@@ -49,46 +50,42 @@ class FunctionComponent(ResilientComponent):
     def _fn_res_to_icd_function(self, event, *args, **kwargs):
         """Function: This function transfers an qradar severity to a priority on the icd ticket"""
         try:
-            # Get the function parameters
-            #time_date =options.get("time_date")
+            
             incident_id=kwargs.get("incident_id")
             log = logging.getLogger(__name__)
             log.info("incident_id: %s", incident_id)
 
+            # Resilient client and api calls
             res_client = self.rest_client()
             incident_str='/incidents/{incident_id}/'.format(incident_id=incident_id)
             artifact_str='/incidents/{incident_id}/artifacts'.format(incident_id=incident_id)
-
             content=res_client.get(incident_str)
             art_content=res_client.get(artifact_str)
+
+            # Time and date
             utc_ts = content['create_date']
             tz_dt = tz_from_utc_ms_ts(utc_ts, pytz.timezone('Etc/GMT+6'))
             timeval=tz_dt.isoformat()
             time="Date and Time: {0}".format(timeval)
 
-            #for i in art_content:
-            #    pprint.pprint(type(i))
+            #artifact population to icd ticket 
+            icd_priority = 1
             details_payload=''
-            artifact_limit=0
+            artifact_limit=len(art_content)
             i = 0
             try:
-                artifact_limit=int(input("How many artifacts do you wish to populate?"))
                 while i < artifact_limit:
                     if art_content[i]['properties'][0]['name'] in ('source' , 'destination'): 
                         details_payload +='ID: {1} IP Address {2}: {0} \n'.format(art_content[i]['value'],art_content[i]['id'],art_content[i]['properties'][0]['name'].capitalize())
                         i += 1
             except:
-                log.error("Some artifacts may not have populated due to incorrect usage")
+                log.error("Some artifacts may not have populated, please double check on icd desk")
             
-            
-            yield StatusMessage("starting...")
-            #pprint.pprint(art_content)
-            
-            #pprint.pprint(content)
+            ## QRadar severity checking
+
             qradar_sev = content['properties']['qradar_severity']  # number
-            #qradar_sev=6
-            
-            icd_priority = 1
+            log.info("qradar_sev: %s", qradar_sev)
+            payload = ResultPayload('fn_res_to_icd', **kwargs)
 
             if qradar_sev >= 1 and qradar_sev <= 3:
                 icd_priority = 4
@@ -99,10 +96,10 @@ class FunctionComponent(ResilientComponent):
             elif qradar_sev >=7:
                 icd_priority = 1
             else:
-                log.warning("You have not set a Qradar priority, icd will be 4")
+                log.warning("You have not set a Qradar priority, icd priority will be min value (4)")
                 icd_priority = 4
-
-            log.info("qradar_sev: %s", qradar_sev)
+            
+            # Params and Desk call
             params =  { "DESCRIPTION": time ,
              "DESCRIPTION_LONGDESCRIPTION": details_payload ,
              "REPORTEDBYID": "resilient_test@in.ibm.com" ,
@@ -114,26 +111,23 @@ class FunctionComponent(ResilientComponent):
              "CLASSIFICATIONID": "SECURITY ISSUE" ,
              "_lid": "resilient_test@in.ibm.com" ,
              "_lpwd": "Welcome@1234"  } 
-            #["CreateMXINCIDENTResponse"]["@xmlns"]["MXINCIDENTSet'"]["INCIDENT"]["@rowstamp"]["WORKLOG"]["@rowstamp"]["RECORDKEY"]
             response = requests.post("https://icdaas.sccd.ibmserviceengage.com/maximo_cbs-dev2/rest/os/MXINCIDENT/", params=params, verify=False)
-            #pprint.pprint(response.status_code)
-            #pprint.pprint(response.text)
         
+            # xml conversion to dict and reading
             response_dict=xmltodict.parse(response.text)
-            #response_dict=dict(response_obj)
-            
-            #icd_id = 2
             map_list= ["CreateMXINCIDENTResponse","MXINCIDENTSet","INCIDENT","TICKETID"]
             icd_id = getFromDict(response_dict,map_list)
             
             yield StatusMessage("Completed successfully")
-            results = {
+            results = payload.done(success=True,content={
                 "Incident escalated" : incident_id,
                 "icd_id":icd_id,
                 "Details":details_payload
-            }
+            })
             
             # Produce a FunctionResult with the results
             yield FunctionResult(results)
+            log.debug("RESULTS: %s", results)
+            log.info("Complete")
         except Exception:
             yield FunctionError()
