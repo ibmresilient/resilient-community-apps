c9e7e64d309f75d39cfa17ee521fa854886f3770
Sean O Gorman <Sean.OGorman@ibm.com>
2019-06-14 13:08:52 +0100

    pylint to selftest

--- fn_res_to_icd/fn_res_to_icd/util/selftest.py
+++ fn_res_to_icd/fn_res_to_icd/util/selftest.py
@@ -3,35 +3,28 @@
 """Function implementation
    test with: resilient-circuits selftest -l fn_res_to_icd
 """
-
 import logging
 import requests
-import pprint
 
-log = logging.getLogger(__name__)
-log.setLevel(logging.INFO)
-log.addHandler(logging.StreamHandler())
-
+LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.INFO)
+LOG.addHandler(logging.StreamHandler())
 
 def selftest_function(opts):
-   """
-   Placeholder for selftest function. An example use would be to test package api connectivity.
-   Suggested return values are be unimplemented, success, or failure.
-   """
-   try:
-      options = opts.get("fn_res_to_icd", {})
-      icd_email=options.get("icd_email")
-      icd_pass=options.get("icd_pass")
-      log.info(icd_email)
-      log.info(icd_pass)
-      params = {"_lid": icd_email , "_lpwd": icd_pass }
-      log.info('executing api call with credentials')
-      response = requests.post("https://icdaas.sccd.ibmserviceengage.com/maximo_cbs-dev2/oslc/ping/", params=params,verify=False)
-      log.info(response.status_code)
-      return {"state": "success"}
-   except Exception as err:
-      log.error(err)
-      return {"state": "failure"}
+    try:
+        options = opts.get("fn_res_to_icd", {})
+        icd_email = options.get("icd_email")
+        icd_pass = options.get("icd_pass")
+        LOG.info(icd_email)
+        LOG.info(icd_pass)
+        params = {"_lid": icd_email, "_lpwd": icd_pass}
+        LOG.info('executing api call with credentials')
+        response = requests.post("https://icdaas.sccd.ibmserviceengage.com/maximo_cbs-dev2/oslc/ping/", params=params, verify=False)
+        LOG.info(response.status_code)
+        return {"state": "success"}
+    except Exception as err:
+        LOG.error(err)
+        return {"state": "failure"}
 
 
   
