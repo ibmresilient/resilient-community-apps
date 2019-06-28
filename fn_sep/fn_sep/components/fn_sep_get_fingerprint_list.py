d19781c467369e1491e0b4459e4c2b6a082ea9df
John Prendergast <johnpren@ie.ibm.com>
2019-06-28 09:59:18 +0100

    INT-1555 - Fixed bug with status message on 410 error.

--- fn_sep/fn_sep/components/fn_sep_get_fingerprint_list.py
+++ fn_sep/fn_sep/components/fn_sep_get_fingerprint_list.py
@@ -86,14 +86,15 @@ class FunctionComponent(ResilientComponent):
 
             results = rp.done(True, rtn)
 
-            if "errors" in rtn and rtn["errors"][0]["error_code"] == 410:
+            if "errorCode" in rtn and int(rtn["errorCode"]) == 410:
                 # If this error was trapped user probably tried to get an invalid fingerprint list.
                 yield StatusMessage(
-                    "Got a 410 error while attempting to get a fingerprint list for fingerprint name '{0}' "
-                    "because of a possible invalid or deleted id.".format(sep_fingerprintlist_name))
+                    "Got a 410 error while attempting to get a fingerprint list for fingerprint name '{0}' and "
+                    "domain id '{1}' because of a possible invalid or deleted id.".format(sep_fingerprintlist_name,
+                                                                                          sep_domainid))
             else:
-                yield StatusMessage("Returning 'Symantec SEP Get File Fingerprint List' results for fingerprint name '{}'."
-                                    .format(sep_fingerprintlist_name))
+                yield StatusMessage("Returning 'Symantec SEP Get File Fingerprint List' results for fingerprint name "
+                                    "'{0}' and domain id '{1}' .".format(sep_fingerprintlist_name, sep_domainid))
 
             LOG.debug(json.dumps(results["content"]))
 
