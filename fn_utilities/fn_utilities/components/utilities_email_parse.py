67548e5b324c24967fbd65be104c531c50c652a5
Shane Curtin <shane.curtin@ie.ibm.com>
2019-07-11 11:53:53 +0100

    INT-1476: Add utilities_parse_email_attachments input - If its set to True, found email attachements are added as Artifacts

--- fn_utilities/fn_utilities/components/utilities_email_parse.py
+++ fn_utilities/fn_utilities/components/utilities_email_parse.py
@@ -5,15 +5,15 @@
 """Function implementation"""
 
 import logging
-import sys
 import os
-import time
-import email
 import json
 import base64
 import mailparser
 from resilient_circuits import ResilientComponent, function, StatusMessage, FunctionResult, FunctionError
 from resilient_lib import ResultPayload, get_file_attachment_metadata, get_file_attachment, get_all_function_inputs, write_to_tmp_file, remove_dir
+
+EMAIL_ATTACHMENT_ARTIFACT_ID = 7
+ARTIFACT_URI = "/incidents/{0}/artifacts/files"
 
 
 class FunctionComponent(ResilientComponent):
@@ -21,7 +21,9 @@ class FunctionComponent(ResilientComponent):
 
     @function("utilities_email_parse")
     def _email_parse_function(self, event, *args, **kwargs):
-        """Function: Extract message headers and body parts from an EML email message."""
+        """Function: Extract message headers and body parts from an email message (.eml or .msg).
+        Any attachments found are added to the Incident as Artifacts if 'utilities_parse_email_attachments' is set to True"""
+
         try:
             log = logging.getLogger(__name__)
 
@@ -101,8 +103,35 @@ class FunctionComponent(ResilientComponent):
                     parsed_email_dict = json.loads(parsed_email.mail_json, encoding="utf-8")
                     parsed_email_dict["plain_body"] = parsed_email.text_plain_json
                     parsed_email_dict["html_body"] = parsed_email.text_html_json
-                    results = rp.done(True, parsed_email_dict)
                     yield StatusMessage("Email parsed")
+
+                    # If the input 'utilities_parse_email_attachments' is true and some attachments were found
+                    if fn_inputs.get("utilities_parse_email_attachments") and parsed_email_dict.get("attachments"):
+
+                        yield StatusMessage("Attachments found in email message")
+                        attachments_found = parsed_email_dict.get("attachments")
+
+                        # Loop attachments found
+                        for attachment in attachments_found:
+
+                            yield StatusMessage(u"Attempting to add {0} to Incident: {1}".format(attachment.get("filename"), fn_inputs.get("incident_id")))
+
+                            # Write the attachment.payload to a temp file
+                            path_tmp_file, path_tmp_dir = write_to_tmp_file(data=attachment.get("payload"),
+                                                                            tmp_file_name=attachment.get("filename"),
+                                                                            path_tmp_dir=path_tmp_dir)
+
+                            artifact_description = u"This email attachment was found in the parsed email message from: '{0}'".format(u"provided base64content" if fn_inputs.get("base64content") else attachment_metadata.get("name"))
+
+                            # POST the artifact to Resilient as an 'Email Attachment' Artifact
+                            res_client.post_artifact_file(uri=ARTIFACT_URI.format(fn_inputs.get("incident_id")),
+                                                          artifact_type=EMAIL_ATTACHMENT_ARTIFACT_ID,
+                                                          artifact_filepath=path_tmp_file,
+                                                          description=artifact_description,
+                                                          value=attachment.get("filename"),
+                                                          mimetype=attachment.get("mail_content_type"))
+
+                    results = rp.done(True, parsed_email_dict)
 
             else:
                 reason = u"Raw email in unsupported format. Failed to parse {0}".format(u"provided base64content" if fn_inputs.get("base64content") else attachment_metadata.get("name"))
