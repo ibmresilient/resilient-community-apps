c0a17f325749c6d955ce6dc611c10b674ba7e88b
Ryan Gordon <Ryan.Gordon1@ibm.com>
2019-07-23 09:26:28 +0100

    INT-1511 New Algorithm for running tests
    Changed the way we review which is the most recently modified integration.
    
    Now we check the gitdiff
    + Gather all changed files which contain fn_ or rc_ in the name
    + Remove duplicates
    + For each integration we then run the tox tests

--- .scripts/run_most_recent_tests.sh
+++ .scripts/run_most_recent_tests.sh
@@ -1,26 +1,43 @@
 #!/bin/bash
 
-# Run tests for the most recently modified package
-most_recent_package=$(ls -td `find ./* -maxdepth 1 -type f -name 'setup.py'` | head -1)
-echo "Most Recent package shows as : ${most_recent_package}";
-toxfile="$(dirname $most_recent_package)/tox.ini"
+# Declare an array that will hold the fn_ or rc_ packages 
+packages_that_have_been_changed=()
 
-# Run the tests if current TOXENV is applicable for this tox.ini file
-valid_envs=`env -u TOXENV tox -c $toxfile --listenvs;`
-
-echo "Environments supported by toxfile: $toxfile $valid_envs" 
-if [[ "$valid_envs" =~ "$TOXENV" ]]
-then
-    # Run tox with dummy details which will serve as the user when running tests 
-    tox -c $toxfile -- --resilient_email 'integrations@example.org' --resilient_password 'supersecret' --resilient_host 'example.com' --resilient_org 'TestOrg' -m "not livetest" tests;
-    last_status=$?;
-    if [ $last_status -ne 0 ]; then
-        printf 'FAILURE %s: [%d]\n' $toxfile $last_status;
-        status=$last_status;
+# For every file in the diff 
+for file in $(git diff --name-only HEAD~0 HEAD~1); 
+do 
+    # If the file contains either fn_ or rc_ in the path 
+    if [[ $file =~ (fn_|rc_)+ ]]; 
+    then 
+    # Strip everything except the first directory in the path (integration name) and append to an array
+    packages_that_have_been_changed+=($(echo "$file" | awk -F "/" '{print $2}')); 
     fi
-else
-    printf 'Skipping %s because TOXENV %s incompatible\n' "$toxfile" "$TOXENV"
-fi
+done
+
+# Make a new array which acts as a Set to gather only unique package names 
+INTEGRATIONS=($(for v in "${packages_that_have_been_changed[@]}"; do echo "$v";done| sort| uniq| xargs));
+echo "Most recently modified integrations from last commit show as : ${INTEGRATIONS}"
+
+for integration in ${INTEGRATIONS[@]};
+do 
+    echo "Running tox tests for this package: $integration" 
+    toxfile=${integration}"/tox.ini"
+    # Run the tests if current TOXENV is applicable for this tox.ini file
+    valid_envs=`env -u TOXENV tox -c $toxfile --listenvs;`
+    echo "Environments supported by toxfile: $toxfile $valid_envs" 
+    if [[ "$valid_envs" =~ "$TOXENV" ]]
+    then
+        # Run tox with dummy details which will serve as the user when running tests 
+        tox -c $toxfile -- --resilient_email 'integrations@example.org' --resilient_password 'supersecret' --resilient_host 'example.com' --resilient_org 'TestOrg' -m "not livetest" tests;
+        last_status=$?;
+        if [ $last_status -ne 0 ]; then
+            printf 'FAILURE %s: [%d]\n' $toxfile $last_status;
+            status=$last_status;
+        fi
+    else
+        printf 'Skipping %s because TOXENV %s incompatible\n' "$toxfile" "$TOXENV"
+    fi
+done
 printf 'Test Run Complete.  Final Status [%d]\n' $status;
 exit $status
 
