f85bbae5a6df5275fec67f9967124ba0ddb8d5a1
Mark R Scherfling <Mark.Scherfling@ibm.com>
2019-07-30 17:07:55 -0400

    Update setup.py
    INT-1609 - version should be 1.1.0

--- rc-data-feed/setup.py
+++ rc-data-feed/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rc_data_feed',
-    version='1.0.1',
+    version='1.1.0',
     license='MIT',
     author='IBM Resilient Labs',
     author_email='resil.labs@gmail.com',
