72df8d1858012b6fa16e6a0b667da17c2b70eda9
Ryan Gordon <Ryan.Gordon1@ibm.com>
2019-08-12 10:41:20 +0100

    INT-686 Gathering Config options function and initial incident_list

--- fn_symantec_dlp/fn_symantec_dlp/lib/dlp_soap_client.py
+++ fn_symantec_dlp/fn_symantec_dlp/lib/dlp_soap_client.py
@@ -2,6 +2,8 @@
 # pragma pylint: disable=unused-argument, no-self-use
 
 
+import datetime
+
 
 class DLPSoapClient():
     class_vars_loaded = False
@@ -29,4 +31,56 @@ class DLPSoapClient():
         setting up a client with Symantec DLP. 
         :type app_configs: dict
         """
+
+        cls.host = cls.get_config_option(app_configs=app_configs,
+                                            option_name="sdlp_host",
+                                            optional=False)
+
+
+
+    @staticmethod
+    def get_config_option(app_configs, option_name, optional=False, placeholder=None):
+        """get_config_option Given option_name, checks if it is in appconfig.
+        Raises ValueError if a mandatory option is missing
+
+        :param app_configs: a dictionary containing key-value pairs used for
+        setting up a client with Symantec DLP.
+        :type app_configs: dict
+        :param option_name: the name of the option to get
+        :type option_name: string
+        :param optional: defaults to False
+        :type optional: bool, optional
+        :param placeholder: defaults to None
+        :type placeholder: optional
+        :return: returns the specified app.config if found
+        """
+        option = app_configs.get(option_name)
+        err = "'{0}' is mandatory and is not set in app.config file. You must set this value to run this function".format(
+            option_name)
+
+        if not option and optional is False:
+            raise ValueError(err)
+        elif optional is False and placeholder is not None and option == placeholder:
+            raise ValueError(err)
+        else:
+            return option
+
+    @classmethod
+    def incident_list(cls, savedReportId=0, incidentCreationDateLaterThan=datetime.datetime.now()):
+        """incident_list API Call to gather a list of incidents from a saved report.
+
+        :param savedReportId: the ID of a saved report,
+        if not provided no incidents will be found, defaults to 0
+        :type savedReportId: int, optional
+        :param incidentCreationDateLaterThan: Only incidents that were created after the incidentCreationDateLaterThan date will be returned, defaults to datetime.datetime.now()
+        :type incidentCreationDateLaterThan: datetime, required
+        :return: A list of incidentIds and incidentLongIds
+        :rtype: [type]
+        """
+        incident_list = cls.soap_client.service.incidentList(
+            savedReportId=savedReportId,
+            incidentCreationDateLaterThan=incidentCreationDateLaterThan)
+
+        return incident_list
+
 
