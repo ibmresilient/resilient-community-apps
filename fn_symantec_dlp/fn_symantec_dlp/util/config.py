32591fbcb3596e6342fd881e24fe4bb192e8e9e9
Ryan Gordon <Ryan.Gordon1@ibm.com>
2019-08-16 12:43:47 +0100

    INT-686 Config Options

--- fn_symantec_dlp/fn_symantec_dlp/util/config.py
+++ fn_symantec_dlp/fn_symantec_dlp/util/config.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# Copyright © IBM Corporation 2010, 2019
 
 """Generate a default configuration-file section for fn_symantec_dlp"""
 
@@ -9,8 +10,11 @@ def config_section_data():
     """Produce the default configuration section for app.config,
        when called by `resilient-circuits config [-c|-u]`
     """
-#    config_data = u"""[fn_symantec_dlp]
-#setting=xxx
-#"""
-#    return config_data
-    return None
+    config_data = u"""[fn_symantec_dlp]
+    sdlp_listener_toggle=True
+    sdlp_host=https://<serverip>:<port>
+    sdlp_wsdl=https://<serverip>:<port>/ProtectManager/services/v2011/incidents?wsdl
+    sdlp_username=admin
+    sdlp_password=admin
+    """
+    return config_data
