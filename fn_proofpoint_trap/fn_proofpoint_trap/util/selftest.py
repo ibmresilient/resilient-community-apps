0d229181605e1598d89ff98efc060c762a1f62de
John Prendergast <johnpren@ie.ibm.com>
2019-08-23 15:33:26 +0100

    INT-1805 - Added selftest to function.

--- fn_proofpoint_trap/fn_proofpoint_trap/util/selftest.py
+++ fn_proofpoint_trap/fn_proofpoint_trap/util/selftest.py
@@ -5,16 +5,77 @@
    test with: resilient-circuits selftest -l fn_sep
 """
 import logging
+import requests
+import datetime
+import os
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 log.addHandler(logging.StreamHandler())
 
+def timestamp_minutes_ago(minutes):
+    """
+    Return ISO 8601 Timestamp of X minutes ago
+    :param minutes:
+    :return:
+    """
+    now = datetime.datetime.now()
+    past = now - datetime.timedelta(minutes=minutes)
+    return past.isoformat()
+
+def get_config_option(option_name, opts, optional=False):
+    """Given option_name, check if it is in appconfig.
+       Raises ValueError if it is missing and mandatory.
+    """
+    option = opts.get(option_name)
+
+    if not option and not optional:
+        err = '"{0}" is mandatory and not set in the app.config file.'.format(option_name)
+        raise ValueError(err)
+    else:
+        return option
 
 def selftest_function(opts):
     """
-    Placeholder for selftest function. An example use would be to test package api connectivity.
-    Suggested return values are be unimplemented, success, or failure.
+    Simple test to verify ProofPoint Trap connectivity.
     """
     options = opts.get("fn_proofpoint_trap", {})
-    return {"state": "unimplemented"}
+    base_url = get_config_option('base_url', options)
+    api_key = get_config_option('api_key', options)
+    headers = {'Authorization': api_key}
+
+    cafile = options.get('cafile')
+    bundle = os.path.expanduser(cafile) if cafile else False
+
+    lastupdate = 60
+
+    url = '{0}/incidents'.format(base_url)
+
+    params = {
+        "created_after": timestamp_minutes_ago(lastupdate)
+    }
+
+    try:
+        res = requests.get(
+            url,
+            headers=headers,
+            params=params,
+            verify=bundle
+        )
+        res = requests.get(url, headers=headers, params=params, verify=bundle)
+        res.raise_for_status()
+
+        if res.status_code == 200:
+            return {'state': 'success'}
+
+        return {
+            'state': 'failure',
+            'reason': 'status code {0}'.format(res.status_code)
+        }
+
+    except Exception as ex:
+        log.error(ex)
+        return {
+            'state': 'failure',
+            'reason': ex
+        }
