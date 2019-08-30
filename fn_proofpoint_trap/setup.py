dba7d4132cc508754629c59bd6bd94df88b10270
John Prendergast <johnpren@ie.ibm.com>
2019-08-30 16:44:16 +0100

    INT-1801 - Updated setup for get list members function.

--- fn_proofpoint_trap/setup.py
+++ fn_proofpoint_trap/setup.py
@@ -2,6 +2,20 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+import glob
+import ntpath
+
+def get_module_name(module_path):
+    """
+    Return the module name of the module path
+    """
+    return ntpath.split(module_path)[1].split(".")[0]
+
+def snake_to_camel(word):
+    """
+    Convert a word from snake_case to CamelCase
+    """
+    return ''.join(x.capitalize() or '_' for x in word.split('_'))
 
 setup(
     name='fn_proofpoint_trap',
@@ -9,11 +23,13 @@ setup(
     license='MIT',
     author='Resilient Labs',
     author_email='resil.labs@gmail.com',
+    url='https://ibm.biz/resilientcommunity',
     description="Resilient integration for Proofpoint TRAP",
     long_description="Resilient integration for Proofpoint TRAP. Pulls information data from Proofpoint TRAP",
     install_requires=[
         'resilient_circuits>=30.0.0',
-        'resilient>=30.0.0'
+        'resilient>=30.0.0',
+        'resilient_lib>=32.0.0'
     ],
     packages=find_packages(),
     include_package_data=True,
@@ -24,7 +40,8 @@ setup(
     entry_points={
         "resilient.circuits.components": [
             "FnProofpointTrapGetIncidentDetailsFunctionComponent = fn_proofpoint_trap.components.fn_proofpoint_trap_get_incident_details:FunctionComponent",
-            "FnPptrIncidentPolling = fn_proofpoint_trap.components.fn_proofpoint_trap_polling:PPTRIncidentPolling",
+            "FnProofpointTrapGetListMembersFunctionComponent = fn_proofpoint_trap.components.fn_proofpoint_trap_get_list_members:FunctionComponent",
+            "FnPptrIncidentPolling = fn_proofpoint_trap.components.fn_proofpoint_trap_polling:PPTRIncidentPolling"
         ],
         "resilient.circuits.configsection": ["gen_config = fn_proofpoint_trap.util.config:config_section_data"],
         "resilient.circuits.customize": ["customize = fn_proofpoint_trap.util.customize:customization_data"],
