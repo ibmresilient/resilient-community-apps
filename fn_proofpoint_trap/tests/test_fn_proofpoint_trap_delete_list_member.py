29307064d99a4260bce8183cd0af71ae852b2e39
John Prendergast <johnpren@ie.ibm.com>
2019-09-26 13:32:59 +0100

    INT-1803 - Add basic tests for delete member from list Function.

--- fn_proofpoint_trap/tests/test_fn_proofpoint_trap_delete_list_member.py
+++ fn_proofpoint_trap/tests/test_fn_proofpoint_trap_delete_list_member.py
@@ -1,20 +1,35 @@
 # -*- coding: utf-8 -*-
+# (c) Copyright IBM Corp. 2010, 2019. All Rights Reserved.
+# pragma pylint: disable=unused-argument, no-self-use
 """Tests using pytest_resilient_circuits"""
 
 from __future__ import print_function
 import pytest
+from mock import patch
+
 from resilient_circuits.util import get_config_data, get_function_definition
 from resilient_circuits import SubmitTestFunction, FunctionResult
+from mock_artifacts import mocked_pptr_client, get_mock_config
 
 PACKAGE_NAME = "fn_proofpoint_trap"
 FUNCTION_NAME = "fn_proofpoint_trap_delete_list_member"
 
-# Read the default configuration-data section from the package
-config_data = get_config_data(PACKAGE_NAME)
+# Read the mocked configuration-data section.
+config_data = get_mock_config()
 
 # Provide a simulation of the Resilient REST API (uncomment to connect to a real appliance)
 resilient_mock = "pytest_resilient_circuits.BasicResilientMock"
 
+def assert_keys_in(json_obj, *keys):
+    for key in keys:
+        assert key in json_obj
+
+def assert_values(json_obj, expected_results):
+    for key in json_obj:
+        assert json_obj[key] == expected_results[key]
+        if type(json_obj[key]) == dict:
+            for subkey in json_obj[key]:
+                assert json_obj[key][subkey] == expected_results[key][subkey]
 
 def call_fn_proofpoint_trap_delete_list_member_function(circuits, function_params, timeout=10):
     # Fire a message to the function
@@ -35,15 +50,19 @@ class TestFnProofpointTrapDeleteListMember:
         func = get_function_definition(PACKAGE_NAME, FUNCTION_NAME)
         assert func is not None
 
+    @patch('fn_proofpoint_trap.components.fn_proofpoint_trap_delete_list_member.PPTRClient', side_effect=mocked_pptr_client)
     @pytest.mark.parametrize("trap_list_id, trap_member_id, expected_results", [
-        (123, 123, {"value": "xyz"}),
-        (123, 123, {"value": "xyz"})
+        (1, 8, "OK")
     ])
-    def test_success(self, circuits_app, trap_list_id, trap_member_id, expected_results):
+    def test_success(self, mock_delete, circuits_app, trap_list_id, trap_member_id, expected_results):
         """ Test calling with sample values for the parameters """
-        function_params = { 
+        keys = ["content", "inputs", "metrics", "raw", "reason", "success", "version"]
+
+        function_params = {
             "trap_list_id": trap_list_id,
             "trap_member_id": trap_member_id
         }
         results = call_fn_proofpoint_trap_delete_list_member_function(circuits_app, function_params)
-        assert(expected_results == results)
+        assert_keys_in(results, *keys)
+        content = results["content"]
+        assert (expected_results == content)
