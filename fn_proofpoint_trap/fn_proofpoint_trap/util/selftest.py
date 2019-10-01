a40147cde9d264c4631a86fabc49b8bd4526e00d
John Prendergast <johnpren@ie.ibm.com>
2019-10-01 17:36:21 +0100

    INT-1907 - Simplified selftest.

--- fn_proofpoint_trap/fn_proofpoint_trap/util/selftest.py
+++ fn_proofpoint_trap/fn_proofpoint_trap/util/selftest.py
@@ -5,9 +5,8 @@
    test with: resilient-circuits selftest -l fn_sep
 """
 import logging
-import requests
 import datetime
-import os
+from fn_proofpoint_trap.lib.pptr_client import PPTRClient
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
@@ -23,59 +22,20 @@ def timestamp_minutes_ago(minutes):
     past = now - datetime.timedelta(minutes=minutes)
     return past.isoformat()
 
-def get_config_option(option_name, opts, optional=False):
-    """Given option_name, check if it is in appconfig.
-       Raises ValueError if it is missing and mandatory.
-    """
-    option = opts.get(option_name)
-
-    if not option and not optional:
-        err = '"{0}" is mandatory and not set in the app.config file.'.format(option_name)
-        raise ValueError(err)
-    else:
-        return option
-
 def selftest_function(opts):
     """
     Simple test to verify ProofPoint Trap connectivity.
     """
     options = opts.get("fn_proofpoint_trap", {})
-    base_url = get_config_option('base_url', options)
-    api_key = get_config_option('api_key', options)
-    headers = {'Authorization': api_key}
-
-    cafile = options.get('cafile')
-    bundle = os.path.expanduser(cafile) if cafile else False
-
     lastupdate = 60
-
-    url = '{0}/incidents'.format(base_url)
-
-    params = {
-        "created_after": timestamp_minutes_ago(lastupdate)
-    }
-
     try:
-        res = requests.get(
-            url,
-            headers=headers,
-            params=params,
-            verify=bundle
-        )
-        res = requests.get(url, headers=headers, params=params, verify=bundle)
-        res.raise_for_status()
+        pptr = PPTRClient(opts, options)
+        r = pptr.get_incidents(lastupdate)
 
-        if res.status_code == 200:
-            return {'state': 'success'}
+        if isinstance(r, list):
+            return {"state": "success"}
+        else:
+            return {"state": "failure"}
 
-        return {
-            'state': 'failure',
-            'reason': 'status code {0}'.format(res.status_code)
-        }
-
-    except Exception as ex:
-        log.error(ex)
-        return {
-            'state': 'failure',
-            'reason': ex
-        }
+    except Exception as e:
+        return {"state": "failure", "status_code": e}
