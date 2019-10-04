abcf07c914c5f8cae3f3317212e9b33c59b9e6b8
John Prendergast <johnpren@ie.ibm.com>
2019-10-04 12:36:03 +0100

    INT-1675 - Added Unit test for function get_incident_details.

--- fn_proofpoint_trap/tests/test_fn_proofpoint_trap_get_incident_details.py
+++ fn_proofpoint_trap/tests/test_fn_proofpoint_trap_get_incident_details.py
@@ -1,19 +1,32 @@
 # -*- coding: utf-8 -*-
+# (c) Copyright IBM Corp. 2010, 2019. All Rights Reserved.
+# pragma pylint: disable=unused-argument, no-self-use
 """Tests using pytest_resilient_circuits"""
 
 from __future__ import print_function
 import pytest
+from mock import patch
+
 from resilient_circuits.util import get_config_data, get_function_definition
 from resilient_circuits import SubmitTestFunction, FunctionResult
+from mock_artifacts import mocked_pptr_client, get_mock_config
 
 PACKAGE_NAME = "fn_proofpoint_trap"
 FUNCTION_NAME = "fn_proofpoint_trap_get_incident_details"
 
-# Read the default configuration-data section from the package
-config_data = get_config_data(PACKAGE_NAME)
+# Read the mocked configuration-data section.
+config_data = get_mock_config()
 
 # Provide a simulation of the Resilient REST API (uncomment to connect to a real appliance)
 resilient_mock = "pytest_resilient_circuits.BasicResilientMock"
+
+def assert_keys_in(json_obj, *keys):
+    for key in keys:
+        assert key in json_obj
+
+def assert_value_not_none(json_obj, *keys):
+    for key in keys:
+        assert json_obj[key] is not None
 
 
 def call_fn_proofpoint_trap_get_incident_details_function(circuits, function_params, timeout=10):
@@ -35,14 +48,19 @@ class TestFnProofpointTrapGetIncidentDetails:
         func = get_function_definition(PACKAGE_NAME, FUNCTION_NAME)
         assert func is not None
 
+    @patch('fn_proofpoint_trap.components.fn_proofpoint_trap_get_incident_details.PPTRClient', side_effect=mocked_pptr_client)
     @pytest.mark.parametrize("trap_incident_id, expected_results", [
-        (123, {"value": "xyz"}),
-        (123, {"value": "xyz"})
+        (123, None)
     ])
-    def test_success(self, circuits_app, trap_incident_id, expected_results):
+    def test_success(self, mock_get, circuits_app, trap_incident_id, expected_results):
         """ Test calling with sample values for the parameters """
-        function_params = { 
+
+        keys = ["content", "data", "href", "inputs", "metrics", "raw", "reason", "success", "version"]
+        keys_data = ["assignee", "created_at", "description", "hosts", "event_count", "events", "score", "type"]
+        function_params = {
             "trap_incident_id": trap_incident_id
         }
         results = call_fn_proofpoint_trap_get_incident_details_function(circuits_app, function_params)
-        assert(expected_results == results)
+        assert_keys_in(results, *keys)
+        data = results["data"]
+        assert_keys_in(data, *keys_data)
