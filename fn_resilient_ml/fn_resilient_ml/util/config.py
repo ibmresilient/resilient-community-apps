f4d540803d094a1ae9d37137e58caa12c6fb69b6
yongjian feng <yj@debian64Bit.debian64.irsys.com>
2019-10-23 13:51:22 -0400

    INT-1975: change config.py

--- fn_resilient_ml/fn_resilient_ml/util/config.py
+++ fn_resilient_ml/fn_resilient_ml/util/config.py
@@ -9,8 +9,7 @@ def config_section_data():
     """Produce the default configuration section for app.config,
        when called by `resilient-circuits config [-c|-u]`
     """
-#    config_data = u"""[fn_resilient_ml]
-#setting=xxx
-#"""
-#    return config_data
-    return None
+    config_data = u"""[fn_resilient_ml]
+num_features=50
+"""
+    return config_data
