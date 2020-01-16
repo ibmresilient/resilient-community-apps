1e12a664bbcbe7723a2aa91d07c29d9c8702109a
yongjian feng <yj@debian64Bit.debian64.irsys.com>
2020-01-16 16:31:45 -0500

    One more rule for searching incidents using an input string.

--- fn_resilient_ml/scripts/nlp_codegen.sh
+++ fn_resilient_ml/scripts/nlp_codegen.sh
@@ -3,6 +3,6 @@ resilient-circuits codegen \
 -f resilient_nlp_search \
 -m resilient_ml \
 --workflow resilient_nlp_search \
---rule "NLP Search" \
+--rule "NLP Search" "NLP String Search" \
 --datatable nlp_similar_incidents \
 --exportfile export.res
