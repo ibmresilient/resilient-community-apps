becd0bc92837a1138c7346a9ec96000bf7f88229
Brian D Reid <Brian.Reid@ibm.com>
2020-01-29 12:07:52 -0500

    Update setup.py to require resilient_lib
    resilient_lib is imported in greynoise_ip_query.py

--- fn_greynoise/setup.py
+++ fn_greynoise/setup.py
@@ -27,7 +27,8 @@ setup(
     description="Resilient Circuits Components for 'fn_greynoise'",
     long_description="Collect analysis on IP Addresses using Greynoise",
     install_requires=[
-        'resilient_circuits>=30.0.0'
+        'resilient_circuits>=30.0.0',
+        'resilient_lib'
     ],
     packages=find_packages(),
     include_package_data=True,
