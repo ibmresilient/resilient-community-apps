998ea6f47586938c551acf875d55d472d7a1b759
Brian D Reid <Brian.Reid@ibm.com>
2020-01-30 14:42:19 -0500

    require resilient_lib in fn_elasticsearch
    resilient_lib is imported in the main component, but not required in setup.py for installation

--- fn_elasticsearch/setup.py
+++ fn_elasticsearch/setup.py
@@ -14,7 +14,8 @@ setup(
     long_description="Allows users of the Resilient Platform to connect to and query an ElasticSearch Database. Users can specify the location of a remote ElasticSearch instance and query this instance for data through the Resilient Platform ",
     install_requires=[
         'resilient_circuits>=31.0.0',
-        'elasticsearch>=6.3.0'
+        'elasticsearch>=6.3.0',
+        'resilient_lib>=32.0.0'
     ],
     packages=find_packages(),
     include_package_data=True,
