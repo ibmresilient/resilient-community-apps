5a7afe7763998269b3ed8ce6e84de10b4f9eb7d8
Carol Namkoong <cnamkoong@carols-mbp.cambridge.ibm.com>
2020-02-12 16:53:58 -0500

    new function

--- fn_pulsedive/fn_pulsedive/components/funct_pulsedive_search.py
+++ fn_pulsedive/fn_pulsedive/components/funct_pulsedive_search.py
@@ -2,14 +2,13 @@
 # (c) Copyright IBM Corp. 2018. All Rights Reserved.
 # pragma pylint: disable=unused-argument, no-self-use
 """Function implementation"""
-import urllib.parse
+
 import logging
 from io import BytesIO
-import pprint
 from resilient_circuits import \
     (ResilientComponent, function, handler,
      StatusMessage, FunctionResult, FunctionError)
-from resilient_lib import validate_fields, RequestsCommon, write_file_attachment, ResultPayload
+from resilient_lib import validate_fields, RequestsCommon, write_file_attachment
 
 CONFIG_SECTION = "fn_pulsedive"
 
@@ -32,143 +31,211 @@ class FunctionComponent(ResilientComponent):
         """Configuration options have changed, save new values"""
         self._init_function(opts)
 
+    def _get_indicator_mapping(self, **kwargs):
+        """
+        Retrieve function inputs and create url parameters to search Indicators
+        :param kwargs:
+            value: filter by string. Can be comma-separated list. Blank returns all.
+                field type: text
+            type: filter by type of indicator. Defaults to all.
+                field type: multiselect: "Artifact", "Domain", "IP", "IPv6", "URL"
+            risk: filter by risk level. Ignore "Retired" as per PD dev suggestion. Defaults to all.
+                field type: multiselect: "Unknown", "None", "Low", "Medium", "High", "Critical"
+            attribute: filter by attributes, eg protocols or ports. Can be comma-separated list.
+                field type: text
+            property: filter by properties, eg content type. Can be comma-separated list.
+                field type: text
+            threat: filter by threat name. Can be comma-separated list.
+                field type: text
+            feed: filter by feed name. Can be comma-separated list.
+                field type: text
+            limit: option to limit number of indicators returned.
+                field type: select, values: "100 results", "1000 results", "10000 results"
+            pretty: option to pretty-print results (will be written to attachment)
+                field type: select, values: "Yes", "No"
+            lastseen: option to set scope of data
+                field type: select, values: "day", "week", "month"
+            (don't use this filter:
+                export: option to export the Indicator search results to CSV
+                Just add the link automatically)
+            TODO:
+            latest = usage unclear. Ignore for now.
+        :return: dict of parameters
+        """
+
+        # convert the # of returns
+        temp_limit = self.get_select_param(kwargs.get("pulsedive_limit"))
+        limit_options = {"100 results": "hundred",
+                         "1000 results": "thousand",
+                         "10000 results": "tenthousand"}
+
+        # convert time frame of returns
+        temp_time = self.get_select_param(kwargs.get("pulsedive_lastseen"))
+        time_options = {"Last 24 hours": "day", "Last week": "week", "Last month": "month"}
+
+        # map action input fields to Pulsedive Indicator filters
+        params_map = {
+            "value[]": kwargs.get("pulsedive_search_value"),
+            "type[]": self.get_select_param(kwargs.get("pulsedive_indicator_type")),
+            "risk[]": self.get_select_param(kwargs.get("pulsedive_risk")),
+            "attribute[]": kwargs.get("pulsedive_attribute"),
+            "property[]": kwargs.get("pulsedive_property"),
+            "threat[]": kwargs.get("pulsedive_threat"),
+            "feed[]": kwargs.get("pulsedive_feed"),
+            "limit": limit_options.get(temp_limit, "hundred"),
+            "pretty": self.get_select_param(kwargs.get("pulsedive_pretty")),
+            "lastseen": time_options.get(temp_time, ""),
+            # "export": self.get_select_param(kwargs.get("pulsedive_export"))
+        }
+        return params_map
+
+    def _get_threat_mapping(self, **kwargs):
+        """
+        Retrieve function inputs and create url parameters to search Threats
+        :param kwargs:
+            value: filter by string. Can be comma-separated list. Blank returns all.
+                field type: text
+            category: filter by 1 or more categories. Defaults to all categories.
+                field type: multiselect, values: "General", "Abuse", "APT", "Attack", "Botnet",
+                    "Crime", "Exploit Kit", "Fraud", "Group", "Malware", "Proxy", "PUP",
+                    "RAT", "Reconnaissance", "Spam", "Phishing", "Terrorism", "Vulnerability"
+            attribute: filter by attributes, eg protocol ir ports. Can be comma-separated list.
+                field type: text
+            property: filter by properties, eg content type. Can be comma-separated list.
+                field type: text
+            risk: filter by risk level. Ignore "Retired" as per PD dev suggestion. Defaults to all.
+                field type: multiselect: "Unknown", "None", "Low", "Medium", "High", "Critical"
+            splitrisk: option to split out indicator counts by risk
+                field type: select, values: "Yes", "No"
+            pretty: option to pretty-print results (will be written to attachment)
+                field type: select, values: "Yes", "No"
+            TODO:
+            latest = usage unclear. Ignore for now.
+        :return: dict of parameters
+        """
+
+        # map action input fields to Pulsedive Indicator filters
+        params_map = {
+            "search": "threat",
+            "value[]": kwargs.get("pulsedive_search_value"),
+            "category[]": self.get_select_param(kwargs.get("pulsedive_category")),
+            "attribute[]": kwargs.get("pulsedive_attribute"),
+            "property[]": kwargs.get("pulsedive_property"),
+            "risk[]": self.get_select_param(kwargs.get("pulsedive_risk")),
+            "splitrisk": self.get_select_param(kwargs.get("pulsedive_splitrisk")),
+            "pretty": self.get_select_param(kwargs.get("pulsedive_pretty"))
+        }
+        return params_map
+
+    def _get_feed_mapping(self, **kwargs):
+        """
+        Retrieve function inputs and create url parameters to search Feeds
+        :param kwargs:
+            value: filter by string. Can be comma-separated list. Blank returns all.
+                field type: text
+            category: filter by 1 or more categories. Defaults to all categories.
+                field type: multiselect, values: "General", "Abuse", "APT", "Attack", "Botnet",
+                    "Crime", "Exploit Kit", "Fraud", "Group", "Malware", "Proxy", "PUP",
+                    "RAT", "Reconnaissance", "Spam", "Phishing", "Terrorism", "Vulnerability"
+            splitrisk: option to split out indicator counts by risk
+                field type: select, values: "Yes", "No"
+            pretty: option to pretty-print results (will be written to attachment)
+                field type: select, values: "Yes", "No"
+        :return: dict of parameters
+        """
+
+        # map action input fields to Pulsedive Indicator filters
+        params_map = {
+            "search": "feed",
+            "value[]": kwargs.get("pulsedive_search_value"),
+            "category[]": self.get_select_param(kwargs.get("pulsedive_category")),
+            "splitrisk": self.get_select_param(kwargs.get("pulsedive_splitrisk")),
+            "pretty": self.get_select_param(kwargs.get("pulsedive_pretty"))
+        }
+        return params_map
+
+
     @function("pulsedive_search")
-    def _pulsedive_search_function(self, event, *args, **kwargs):
-        """Function: Search Pulsedive for Indicators, Threats, or Feeds, using type-related filters."""
+    def _pulsedive_search_function(self, event, **kwargs):
+        """
+        Function: Search Pulsedive for Indicators, Threats, or Feeds, using type-related filters.
+        This function gets input values from the action rule activity fields and
+        sends them to Pulsedive's Search endpoint https://pulsedive.com/api/?q=search.
+        Results: a summary will be written to an incident note and
+                 full details will be written to an incident attachment.
+                 An option to export Indicator (only) search to CSV is provided in a link in Note.
+        """
         try:
             log = logging.getLogger(__name__)
 
             log.info("config params: %s", self.options)
 
             # Get the function parameters:
-            log.info("CAROL kwargs: %s", kwargs)
-            incident_id = kwargs.get("incident_id")  # number
-            pulsedive_search_value = kwargs.get("pulsedive_search_value").lower()
-            pulsedive_search_type = self.get_select_param(kwargs.get("pulsedive_search_type"))  # select, values: "Indicator", "Threat", "Feed"
-            pulsedive_indicator_type = self.get_select_param(kwargs.get("pulsedive_indicator_type"))  # multiselect, values: "Artifact", "Domain", "IP", "IPv6", "URL"
-            pulsedive_risk = self.get_select_param(kwargs.get("pulsedive_risk"))  # multiselect, values: "Unknown", "None", "Low", "Medium", "High", "Critical", "Retired"
-            # pulsedive_lastseen = self.get_select_param(kwargs.get("pulsedive_lastseen"))  # select, values: "day", "week", "month"
-            temp_time = self.get_select_param(kwargs.get("pulsedive_lastseen"))
-            if temp_time == "Last 24 hours":
-                pulsedive_lastseen = "day"
-            elif temp_time == "Last week":
-                pulsedive_lastseen = "week"
-            elif temp_time == "Last month":
-                pulsedive_lastseen = "month"
+            log.info("function params: %s", kwargs)
+
+            incident_id = kwargs.get("incident_id")   # number
+
+            # what type of search is requested
+            pulsedive_search_type = self.get_select_param(
+                kwargs.get("pulsedive_search_type"))  # select: "Indicator", "Threat", "Feed"
+
+            # map action input filters to Pulsedive query filters
+            if pulsedive_search_type == "Indicator":
+                mapping = self._get_indicator_mapping(**kwargs)
+            elif pulsedive_search_type == "Threat":
+                mapping = self._get_threat_mapping(**kwargs)
             else:
-                pulsedive_lastseen = ""     # all time
+                mapping = self._get_feed_mapping(**kwargs)
 
-            # pulsedive_latest = self.get_select_param(kwargs.get("pulsedive_latest"))  # select, values: "latest", "historical"
-            pulsedive_threat = kwargs.get("pulsedive_threat")  # text
-            pulsedive_feed = kwargs.get("pulsedive_feed")  # text
-            # pulsedive_limit = self.get_select_param(kwargs.get("pulsedive_limit"))  # select, values: "100 results", "1000 results", "10000 results"
-            temp_limit = self.get_select_param(kwargs.get("pulsedive_limit"))
-            if temp_limit == "100 results":
-                pulsedive_limit = "hundred"
-            elif temp_limit == "1000 results":
-                pulsedive_limit = "thousand"
-            elif temp_limit == "10000 results":
-                pulsedive_limit = "tenthousand"
+            # eliminate empty/null vars
+            pulsedive_data = {}
+            for k, v in mapping.items():
+                if v is not None and v != "":
+                    pulsedive_data[k] = v
+            pulsedive_data["key"] = self.options["pulsedive_api_key"]
+            log.info("%s parameters: %s", pulsedive_search_type, pulsedive_data)
 
-            pulsedive_category = self.get_select_param(kwargs.get("pulsedive_category"))  # multiselect, values: "General", "Abuse", "APT", "Attack", "Botnet", "Crime", "Exploit Kit", "Fraud", "Group", "Malware", "Proxy", "PUP", "RAT", "Reconnaissance", "Spam", "Phishing", "Terrorism", "Vulnerability"
-
+            # set attachment name if user doesn't specify one
             if kwargs.get("attachment_name") is None:
                 attachment_name = u"pulsedive_search_{}.txt".format(pulsedive_search_type)
             else:
                 attachment_name = kwargs.get("attachment_name").replace(" ", "_")
-
-            log.info("incident_id: %s", incident_id)
-            log.info("pulsedive_search_value: %s", pulsedive_search_value)
-            log.info("pulsedive_search_type: %s", pulsedive_search_type)
-            log.info("pulsedive_indicator_type: %s", pulsedive_indicator_type)
-            log.info("pulsedive_risk: %s", pulsedive_risk)
-            log.info("pulsedive_lastseen: %s", pulsedive_lastseen)
-            # log.info("pulsedive_latest: %s", pulsedive_latest)
-            log.info("pulsedive_threat: %s", pulsedive_threat)
-            log.info("pulsedive_feed: %s", pulsedive_feed)
-            log.info("pulsedive_limit: %s", pulsedive_limit)
-            log.info("pulsedive_category: %s", pulsedive_category)
-            log.info('attachment_name: %s', attachment_name)
+            log.info("%s attachment name: %s", attachment_name)
 
             # PUT YOUR FUNCTION IMPLEMENTATION CODE HERE
             yield StatusMessage("starting...")
 
             # form the url request
             api_url = "{}/search.php?".format(self.options["pulsedive_api_url"])
-            pulsedive_data = {}
-            # add filters according to search type - Indicator, Threat, or Fee
-            log.info("CAROL: search type: %s", pulsedive_search_type)
-            if pulsedive_search_type == "Indicator":
-                pulsedive_data["value"] = pulsedive_search_value
-                pulsedive_data["type"] = ",".join(pulsedive_indicator_type)
-                pulsedive_data["risk"] = ",".join(pulsedive_risk)
-                pulsedive_data["threat"] = ",".join(pulsedive_threat)
-                pulsedive_data["feed"] = ",".join(pulsedive_feed)
-                pulsedive_data["limit"] = pulsedive_limit
-                pulsedive_data["lastseen"] = pulsedive_lastseen
-                # pulsedive_data["latest"] = pulsedive_latest
-                # pulsedive_data["export"] = 1  # export to csv, for Indicator search only. Doesn't work.
-            elif pulsedive_search_type == "Threat":
-                pulsedive_data = {
-                    "search": pulsedive_search_type.lower(),
-                    "value": pulsedive_search_value.lower(),
-                    "category[]": map(lambda x: x.lower(), pulsedive_category),
-                    "risk[]": map(lambda x: x.lower(), pulsedive_risk)
-                }
-                # pulsedive_data["search"] = pulsedive_search_type
-                # pulsedive_data["value"] = pulsedive_search_value
-                # pulsedive_data["category[]"] = pulsedive_category
-                # pulsedive_data["risk[]"] = pulsedive_risk
-                # pulsedive_data["category"] = pulsedive_category
-                # pulsedive_data["risk"] = pulsedive_risk
-                # pulsedive_data["category"] = ",".join(pulsedive_category)
-                # pulsedive_data["risk"] = ",".join(pulsedive_risk)
 
-            else:   # Feed
-                pulsedive_data["search"] = pulsedive_search_type
-                pulsedive_data["value"] = pulsedive_search_value
-                pulsedive_data["category"] = ",".join(pulsedive_category)
-
-            pulsedive_data["key"] = self.options["pulsedive_api_key"]
-            pulsedive_data["splitrisk"] = "1"
-
-            log.info("CAROL data: %s", pulsedive_data)
             # make the api call
             rc = RequestsCommon(self.opts, self.options)    # initialize
             resp = rc.execute_call_v2("get",
                                       url=api_url,
                                       params=pulsedive_data
-                                      # params=urllib.parse.urlencode(pulsedive_data, doseq=True)
                                       )
-            log.info("api response: %s", resp.json())
-            log.info("CAROL CODE %s", resp.status_code)
-            log.info("CAROL content %s", resp.content)
-            log.info("CAROL request %s", resp.request)
-            log.info("request url: %s", resp.request.url)
-
-            # get record counts to put in note summary
-            summary_count = len(resp.json()["results"])
-
-            # provide pretty-print format for readability in output
-            pp = pprint.PrettyPrinter(indent=4)
 
             # prepare results to send for output
             results = {
-                "url": api_url,
-                # "request_url": resp.request.url,
-                "fn_inputs": {"search_type": pulsedive_search_type,
-                              "attachment_name": attachment_name},
+                "search_type": pulsedive_search_type,
                 "request_parameters": pulsedive_data,
-                "content": resp.json(),
-                "pretty": pp.pformat(resp.json()),
-                "counts": summary_count
+                "request_url": resp.request.url,
+                "attachment_name": attachment_name,
+                "json": resp.json()
             }
 
             # Get the rest client so we can add the attachment to the incident
             client = self.rest_client()
-            # Convert string to bytes and encode
-            content = "content: {}".format(results["pretty"])
-            datastream = BytesIO(content.encode("utf-8"))
+
+            # prepare datastream to output to attachment
+            if pulsedive_data["pretty"] == "Yes":
+                # Pulsedive returns pp format if requested. Convert to bytestream for file handling.
+                datastream = BytesIO(resp.content)
+            else:
+                # Convert dict to string first, then convert to bytestream for file handling.
+                ds = "{}".format(resp.json())
+                datastream = BytesIO(ds.encode("utf-8"))
+
             # Write the file as attachment: failures will raise an exception
             write_file_attachment(client, attachment_name, datastream=datastream,
                                   incident_id=incident_id, task_id=None)
