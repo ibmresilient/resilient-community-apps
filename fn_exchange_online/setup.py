7480bbee565bf61d23862a5632d2b2ed6d1e5da7
AnnMarie Norcross <annmarie.meier.norcross@ibm.com>
2020-02-13 15:06:07 -0500

    INT-2268 update setup.py to include tzlocal

--- fn_exchange_online/setup.py
+++ fn_exchange_online/setup.py
@@ -27,6 +27,7 @@ setup(
     description="Resilient Circuits Components for 'fn_exchange_online'",
     long_description="Resilient Integration with Exchange Online provides the capability to access and manipulate Microsoft Exchange Online messages from Resilient.",
     install_requires=[
+        'tzlocal',
         'resilient_circuits>=31.0.0',
         'resilient_lib>=35.0.dev'
     ],
