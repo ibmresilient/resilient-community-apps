a4511bb182965bf11416a6a022f722139974781e
Brian Reid <brian.reid@ibm.com>
2020-02-25 15:37:05 -0500

    INT-2332: require resilient_lib

--- fn_urlhaus/setup.py
+++ fn_urlhaus/setup.py
@@ -14,7 +14,8 @@ setup(
     long_description="""Resilient Circuits Components for 'fn_urlhaus'. Supported is inqueries on ip addresses, hashes and domains
               and submissions on malware distributing urls""",
     install_requires=[
-        'resilient_circuits>=30.0.0'
+        'resilient_circuits>=30.0.0',
+        'resilient_lib'
     ],
     packages=find_packages(),
     include_package_data=True,
