c771c35c198e1f9d93e30333935054ed8573a3f3
Brian Reid <brian.reid@ibm.com>
2020-03-03 09:58:25 -0500

    INT-2336: utilize resilient-lib for requests

--- fn_urlscanio/fn_urlscanio/components/urlscanio.py
+++ fn_urlscanio/fn_urlscanio/components/urlscanio.py
@@ -6,11 +6,12 @@
 import base64
 import logging
 import json
-import requests
 import time
 from resilient_circuits import ResilientComponent, function, StatusMessage, FunctionResult, FunctionError
 from resilient_lib import write_file_attachment
+from resilient_lib.components.integration_errors import IntegrationError
 from io import BytesIO
+from resilient_lib import RequestsCommon
 
 CONFIG_DATA_SECTION = 'urlscanio'
 
@@ -22,6 +23,8 @@ class FunctionComponent(ResilientComponent):
         super(FunctionComponent, self).__init__(opts)
         self.options = opts.get(CONFIG_DATA_SECTION, {})
         self.apikey = self.options.get("urlscanio_api_key")
+        self.urlscanio_report_url = self.options.get("urlscanio_report_url")
+        self.urlscanio_screenshot_url = self.options.get("urlscanio_screenshot_url")
         self.timeout = int(self.options.get("timeout", 600))
         if self.apikey is None:
             raise Exception("No API key found - please add urlscanio_api_key under a [urlscanio] in app.config")
@@ -56,9 +59,13 @@ class FunctionComponent(ResilientComponent):
             if urlscanio_referer:
                 urlscanio_data["referer"] = urlscanio_referer
 
-            urlscanio_post = requests.post('https://urlscan.io/api/v1/scan/',
-                                           headers=urlscanio_headers,
-                                           data=json.dumps(urlscanio_data))
+            req_common = RequestsCommon(self.options, self.opts)
+
+            urlscanio_scan_url = "{}/scan/".format(self.urlscanio_report_url)
+            urlscanio_post = req_common.execute_call_v2("POST", urlscanio_scan_url, self.timeout,
+                                       headers=urlscanio_headers,
+                                       data=json.dumps(urlscanio_data))
+
             urlscanio_post.raise_for_status()
 
             # The post response contains a UUID that we use to check for the report
@@ -75,28 +82,35 @@ class FunctionComponent(ResilientComponent):
                 time.sleep(10)
                 if time.time() > start_time + self.timeout:
                     yield RuntimeError("Timeout: report was not ready after {} seconds".format(self.timeout))
-                urlscanio_get = requests.get('https://urlscan.io/api/v1/result/{}/'.format(uuid))
-                if urlscanio_get.status_code == 404:
+                urlscanio_result_url = "{}/result/{}".format(self.urlscanio_report_url, uuid)
+                try:
+                    urlscanio_get = req_common.execute_call_v2("GET", urlscanio_result_url, self.timeout)
+                    if urlscanio_get.status_code == 200:
+                        # Report is done
+                        break
+                    else:
+                        # Some other error condition
+                        urlscanio_get.raise_for_status()
+                    # requests-common will through an IntegrationError if 404 is received
+                except IntegrationError as ie:
                     # 404 means the report is not yet complete
-                    yield StatusMessage("Waiting for report...")
-                elif urlscanio_get.status_code == 200:
-                    # Report is done
-                    break
-                else:
-                    # Some other error condition
-                    urlscanio_get.raise_for_status()
+                    if ie.value[:3] == '404':
+                        yield StatusMessage("Waiting for report...")
+                    else:
+                        # Some problem other than 404
+                        raise IntegrationError
 
             yield StatusMessage("Report is ready")
 
             # get the full report json - usually a big blob
-            urlscanio_report_url = 'https://urlscan.io/api/v1/result/{}/'.format(uuid)
-            urlscanio_report_get = requests.get(urlscanio_report_url)
+            urlscanio_report_url = '{}/result/{}/'.format(self.urlscanio_report_url, uuid)
+            urlscanio_report_get = req_common.execute_call_v2("GET", urlscanio_report_url, self.timeout)
             urlscanio_report_json = urlscanio_report_get.json()
             yield StatusMessage("Downloaded report from {}".format(urlscanio_report_url))
 
             # Grab the PNG screenshot.  Return as a base64 string so it can be passed to another function as needed
-            urlscanio_png_url = 'https://urlscan.io/screenshots/{}.png'.format(uuid)
-            urlscanio_png_get = requests.get(urlscanio_png_url)
+            urlscanio_png_url = '{}/{}.png'.format(self.urlscanio_screenshot_url, uuid)
+            urlscanio_png_get = req_common.execute_call_v2("GET", urlscanio_png_url, self.timeout)
             urlscanio_png_b64 = base64.b64encode(urlscanio_png_get.content)
             yield StatusMessage("Downloaded PNG screenshot from {}".format(urlscanio_png_url))
 
