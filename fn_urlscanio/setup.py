3b136912f2eaee09dd5ebfcd2fb590a758aecba5
Brian Reid <brian.reid@ibm.com>
2020-03-03 11:38:14 -0500

    INT-2336: bump version

--- fn_urlscanio/setup.py
+++ fn_urlscanio/setup.py
@@ -19,7 +19,7 @@ def snake_to_camel(word):
 
 setup(
     name='fn_urlscanio',
-    version='1.0.1',
+    version='1.1.0',
     license='MIT',
     author='IBM Resilient',
     url='http://ibm.biz/resilientcommunity',
