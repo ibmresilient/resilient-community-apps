9f0dc39866f67c251a6e3a3e0c133a475982a0ec
Brian D Reid <Brian.Reid@ibm.com>
2020-03-04 10:25:44 -0500

    Update setup.py
    bump version after python3 changes

--- rc-cts-shadowserver/setup.py
+++ rc-cts-shadowserver/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rc-cts-shadow-server',
-    version='1.0.0',
+    version='1.0.1',
     url='https://github.com/ibmresilient/resilient-community-apps',
     license='MIT',
     author='IBM Resilient Labs',
