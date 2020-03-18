ee886e024cdad28599e3639530d3c1c55391ae27
AnnMarie Norcross <annmarie.meier.norcross@ibm.com>
2020-03-18 16:03:42 -0400

    INT-1379 implement selftest

--- fn_secureworks_ctp/fn_secureworks_ctp/util/selftest.py
+++ fn_secureworks_ctp/fn_secureworks_ctp/util/selftest.py
@@ -1,3 +1,4 @@
+# (c) Copyright IBM Corp. 2010, 2020. All Rights Reserved.
 # -*- coding: utf-8 -*-
 # pragma pylint: disable=unused-argument, no-self-use
 """Function implementation
@@ -5,16 +6,49 @@
 """
 
 import logging
+from resilient_lib import RequestsCommon, validate_fields
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 log.addHandler(logging.StreamHandler())
 
+CONFIG_DATA_SECTION='fn_secureworks_ctp'
 
 def selftest_function(opts):
-    """
-    Placeholder for selftest function. An example use would be to test package api connectivity.
-    Suggested return values are be unimplemented, success, or failure.
-    """
-    options = opts.get("fn_secureworks_ctp", {})
-    return {"state": "unimplemented"}
+
+    options = opts.get(CONFIG_DATA_SECTION, {})
+
+    validate_fields(['base_url', 'username', 'password'], options)
+
+    base_url = options.get('base_url')
+    username = options.get('username')
+    password = options.get('password')
+    cafile = options.get('cafile')
+    bundle = os.path.expanduser(cafile) if cafile else False
+    api_key = u"APIKEY {0}:{1}".format(username, password)
+    headers = {
+                'Authorization': api_key,
+                'content-type': "application/json"
+                }
+    payload = {'ticketType': 'INCIDENT'}
+
+    url = u"{0}/tickets/count".format(base_url)
+    try:
+        rc = RequestsCommon(opts, options)
+        response = rc.execute_call_v2("post", url, headers=headers, params=payload, verify=bundle,
+                                      proxies=rc.get_proxies())
+
+        if response.status_code == 200:
+            return {'state': 'success'}
+
+        reason = u"status code {0}".format(response.status_code)
+        return {
+               'state': 'failure',
+               'reason': reason
+                }
+
+    except Exception as exc:
+        return {
+               'state': 'failure',
+               'reason': exc
+               }
