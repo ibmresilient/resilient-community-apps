8880349fe25ceb9b7927e51972223653d80a8628
Priya-Sapra <priya.sapra@ibm.com>
2020-04-17 12:07:41 -0400

    remove codegen script

--- fn_machine_learning_nlp/scripts/nlp_codegen.sh
+++
@@ -1,8 +0,0 @@
-resilient-circuits codegen \
--p fn_machine_learning_nlp \
--f resilient_nlp_search \
--m resilient_ml \
---workflow resilient_nlp_search \
---rule "NLP Search" "NLP String Search" \
---datatable nlp_similar_incidents \
---exportfile export.res
