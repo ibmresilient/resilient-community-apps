527d933e587ff413473338a8501e4203903bc657
Mark-Scherfling <mark.scherfling@ibm.com>
2020-04-18 15:29:43 -0400

    INT-2400 - staging changes

--- rc-data-feed-plugin-elasticfeed/data_feeder_plugins/elasticfeed/util/config.py
+++ rc-data-feed-plugin-elasticfeed/data_feeder_plugins/elasticfeed/util/config.py
@@ -13,10 +13,21 @@ class=ElasticFeed
 url=https://your_org.com
 port=9200
 # if using multiple organizations, consider indexes such as resilient_<org_ID>
-# each document type will append to this index as elastic 6.0 onlyl supports one document type per index
+# each document type will append to this index as elastic 6.0 only supports one document type per index
 index_prefix=resilient_
 auth_user=
 auth_password=
 cafile=false
 """
     return config_data
+
+def apphost_config_section_data():
+    return u"""[feeds]
+# comma separated section names. ex. sqlserver_feed,file_feed
+feed_names=elastic_feed
+reload=False
+# set to true if ElasticSearch errors occur during reload=True
+reload_query_api_method=False
+# feed_data is the default message destination that will be listened to
+queue=feed_data
+"""
