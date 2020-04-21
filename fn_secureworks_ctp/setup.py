6ab41fe84d4fa7977c80b9b4428dafbc7b3c2cc4
AnnMarie Norcross <annmarie.meier.norcross@ibm.com>
2020-04-21 17:03:13 -0400

    INT-1378 update version to indicate pre-release

--- fn_secureworks_ctp/setup.py
+++ fn_secureworks_ctp/setup.py
@@ -20,7 +20,7 @@ def snake_to_camel(word):
 
 setup(
     name='fn_secureworks_ctp',
-    version='1.0.0',
+    version='0.9.0',
     license='MIT',
     author_email='',
     url='https://ibm.com/mysupport',
