5273a907cfd74dca3f6890e5e5f13a4e5c14c125
Mark-Scherfling <mark.scherfling@ibm.com>
2020-04-24 16:28:20 -0400

    INT-2400 - version for fn_qradar_integrations

--- fn_qradar_integration/setup.py
+++ fn_qradar_integration/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fn_qradar_integration',
-    version='2.0.4',
+    version='2.0.5',
     license='MIT License',
     author='IBM Resilient',
     author_email='support@resilientsystems.com',
