e17b6b0aaa29b7e21777d25235be2b4685a2b1ef
Mark-Scherfling <mark.scherfling@ibm.com>
2020-04-30 08:15:09 -0400

    INT-2173 - added docker files

--- fn_splunk_integration/setup.py
+++ fn_splunk_integration/setup.py
@@ -29,6 +29,7 @@ setup(
             "SplunkAddIntelItemFunctionComponent = fn_splunk_integration.components.splunk_add_intel_item:FunctionComponent"
         ],
         "resilient.circuits.configsection": ["gen_config = fn_splunk_integration.util.config:config_section_data"],
-        "resilient.circuits.customize": ["customize = fn_splunk_integration.util.customize:customization_data"]
+        "resilient.circuits.customize": ["customize = fn_splunk_integration.util.customize:customization_data"],
+        "resilient.circuits.selftest": ["selftest = fn_splunk_integration.util.selftest:selftest_function"]
     }
 )
