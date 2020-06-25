06ffdeaaacb42c7453bf6cd8ebceba6d3b655cef
Mark-Scherfling <mark.scherfling@ibm.com>
2020-06-25 09:13:13 -0400

    INT-2688 - generalize setup.py entry point logic

--- fn_ansible/setup.py
+++ fn_ansible/setup.py
@@ -3,6 +3,20 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+import glob
+import ntpath
+
+def get_module_name(module_path):
+    """
+    Return the module name of the module path
+    """
+    return ntpath.split(module_path)[1].split(".")[0]
+
+def snake_to_camel(word):
+    """
+    Convert a word from snake_case to CamelCase
+    """
+    return ''.join(x.capitalize() or '_' for x in word.split('_'))
 
 setup(
     name='fn_ansible',
@@ -27,7 +41,10 @@ setup(
     ],
     entry_points={
         "resilient.circuits.components": [
-            "FnAnsibleFunctionComponent = fn_ansible.components.function_ansible:FunctionComponent"
+            # When setup.py is executed, loop through the .py files in the components directory and create the entry points.
+            "{}FunctionComponent = fn_ansible_tower.components.{}:FunctionComponent"\
+                .format(snake_to_camel(get_module_name(filename)), get_module_name(filename))
+            for filename in glob.glob("./fn_ansible/components/[a-zA-Z]*.py")
         ],
         "resilient.circuits.configsection": ["gen_config = fn_ansible.util.config:config_section_data"],
         "resilient.circuits.customize": ["customize = fn_ansible.util.customize:customization_data"],
