9a1cb62adda35261e3beaa52112df0ba60697174
AnnMarie Norcross <annmarie.meier.norcross@ibm.com>
2020-06-29 10:30:48 -0400

    change assigneToCustomer to assignedToCustomer

--- fn_secureworks_ctp/fn_secureworks_ctp/lib/scwx_ctp_client.py
+++ fn_secureworks_ctp/fn_secureworks_ctp/lib/scwx_ctp_client.py
@@ -61,7 +61,7 @@ class SCWXClient(object):
         url = u"{0}/tickets/updates".format(self.base_url)
         url = u"{0}?ticketType={1}".format(url, ticket_type)
         url = u"{0}&limit={1}".format(url, self.limit)
-        url = u"{0}&assignToCustomer={1}".format(url, self.assigned_to_customer)
+        url = u"{0}&assignedToCustomer={1}".format(url, self.assigned_to_customer)
         url = u"{0}&worklogs={1}".format(url, "UPDATED")
         if grouping_type:
             url = u"{0}&groupingType={1}".format(url, grouping_type)
