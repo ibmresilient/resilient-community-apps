9073997594fcbd39a27139982477ec82408c531e
AnnMarie Norcross <annmarie.meier.norcross@ibm.com>
2020-06-30 10:17:28 -0400

    Update for new beta version number

--- fn_secureworks_ctp/setup.py
+++ fn_secureworks_ctp/setup.py
@@ -20,7 +20,7 @@ def snake_to_camel(word):
 
 setup(
     name='fn_secureworks_ctp',
-    version='0.9.5',
+    version='0.9.6',
     license='MIT',
     author_email='',
     url='https://ibm.com/mysupport',
