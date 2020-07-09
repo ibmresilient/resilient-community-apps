479cf4f97fb5e1b45d202f422d5bdfe8bca42792
Brian Reid <brian.reid@ibm.com>
2020-07-09 17:20:15 -0400

    INT-2772 require resilient lib

--- fn_xforce/setup.py
+++ fn_xforce/setup.py
@@ -13,7 +13,7 @@ setup(
     long_description="Resilient Circuits Components for 'fn_xforce'",
     install_requires=[
         'resilient_circuits>=30.0.0',
-        'requests'
+        'resilient-lib>=34.0.0'
     ],
     packages=find_packages(),
     include_package_data=True,
