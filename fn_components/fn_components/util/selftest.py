c17abbf2d0ba2a4de745108f752aee9a5308f3c9
Mark-Scherfling <mark.scherfling@ibm.com>
2020-07-10 08:44:28 -0400

    INT-2746 - selftest cleanup

--- fn_components/fn_components/util/selftest.py
+++ fn_components/fn_components/util/selftest.py
@@ -6,18 +6,14 @@
 """
 
 import logging
-import requests
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 log.addHandler(logging.StreamHandler())
-
 
 def selftest_function(opts):
     """
     Placeholder for selftest function. An example use would be to test package api connectivity.
     Suggested return values are be unimplemented, success, or failure.
     """
-    # Reading configuration variables from app.config file
-    options = opts.get("fn_compents", {})
 
     return {"state": "Success"}
