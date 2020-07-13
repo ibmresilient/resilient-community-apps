7a68fecf35212401c03d9dcc626b2276841830ba
Mark-Scherfling <mark.scherfling@ibm.com>
2020-07-13 14:03:10 -0400

    INT-2688 - code review changes

--- fn_ansible/fn_ansible/util/selftest.py
+++ fn_ansible/fn_ansible/util/selftest.py
@@ -6,10 +6,15 @@ Usage: resilient-circuits selftest -l fn_ansible
 """
 
 import logging
+import time
+from fn_ansible.lib.ansible_api import run_playbook
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 log.addHandler(logging.StreamHandler())
+
+ANSIBLE_PING = "ping"
+ANSIBLE_HOST = "localhost"
 
 
 def selftest_function(opts):
@@ -19,7 +24,31 @@ def selftest_function(opts):
     """
     app_configs = opts.get("fn_ansible", {})
 
-    return {
-        "state": "unimplemented",
-        "reason": None
-    }
+    runner_dir = app_configs.get("runner_dir")
+    artifact_dir = app_configs.get("artifact_dir")
+
+    try:
+        selftest_results = run_playbook(
+            id=time.time(),
+            private_data_dir=runner_dir,
+            artifact_dir=artifact_dir,
+            module_name=ANSIBLE_PING,
+            module_args=None,
+            module_hosts=ANSIBLE_HOST
+        )
+
+        if selftest_results['localhost'].get('summary') == "successful":
+            return {
+                "state": "success"
+            }
+        else:
+            return {
+                "state": "failure",
+                "reason": "returned summary: {}".format(selftest_results['localhost'].get('summary'))
+            }
+
+    except Exception as err:
+        return {
+            "state": "failure",
+            "reason": str(err)
+        }
