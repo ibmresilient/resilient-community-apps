ee80c81223813ef7b1b57425ab5dcb38c05b9935
Brian Reid <brian.reid@ibm.com>
2020-07-27 11:56:33 -0400

    INT-2787 add script to copy driver data

---
+++ fn_odbc_query/.container_build_packages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This script is used to bring files from the top level of resilient-community-apps into the integration directory
+# to be used during the docker build process. We need to copy the files into the inegration directory because docker
+# is unable to access files outside of the build process (anything in the filestructure above the working directory).
+
+# Create a drivers directory and copy the content from ../.odbc_drivers
+echo "copying odbc drivers into the working directory"
+mkdir drivers
+cp ../.odbc_drivers ./drivers
