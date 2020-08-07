40a9d8e1d4ac90fc4ffdc666e22fd18140dc02b8
John Prendergast <johnpren@ie.ibm.com>
2020-08-07 19:29:59 +0100

    INT-2867 - Updated description/long_description.

--- fn_bigfix/setup.py
+++ fn_bigfix/setup.py
@@ -9,8 +9,12 @@ setup(
     license='Resilient License',
     author='IBM Resilient',
     author_email='support@resilientsystems.com',
-    description="Resilient Circuits Components for 'fn_bigfix'",
-    long_description="Resilient Circuits Components for 'fn_bigfix'",
+    description="Resilient Circuits Components for BigFix",
+    long_description="""BigFix is a systems-management platform for managing a large numbers of endpoints.
+        The BigFix integration with the Resilient platform allows for the querying and updating of a BigFix deployment. 
+        The integration includes a function to query for IOCs in the BigFix environment. Returned results can be used 
+        to remediate issues or hits, such as a malicious path or filename,  a service or process name, or a registry 
+        key. The integration can also be used to query properties of an endpoint.""",
     install_requires=[
         'resilient_circuits>=31.0.0'
     ],
