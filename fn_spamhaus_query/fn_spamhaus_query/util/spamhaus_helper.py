7e6fc492869563ae82534c4d6e87de33cafb9f97
Shane Curtin <shane.curtin@ie.ibm.com>
2020-08-19 16:50:30 +0100

    INT-2848: Tidy up fn_spamhaus_submit_artifact.py - Add make_api_call to spamhaus_helper.py

--- fn_spamhaus_query/fn_spamhaus_query/util/spamhaus_helper.py
+++ fn_spamhaus_query/fn_spamhaus_query/util/spamhaus_helper.py
@@ -1,13 +1,6 @@
 # (c) Copyright IBM Corp. 2010, 2020. All Rights Reserved.
-import os
 
-
-class ApikeyError(Exception):
-    def __init__(self, msg):
-        self.message = msg
-
-    def __str__(self):
-        return repr(self.message)
+CONFIG_DATA_SECTION = 'fn_spamhaus_query'
 
 
 class SpamhausRequestCallError(Exception):
@@ -16,6 +9,32 @@ class SpamhausRequestCallError(Exception):
 
     def __str__(self):
         return repr(self.error_msg)
+
+
+def make_api_call(base_url, api_key, search_resource, qry, rc):
+    """
+    Function that makes the api call to IP Void
+
+    :param base_url: The spamhaus_wqs_url from the app.config
+    :param api_key: The API Key to IP Void read from the app.config file
+    :param search_resource: The dataset to search
+    :param qry: The artifact value
+    :param rc: RequestsCommon object
+
+    :return: The response of the API call
+    :rtype: response
+    """
+
+    auth_header = {'Authorization': 'Bearer {}'.format(api_key)}
+
+    qry_url = base_url + "/".join([str(search_resource), str(qry)])
+
+    return rc.execute_call_v2(
+        method="get",
+        url=qry_url,
+        headers=auth_header,
+        callback=spamhaus_call_error
+    )
 
 
 def spamhaus_call_error(response_object):
