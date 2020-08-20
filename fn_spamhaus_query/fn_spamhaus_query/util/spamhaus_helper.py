db20dc66246648d0e15e7c16ec60ab17f5029153
Shane Curtin <shane.curtin@ie.ibm.com>
2020-08-20 14:16:24 +0100

    INT-2848: Remove callback fn when making api call

--- fn_spamhaus_query/fn_spamhaus_query/util/spamhaus_helper.py
+++ fn_spamhaus_query/fn_spamhaus_query/util/spamhaus_helper.py
@@ -32,29 +32,5 @@ def make_api_call(base_url, api_key, search_resource, qry, rc):
     return rc.execute_call_v2(
         method="get",
         url=qry_url,
-        headers=auth_header,
-        callback=spamhaus_call_error
+        headers=auth_header
     )
-
-
-def spamhaus_call_error(response_object):
-    """
-    :param response_object: Object returned from request.get call
-    :return: if response object doesn't fall any below specified category returns response object else raises respective
-    errors.
-    """
-    # Checking for status code
-    if response_object.status_code == 400:
-        raise SpamhausRequestCallError("Bad request - there was a syntax error in the request")
-    elif response_object.status_code == 401:
-        raise SpamhausRequestCallError("Authorization failed - please verify a valid DQS key was supplied")
-    elif response_object.status_code == 403:
-        raise SpamhausRequestCallError("Forbidden - Authorization denied")
-    elif response_object.status_code == 406:
-        raise SpamhausRequestCallError("Not Acceptable - The requested Content-Type is not supported")
-    elif response_object.status_code == 429:
-        raise SpamhausRequestCallError("Too Many Requests - Rate limiting in effect, please decrease query rate")
-    elif response_object.status_code == 504:
-        raise SpamhausRequestCallError("Gateway timeout - Query could not be successfully sent")
-    else:
-        return response_object
