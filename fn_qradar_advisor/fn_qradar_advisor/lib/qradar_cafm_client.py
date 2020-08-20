3905394c4f152606fdc91f8cc7b1056ca1d1608f
John Prendergast <johnpren@ie.ibm.com>
2020-08-20 13:46:25 +0100

    INT-2920 - Updated copywrite dates.

--- fn_qradar_advisor/fn_qradar_advisor/lib/qradar_cafm_client.py
+++ fn_qradar_advisor/fn_qradar_advisor/lib/qradar_cafm_client.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # pragma pylint: disable=unused-argument, no-self-use
-# (c) Copyright IBM Corp. 2010, 2019. All Rights Reserved.
+# (c) Copyright IBM Corp. 2010, 2020. All Rights Reserved.
 
 from .qradar_http_info import HttpInfo
 
