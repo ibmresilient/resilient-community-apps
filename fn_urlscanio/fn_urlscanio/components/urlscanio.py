83ad0fd0b14c10ad398036b592bf87cd11d83917
Mark-Scherfling <mark.scherfling@ibm.com>
2020-08-28 16:08:44 -0400

    INT-2952 - urlscan fix

--- fn_urlscanio/fn_urlscanio/components/urlscanio.py
+++ fn_urlscanio/fn_urlscanio/components/urlscanio.py
@@ -11,7 +11,7 @@ from resilient_circuits import ResilientComponent, function, StatusMessage, Func
 from resilient_lib import write_file_attachment
 from resilient_lib.components.integration_errors import IntegrationError
 from io import BytesIO
-from resilient_lib import RequestsCommon
+from resilient_lib import RequestsCommon, validate_fields
 
 CONFIG_DATA_SECTION = 'urlscanio'
 
@@ -21,6 +21,7 @@ class FunctionComponent(ResilientComponent):
 
     def __init__(self, opts):
         super(FunctionComponent, self).__init__(opts)
+        self.opts = opts
         self.options = opts.get(CONFIG_DATA_SECTION, {})
         self.apikey = self.options.get("urlscanio_api_key")
         self.urlscanio_report_url = self.options.get("urlscanio_report_url")
@@ -34,6 +35,9 @@ class FunctionComponent(ResilientComponent):
         """Function: urlscanio"""
         try:
             log = logging.getLogger(__name__)
+
+            validate_fields(['urlscanio_api_key', 'urlscanio_report_url', 'urlscanio_screenshot_url'], self.options)
+            validate_fields(['urlscanio_url'], kwargs)
 
             # Get the function parameters:
             urlscanio_url = kwargs.get("urlscanio_url")              # text
@@ -59,7 +63,7 @@ class FunctionComponent(ResilientComponent):
             if urlscanio_referer:
                 urlscanio_data["referer"] = urlscanio_referer
 
-            req_common = RequestsCommon(self.options, self.opts)
+            req_common = RequestsCommon(self.opts, self.options)
 
             urlscanio_scan_url = u"{}/scan/".format(self.urlscanio_report_url)
             urlscanio_post = req_common.execute_call_v2("POST", urlscanio_scan_url, self.timeout,
