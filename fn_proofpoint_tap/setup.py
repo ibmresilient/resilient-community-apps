5ca97f9b0c355ad543ded4265cadc24337ed4679
John Prendergast <johnpren@ie.ibm.com>
2020-08-28 12:24:10 +0100

    INT-2837 - Bumped version.

--- fn_proofpoint_tap/setup.py
+++ fn_proofpoint_tap/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fn_proofpoint_tap',
-    version='1.0.2',
+    version='1.0.3',
     license='MIT',
     author='IBM Resilient',
     author_email='support@resilientsystems.com',
