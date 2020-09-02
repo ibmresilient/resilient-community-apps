be638eefe3c817879ff1852f1687d1f01acfcb3f
AnnMarie Norcross <annmarie.meier.norcross@ibm.com>
2020-09-02 09:23:29 -0400

    INT-2832 update config file parameters to expose for app host and update docs

--- fn_microsoft_security_graph/fn_microsoft_security_graph/util/config.py
+++ fn_microsoft_security_graph/fn_microsoft_security_graph/util/config.py
@@ -26,17 +26,21 @@ client_secret=xxx
 ## Polling options
 # How often polling should happen. Value is in seconds. To disable polling, set this to zero.
 msg_polling_interval=0
-#incident_template=<location_of_template_file>  # If not set uses default template.
+# Location of jinja template file to escalate incidents.
+# If not set, default template is used.
+incident_template=
 
 # String query to apply to the alert polling component. This will be added to the end of the url
 # when searching for alerts. The example shown below would make the whole search url equal to
 # https://graph.microsoft.com/v1.0/security/alerts/?$filter=assignedTo eq 'analyst@m365x594651.onmicrosoft.com' and severity eq 'high'
 # This query string is full OData so alert query can start with 'top=', 'skip=', 'filter=', etc. Do not add a '$' at the start
 # of the value as that character is reserved for environment variables
-#alert_query=filter=assignedTo eq 'analyst@m365x594651.onmicrosoft.com' and severity eq 'high'
+# alert_query=filter=assignedTo eq 'analyst@m365x594651.onmicrosoft.com' and severity eq 'high'
+alert_query=
 
 # Alert Time range sec - Optional value in seconds to set the start dateTime values for the createdDateTime field when filtering alerts.
 # This is calculated by adding to the filter 'createdDateTime ge (current_dateTime - alert_time_range_sec)
-#alert_time_range_sec=3600
+# alert_time_range_sec=3600
+alert_time_range=
 """
     return config_data
