81b6deb849320282fc2698e21e27309f83dec707
Brian Reid <brian.reid@ibm.com>
2020-09-03 10:44:31 -0400

    INT-2823 use requests

--- rc-data-feed-plugin-splunkfeed/data_feeder_plugins/splunkhecfeed/splunk_http_event_collector.py
+++ rc-data-feed-plugin-splunkfeed/data_feeder_plugins/splunkhecfeed/splunk_http_event_collector.py
@@ -10,7 +10,9 @@
 
 __author__ = "george@georgestarcher.com (George Starcher)"
 
-from resilient_lib import RequestsCommon
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 import logging
 import json
@@ -65,6 +67,14 @@ class http_event_collector:
             self.proxies["https"] = https_proxy
         else:
             self.proxies = None
+
+    def requests_retry_session(self, retries=3,backoff_factor=0.3,status_forcelist=(408,500,502,503,504),session=None):
+        session = session or requests.Session()
+        retry = Retry(total=retries, read=retries, connect=retries, backoff_factor=backoff_factor, status_forcelist=status_forcelist, method_whitelist=frozenset(['HEAD', 'TRACE', 'GET', 'PUT', 'OPTIONS', 'DELETE', 'POST']))
+        adapter = HTTPAdapter(max_retries=retry)
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
+        return session
 
     def __init__(self, token, http_event_server, input_type='json', host="", http_event_port='8088', http_event_server_ssl=True):
         self.token = token
@@ -201,37 +211,23 @@ class http_event_collector:
 
     def _batchThread(self):
         """Internal Function: Threads to send batches of events."""
-        # 503 added for endpoint busy
-        # 408 added in case using HAproxy
-        status_forcelist = (408, 500, 502, 503, 504)
-        max_retries = 3
 
         while True:
             if self.debug:
                 LOG.debug("Events received on thread. Sending to Splunk.")
+                LOG.debug("Splunk proxies: {}".format(self.proxies))
             payload = " ".join(self.flushQueue.get())
-            headers = {'Authorization':'Splunk '+self.token}
-            # Initialize Requests Common object
-            rc = RequestsCommon()
-            i = 1
-            while i != max_retries:
-                # Try to post payload then give up and move on if Exception or we hit max retries
-                try:
-                    res = rc.execute_call_v2("post", self.server_uri, data=payload,
-                                             headers=headers, verify=self.SSL_verify
-                                             )
-                    if res.status_code in status_forcelist:
-                        LOG.debug("Receied unexpected response from the server. Retrying {} more times.".format(max_retries - i))
-                        i = i + 1
-                    else:
-                        LOG.info("POST request to the Splunk server was successful")
-                except Exception:
-                    # Exception is already logged by Requests Common
-                    pass
+            headers = {'Authorization': 'Splunk ' + self.token}
+            # try to post payload twice then give up and move on
+            try:
+                r = self.requests_retry_session().post(self.server_uri, data=payload, headers=headers,
+                                                       verify=self.SSL_verify, proxies=self.proxies)
+            except Exception:
+                pass
 
             if self.debug:
                 try:
-                    LOG.debug(res.text)
+                    LOG.debug(r.text)
                 except:
                     pass
             self.flushQueue.task_done()
