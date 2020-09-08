a69122d4f780f122dd4653fde764731a881f4c47
Shane Curtin <shane.curtin@ie.ibm.com>
2020-09-08 16:36:14 +0100

    INT-2972: Rename app.config heading to fn_shodan

---
+++ fn_shodan/fn_shodan/util/config.py
@@ -0,0 +1,20 @@
+# (c) Copyright IBM Corp. 2010, 2020. All Rights Reserved.
+# -*- coding: utf-8 -*-
+
+"""Generate a default configuration-file section for fn_shodan"""
+
+
+def config_section_data():
+    """
+    Produce add the default configuration section to app.config,
+    for fn_shodan when called by `resilient-circuits config [-c|-u]`
+    """
+
+    config_data = u"""[fn_shodan]
+shodan_apikey=<your-api-key>
+
+# Proxy Configuration if any by default will be None
+http_proxy=
+https_proxy=
+"""
+    return config_data
