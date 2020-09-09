262b1c0c752523f5f4a71d32e3303634b1365303
Brian Reid <brian.reid@ibm.com>
2020-09-09 14:29:13 -0400

    update long description

--- rc-data-feed-plugin-splunkfeed/setup.py
+++ rc-data-feed-plugin-splunkfeed/setup.py
@@ -11,7 +11,12 @@ setup(
     author_email='',
     url="http://ibm.biz/resilientcommunity",
     description="Resilient Circuits Components for sending data feeds to other (BI) systems",
-    long_description="Resilient Circuits Components for sending data feeds to other (BI) systems",
+    long_description="""This package contains the SplunkFeed Plugin to the Data Feed extension.
+    This Data Feed extension allows one to maintain 'replica' data for Resilient incidents, artifacts, tasks, notes, etc.
+    The updates are performed in near real-time.
+    This plugin allows this replica data to be maintained in Splunk.
+    Refer to the documentation on the Data Feed extension for uses cases support and configuration options.
+    Also refer to the other Data Feed plugins which can be used in combination.""",
     install_requires=[
         'rc_data_feed',
         'resilient',
