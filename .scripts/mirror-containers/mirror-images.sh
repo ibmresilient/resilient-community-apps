dba3a070d57565a4d18453961dc3b9554144f4db
SEAN O’GORMAN <Sean.OGorman@ibm.com>
2020-09-16 10:51:44 +0100

    Update mirror-images.sh
    typo

--- .scripts/mirror-containers/mirror-images.sh
+++ .scripts/mirror-containers/mirror-images.sh
@@ -97,7 +97,7 @@ while IFS='' read -r image || [[ -n "$image" ]]; do
     echo "Image tagged; Pushing now to destination registry: $destination_registry"
 
     # Push our newly tagged image to the destination
-    if [[ $container_engine == podman && $insecure_registry == 1]];
+    if [[ $container_engine == podman && $insecure_registry == 1 ]];
     then
     $container_engine push --tls-verify=false "$destination_registry/$REGISTRY_ORG/$image"
     else
