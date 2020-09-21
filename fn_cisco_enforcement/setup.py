e647fd5085b1ad02677c67e494fcacba00bafed3
Mark-Scherfling <mark.scherfling@ibm.com>
2020-09-21 08:47:09 -0400

    INT-2968 - initial checkin

--- fn_cisco_enforcement/setup.py
+++ fn_cisco_enforcement/setup.py
@@ -5,14 +5,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='fn_cisco_enforcement',
-    version='1.0.0',
+    version='1.0.1',
     license='MIT',
     author='IBM Resilient',
     author_email='support@resilientsystems.com',
     description="Resilient Circuits Components for 'fn_cisco_enforcement'",
-    long_description="Resilient Circuits Components for 'fn_cisco_enforcement'",
+    long_description="Manage your Cisco Enforcement customer domain list by adding, listing and deleting entries.",
+    url='https://ibm.biz/resilientcommunity',
     install_requires=[
-        'resilient_circuits>=30.0.0'
+        'resilient_circuits>=30.0.0',
+        'resilient-lib'
     ],
     packages=find_packages(),
     include_package_data=True,
@@ -27,6 +29,7 @@ setup(
             "CiscoDeleteDomainFunctionComponent = fn_cisco_enforcement.components.delete_domain:FunctionComponent"
         ],
         "resilient.circuits.configsection": ["gen_config = fn_cisco_enforcement.util.config:config_section_data"],
-        "resilient.circuits.customize": ["customize = fn_cisco_enforcement.util.customize:customization_data"]
+        "resilient.circuits.customize": ["customize = fn_cisco_enforcement.util.customize:customization_data"],
+        "resilient.circuits.selftest": ["selftest = fn_cisco_enforcement.util.selftest:selftest_function"]
     }
 )
--- fn_cisco_enforcement/tests/test_cisco_enforcement.py
+++ fn_cisco_enforcement/tests/test_cisco_enforcement.py
@@ -4,7 +4,7 @@
 from __future__ import print_function
 import pytest
 from fn_cisco_enforcement.components.event import *
-from fn_cisco_enforcement.lib.resilient_common import *
+from resilient_lib import validate_fields, readable_datetime
 from mock import patch
 import logging
 
@@ -20,35 +20,45 @@ class TestEvent:
         [
             ("True", "grand", "smallgettogether", "A gathering of like minded individuals to discuss our general interests", "index.html", "0EB547304658805AAD788D320F10BF1F292797B5E6D745A3BF617584DA017051", "www.blahbla.com", "this test", "text","text")
         ])
-    @patch.object(FunctionComponent, "__init__",return_value= None)
+    @patch.object(FunctionComponent, "__init__", return_value=None)
     # Testing the non mandatory fields are added to data object correctly
     def test_addothers(self,circuits_app,cisco_dstip, cisco_eventseverity, cisco_eventtype,cisco_eventdescription, cisco_eventhash, cisco_filename, cisco_filehash, cisco_externalurl, cisco_src,expected):
-        fn=FunctionComponent(None)
-        data={"cisco_dstip":""}
-        expected={'cisco_dstip': 'True', 'cisco_eventseverity': 'grand', 'cisco_eventtype': 'smallgettogether', 'cisco_filehash': 'www.blahbla.com', 'cisco_filename': '0EB547304658805AAD788D320F10BF1F292797B5E6D745A3BF617584DA017051', 'cisco_externalurl': 'this test', 'cisco_eventdescription': 'A gathering of like minded individuals to discuss our general interests', 'cisco_eventhash': 'index.html', 'cisco_src': 'text'}
-        results=fn.addothers(data,cisco_dstip, cisco_eventseverity, cisco_eventtype,cisco_eventdescription, cisco_eventhash, cisco_filename, cisco_filehash, cisco_externalurl, cisco_src)
-        assert (expected==results)
+        fn = FunctionComponent(None)
+        data = {"cisco_dstip": ""}
+        expected = {
+            'cisco_dstip': 'True', 'cisco_eventseverity': 'grand', 'cisco_eventtype': 'smallgettogether',
+            'cisco_filehash': 'www.blahbla.com', 'cisco_filename': '0EB547304658805AAD788D320F10BF1F292797B5E6D745A3BF617584DA017051',
+            'cisco_externalurl': 'this test',
+            'cisco_eventdescription': 'A gathering of like minded individuals to discuss our general interests',
+            'cisco_eventhash': 'index.html', 'cisco_src': 'text'
+        }
+        results = fn.addothers(data,cisco_dstip, cisco_eventseverity, cisco_eventtype,cisco_eventdescription, cisco_eventhash, cisco_filename, cisco_filehash, cisco_externalurl, cisco_src)
+        assert(expected == results)
 
     # Test created data object
-    @pytest.mark.parametrize("cisco_deviceid,cisco_deviceversion,cisco_eventtime,cisco_alerttime,cisco_eventdescription,cisco_dsturl,cisco_dstdomain,expected",[("12","1",1518367008000,1518367008000,"test description","www.myurl.com/tt","/tt",{'eventTime': '2018-02-11T16:36:48Z', 'alertTime': '2018-02-11T16:36:48Z', 'deviceId': '12', 'providerName': 1, 'protocolVersion': 1, 'cisco_eventdescription': 'test description', 'dstDomain': '/tt', 'dstUrl': '/tt', 'deviceVersion': '1'})])
+    @pytest.mark.parametrize("cisco_deviceid, cisco_deviceversion, cisco_eventtime, cisco_alerttime, cisco_eventdescription, cisco_dsturl, cisco_dstdomain,expected",[("12","1",1518367008000,1518367008000,"test description","www.myurl.com/tt","/tt",{'eventTime': '2018-02-11T16:36:48Z', 'alertTime': '2018-02-11T16:36:48Z', 'deviceId': '12', 'providerName': 1, 'protocolVersion': 1, 'cisco_eventdescription': 'test description', 'dstDomain': '/tt', 'dstUrl': '/tt', 'deviceVersion': '1'})])
     @patch.object(FunctionComponent, "__init__", return_value=None)
-    def test_createdataobject(self,circuits_app,cisco_deviceid,cisco_deviceversion,cisco_eventtime,cisco_alerttime,cisco_eventdescription,cisco_dsturl,cisco_dstdomain,expected):
-        args={"cisco_deviceid":cisco_deviceid,"cisco_deviceversion":cisco_deviceversion,"cisco_eventtime":cisco_eventtime,"cisco_alerttime":cisco_alerttime,"cisco_eventdescription":cisco_eventdescription,"cisco_dsturl":cisco_dsturl,"cisco_dstdomain":cisco_dstdomain}
+    def test_createdataobject(self, circuits_app, cisco_deviceid, cisco_deviceversion, cisco_eventtime,
+                              cisco_alerttime, cisco_eventdescription, cisco_dsturl, cisco_dstdomain, expected):
+        args = {
+            "cisco_deviceid": cisco_deviceid, "cisco_deviceversion": cisco_deviceversion,
+            "cisco_eventtime": cisco_eventtime, "cisco_alerttime": cisco_alerttime, "cisco_eventdescription": cisco_eventdescription,
+            "cisco_dsturl": cisco_dsturl, "cisco_dstdomain": cisco_dstdomain}
         fn = FunctionComponent(None)
 
         # Default values from app config, just need to be set for this test
-        fn.options={"protocol_version":1,"provider_name":1}
+        fn.options = {"protocol_version": 1, "provider_name": 1}
         log = logging.getLogger(__name__)
-        fn.log=log
+        fn.log = log
         results = fn.createdataobject(args)
         assert (expected == results)
 
-    @pytest.mark.parametrize("fields,expected",
-                             [(["protocol_version"],{"protocol_version": "1"}),(['provider_name'],{"provider_name":"me"})])
+    @pytest.mark.parametrize("fields, expected",
+                             [(["protocol_version"], {"protocol_version": "1"}), (['provider_name'], {"provider_name": "me"})])
     # Test fields are correctly validated
-    def test_validatefields_correct(self,fields,expected):
-        results=validateFields(fields,expected)
-        assert(results==None)
+    def test_validatefields_correct(self, fields, expected):
+        results = validate_fields(fields, expected)
+        assert(results != None)
 
     @pytest.mark.parametrize("fields,expected",
                              [(["protocol_versio2n"], 'Required field is missing or empty: protocol_versio2n'),
@@ -57,26 +67,18 @@ class TestEvent:
     def test_validatefields_incorrect(self, fields, expected):
 
         with pytest.raises(ValueError) as e:
-                validateFields(fields, '')
+                validate_fields(fields, '')
                 assert (e.value.message == expected)
 
-    @pytest.mark.parametrize("time,expected",[(1525075238,'2018-04-30T08:00:38Z'),(1518367008,'2018-02-11T16:36:48Z')])
+    @pytest.mark.parametrize("time,expected", [(1525075238, '2018-04-30T08:00:38Z'), (1518367008, '2018-02-11T16:36:48Z')])
     # Test for correct time format
-    def test_readable_datetime_seconds(self,time,expected):
+    def test_readable_datetime_seconds(self, time, expected):
         results = readable_datetime(time, False)
         assert (results == expected)
 
     @pytest.mark.parametrize("time,expected",
                              [(1525075238, '2018-04-30T08:00:39Z'), (1518367008, '2018-02-11T16:36:49Z')])
     # Test for incorrect time format
-    def test_readable_datetime_miliseconds(self,time,expected):
+    def test_readable_datetime_miliseconds(self, time, expected):
         results = readable_datetime(1525075238, True)
         assert (results != expected)
-
-
-
-
-
-
-
-
