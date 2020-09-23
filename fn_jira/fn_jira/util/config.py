8274ee5644edb828bdcc809d868fa6cb1c53c4b4
Shane Curtin <shane.curtin@ie.ibm.com>
2020-09-23 14:37:09 +0100

    INT-2930: Add configs to config.py - Added: auth_method, timeout, http_proxy, https_proxy

--- fn_jira/fn_jira/util/config.py
+++ fn_jira/fn_jira/util/config.py
@@ -11,9 +11,13 @@ def config_section_data():
     """
     config_data = u"""[fn_jira]
 url=https://<jira url>
+auth_method=AUTH
 user=<jira user>
 password=<jira user password>
+timeout=10
 # use verify_cert to disable untrusted certificate verification
 verify_cert=True
+#http_proxy=
+#https_proxy=
 """
     return config_data
