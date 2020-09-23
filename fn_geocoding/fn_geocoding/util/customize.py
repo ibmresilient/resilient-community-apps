e95b7a8c257a096d034fd3f96e3adc539f0999be
Mark-Scherfling <mark.scherfling@ibm.com>
2020-09-23 08:11:38 -0400

    INT-3021 - fix for customize.py

--- fn_geocoding/fn_geocoding/util/customize.py
+++ fn_geocoding/fn_geocoding/util/customize.py
@@ -3,7 +3,11 @@
 """Generate the Resilient customizations required for fn_geocoding"""
 
 from __future__ import print_function
-from resilient import ImportDefinition
+try:
+    from resilient import ImportDefinition
+except ImportError:
+    # Support Apps running on resilient-circuits < v35.0.195
+    from resilient_circuits.util import ImportDefinition
 
 def codegen_reload_data():
     """Parameters to codegen used to generate the fn_geocoding package"""
