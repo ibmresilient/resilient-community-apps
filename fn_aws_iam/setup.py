6daa400776034b7123c171457158944daf99c7e8
John Prendergast <johnpren@ie.ibm.com>
2020-10-02 18:03:05 +0100

    INT-3079 - Updates for Apphost support.

--- fn_aws_iam/setup.py
+++ fn_aws_iam/setup.py
@@ -19,7 +19,7 @@ def snake_to_camel(word):
 
 setup(
     name='fn_aws_iam',
-    version='1.0.0',
+    version='1.0.1',
     license='MIT',
     author='IBM Resilient Support',
     author_email='',
