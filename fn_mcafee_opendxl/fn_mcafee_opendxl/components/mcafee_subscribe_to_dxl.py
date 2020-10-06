e366f5e1133546ff0e43c373c8edde8c16058a71
AnnMarie Norcross <annmarie.meier.norcross@ibm.com>
2020-10-06 14:35:45 -0400

    INT-3093 updates for new release

--- fn_mcafee_opendxl/fn_mcafee_opendxl/components/mcafee_subscribe_to_dxl.py
+++ fn_mcafee_opendxl/fn_mcafee_opendxl/components/mcafee_subscribe_to_dxl.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# (c) Copyright IBM Corp. 2010, 2017. All Rights Reserved.
+# (c) Copyright IBM Corp. 2010, 2020. All Rights Reserved.
 """Service listener implementation"""
 
 import logging
