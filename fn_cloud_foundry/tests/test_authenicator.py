cff002ac4bcdaf5431395052de5e0427832f6505
Brian Reid <brian.reid@ibm.com>
2020-10-14 16:21:02 -0400

    INT-3100 one test passing. model for the rest

--- fn_cloud_foundry/tests/test_authenicator.py
+++ fn_cloud_foundry/tests/test_authenicator.py
@@ -11,18 +11,25 @@ except ImportError:
 
 
 class TestAuthenticator:
-    @patch("fn_cloud_foundry.util.authentication.ibm_cf_bearer.requests.get")
-    @patch("fn_cloud_foundry.util.authentication.ibm_cf_bearer.requests.post")
-    def test_authenticator_success(self, post, get):
+    @patch("fn_cloud_foundry.util.authentication.ibm_cf_bearer.RequestsCommon.execute_call_v2")
+    def test_authenticator_success(self, rc):
         """
         Test that in event of success all get's processed as expected.
         """
-        get.return_value = give_response(200, {"authorization_endpoint": "test an end"})
-        post.return_value = give_response(200, {
+
+        getResp = give_response(200, {"authorization_endpoint": "test an end"})
+        postResp = give_response(200, {
             "token_type": "say",
             "access_token": "melon"
         })
-        auth = IBMCloudFoundryAuthenticator({}, {"cf_api_apikey": "apikey"}, "url")
+
+        # set the side effect for the mock object
+        # each time the mock is called, it will return the next item in the list
+        # when authenticating, RequestsCommon makes calls in this order: GET, POST, GET, POST
+        # hence the order of the list
+        rc.side_effect = [getResp, postResp, getResp, postResp]
+
+        auth = IBMCloudFoundryAuthenticator({}, {"cf_api_apikey": "apikey"}, "/url")
         auth.get_token()
         assert auth.get_headers() == {"Authorization": "say melon"}
 
