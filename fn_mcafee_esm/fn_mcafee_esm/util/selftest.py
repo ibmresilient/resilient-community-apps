d211f8d04a060d75f79b38c8fb641df8011af5ea
John Prendergast <johnpren@ie.ibm.com>
2020-10-14 18:28:57 +0100

    INT-3102 - Fixed comment typo..

--- fn_mcafee_esm/fn_mcafee_esm/util/selftest.py
+++ fn_mcafee_esm/fn_mcafee_esm/util/selftest.py
@@ -2,7 +2,7 @@
 # (c) Copyright IBM Corp. 2010, 2020. All Rights Reserved.
 # pragma pylint: disable=unused-argument, no-self-use
 """Function implementation
-   test with: resilient-circuits selftest -l fn_aws_iam
+   test with: resilient-circuits selftest -l fn-mcafee-esm
 """
 
 import logging
