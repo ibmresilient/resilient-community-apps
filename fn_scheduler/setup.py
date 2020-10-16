b79ce78d337f41f70eb872d50fa1e42988f4f1e9
Sean O Gorman <Sean.OGorman@ibm.com>
2020-10-16 14:13:03 +0100

    INT-3083 make system psycopg2 system independent

--- fn_scheduler/setup.py
+++ fn_scheduler/setup.py
@@ -37,7 +37,7 @@ setup(
         'APScheduler>=3.6.1',
         'SQLAlchemy>=1.3.8',
         'python-dateutil>=2.8.1',
-        'psycopg2>=2.8.6'
+        'psycopg2-binary>=2.8.6'
     ],
     packages=find_packages(),
     include_package_data=True,
