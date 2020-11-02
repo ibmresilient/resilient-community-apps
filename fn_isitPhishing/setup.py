d489dffd67421ac2d2d975232a94c7d208f0745d
AnnMarie Norcross <annmarie.meier.norcross@ibm.com>
2020-11-02 15:46:44 -0500

    INT-3178 update

--- fn_isitPhishing/setup.py
+++ fn_isitPhishing/setup.py
@@ -2,20 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
-import glob
-import ntpath
-
-def get_module_name(module_path):
-    """
-    Return the module name of the module path
-    """
-    return ntpath.split(module_path)[1].split(".")[0]
-
-def snake_to_camel(word):
-    """
-    Convert a word from snake_case to CamelCase
-    """
-    return ''.join(x.capitalize() or '_' for x in word.split('_'))
 
 setup(
     name='fn_isitPhishing',
@@ -38,10 +24,8 @@ setup(
     ],
     entry_points={
         "resilient.circuits.components": [
-            # When setup.py is executed, loop through the .py files in the components directory and create the entry points.
-            "{}FunctionComponent = fn_isitPhishing.components.{}:FunctionComponent".format(
-                snake_to_camel(get_module_name(filename)), get_module_name(filename)) for filename in
-            glob.glob("./fn_isitPhishing/components/[a-zA-Z]*.py")
+            "IsitphishingUrlFunctionComponent = fn_isitPhishing.components.isitphishing_url:FunctionComponent",
+            "IsitphishingHtmlDocumentFunctionComponent = fn_isitPhishing.components.isitphishing_html_document:FunctionComponent"
         ],
         "resilient.circuits.configsection": ["gen_config = fn_isitPhishing.util.config:config_section_data"],
         "resilient.circuits.customize": ["customize = fn_isitPhishing.util.customize:customization_data"],
