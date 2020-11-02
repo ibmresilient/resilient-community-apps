2759ddc24f0978c9dd9ac02d9f3566ac60063ff1
AnnMarie Norcross <annmarie.meier.norcross@ibm.com>
2020-11-02 15:16:01 -0500

    INT-3178 update fields

--- fn_isitPhishing/setup.py
+++ fn_isitPhishing/setup.py
@@ -2,13 +2,28 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+import glob
+import ntpath
+
+def get_module_name(module_path):
+    """
+    Return the module name of the module path
+    """
+    return ntpath.split(module_path)[1].split(".")[0]
+
+def snake_to_camel(word):
+    """
+    Convert a word from snake_case to CamelCase
+    """
+    return ''.join(x.capitalize() or '_' for x in word.split('_'))
 
 setup(
-     name='fn_isitPhishing',
+    name='fn_isitPhishing',
     version='1.1.0',
     license='MIT',
-    author='Resilient Labs',
+    author='IBM Resilient Labs',
     author_email='resil.labs@gmail.com',
+    url='https://github.com/ibmresilient/resilient-community-apps',
     description="Resilient Circuits Function that queries isitPhishing.org API to analyze a URL or an HTML document",
     long_description="Resilient Circuits Function that queries isitPhishing.org API to analyze a URL or an HTML document",
     install_requires=[
@@ -23,8 +38,10 @@ setup(
     ],
     entry_points={
         "resilient.circuits.components": [
-            "IsitphishingUrlFunctionComponent = fn_isitPhishing.components.isitphishing_url:FunctionComponent",
-            "IsitphishingHtmlDocumentFunctionComponent = fn_isitPhishing.components.isitphishing_html_document:FunctionComponent"
+            # When setup.py is executed, loop through the .py files in the components directory and create the entry points.
+            "{}FunctionComponent = fn_isitPhishing.components.{}:FunctionComponent".format(
+                snake_to_camel(get_module_name(filename)), get_module_name(filename)) for filename in
+            glob.glob("./fn_isitPhishing/components/[a-zA-Z]*.py")
         ],
         "resilient.circuits.configsection": ["gen_config = fn_isitPhishing.util.config:config_section_data"],
         "resilient.circuits.customize": ["customize = fn_isitPhishing.util.customize:customization_data"],
