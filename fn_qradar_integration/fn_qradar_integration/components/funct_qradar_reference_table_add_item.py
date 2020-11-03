06ed60e40338a90aa00dac003a3a0ca388574129
Ryan Gordon <Ryan.Gordon1@ibm.com>
2020-11-03 13:31:29 +0000

    INT-3157 Add Item

---
+++ fn_qradar_integration/fn_qradar_integration/components/funct_qradar_reference_table_add_item.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# pragma pylint: disable=unused-argument, no-self-use
+"""Function implementation"""
+
+import logging
+from resilient_circuits import ResilientComponent, function, handler, StatusMessage, FunctionResult, FunctionError
+from resilient_lib import validate_fields, ResultPayload
+from fn_qradar_integration.util.qradar_utils import QRadarClient
+
+PACKAGE_NAME = "fn_qradar_integration"
+
+
+class FunctionComponent(ResilientComponent):
+    """Component that implements Resilient function 'qradar_reference_table_add_item''"""
+
+    def __init__(self, opts):
+        """constructor provides access to the configuration options"""
+        super(FunctionComponent, self).__init__(opts)
+        self.options = opts.get(PACKAGE_NAME, {})
+
+    @handler("reload")
+    def _reload(self, event, opts):
+        """Configuration options have changed, save new values"""
+        self.options = opts.get(PACKAGE_NAME, {})
+
+    @function("qradar_reference_table_add_item")
+    def _qradar_reference_table_add_item_function(self, event, *args, **kwargs):
+        """Function: Add an item to a given QRadar reference table"""
+        try:
+
+            # Get the wf_instance_id of the workflow this Function was called in
+            wf_instance_id = event.message["workflow_instance"]["workflow_instance_id"]
+
+            yield StatusMessage("Starting 'qradar_reference_table_add_item' running in workflow '{0}'".format(wf_instance_id))
+
+            required_fields = ["qradar_reference_table_name", "qradar_reference_table_item_value"]
+            validate_fields(required_fields, kwargs)
+
+            rp = ResultPayload(PACKAGE_NAME, **kwargs)
+            # Get the function parameters:
+            qradar_reference_table_name = kwargs.get("qradar_reference_table_name")  # text
+            qradar_reference_table_item_value = kwargs.get("qradar_reference_table_item_value")  # text
+
+            log = logging.getLogger(__name__)
+            log.info("qradar_reference_table_name: %s", qradar_reference_table_name)
+            log.info("qradar_reference_table_item_value: %s", qradar_reference_table_item_value)
+
+            qradar_verify_cert = True
+            if "verify_cert" in self.options and self.options["verify_cert"].lower() == "false":
+                qradar_verify_cert = False
+
+            log.debug("Connection to {} using {}".format(self.options["host"],
+                                                         self.options.get("username", None) or self.options.get("qradartoken", None) ))
+
+            qradar_client = QRadarClient(host=self.options["host"],
+                                         username=self.options.get("username", None),
+                                         password=self.options.get("qradarpassword", None),
+                                         token=self.options.get("qradartoken", None),
+                                         cafile=qradar_verify_cert,
+                                         opts=self.opts, function_opts=self.options)
+
+            result = qradar_client.add_ref_element(qradar_reference_table_name,
+                                                   qradar_reference_table_item_value)
+
+            results = rp.done(success=True, 
+                              content=result)
+            yield StatusMessage("Finished 'qradar_reference_table_add_item' that was running in workflow '{0}'".format(wf_instance_id))
+
+            # Produce a FunctionResult with the results
+            yield FunctionResult(results)
+        except Exception:
+            yield FunctionError()
