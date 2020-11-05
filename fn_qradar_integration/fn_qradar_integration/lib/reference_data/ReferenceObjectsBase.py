f3f9c67f8d12d18679e353b2e6152a221944ee1c
Ryan Gordon <Ryan.Gordon1@ibm.com>
2020-11-05 13:56:04 +0000

    INT-3157 Python 2 and 3 compat for ABC class

--- fn_qradar_integration/fn_qradar_integration/lib/reference_data/ReferenceObjectsBase.py
+++ fn_qradar_integration/fn_qradar_integration/lib/reference_data/ReferenceObjectsBase.py
@@ -1,7 +1,16 @@
+import sys
 import abc
 
+# There is a difference between have abstract base classes are handled in py 2 and 3
+# In python 3 we can directly access abc.ABC and inherit from that
+# In python 2 we must create the ABC class using abc.ABCMeta
+if sys.version_info >= (3, 4):
+    ABC = abc.ABC
+else:
+    ABC = abc.ABCMeta('ABC', (), {})
 
-class ReferenceObjectBase(abc.ABC):
+
+class ReferenceObjectBase(ABC):
 
     @abc.abstractmethod
     def add_ref_element(self):
