1f0ffac6b98fbc1fbd636d464df9900d8925f5b7
Ihor-Husar <ihor.husar@ibm.com>
2020-12-10 16:21:40 -0500

    Updated install [dev-deps]

--- .scripts/install_tests_deps.sh
+++ .scripts/install_tests_deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
-
+readonly CUR_DIR=$( cd $(dirname $0) ; pwd -P )
 pip install tox
 pip install pylint
 pip install pytest
 
-./install_resilient_api.sh
+${CUR_DIR}/install_resilient_api.sh
