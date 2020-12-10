ebf40a84e5f5ea18181cab2798886da99bc68cc7
John Prendergast <johnpren@ie.ibm.com>
2020-12-10 13:57:50 +0000

    INT-3203 - Move resilinet clinet calls to seperate module.

--- fn_aws_guardduty/fn_aws_guardduty/lib/aws_gd_poller.py
+++ fn_aws_guardduty/fn_aws_guardduty/lib/aws_gd_poller.py
@@ -7,17 +7,18 @@ import logging
 import time
 import datetime as dt
 
-from resilient import SimpleHTTPException
 from fn_aws_guardduty.lib.aws_gd_cli_man import AwsGdCliMan
 import fn_aws_guardduty.util.config as config
-from fn_aws_guardduty.lib.helpers import IQuery, FCrit, get_lastrun_unix_epoch
+from fn_aws_guardduty.lib.helpers import FCrit, get_lastrun_unix_epoch
 from fn_aws_guardduty.util import const
+from fn_aws_guardduty.lib.resilient_service import ResSvc
 
 LOG = logging.getLogger(__name__)
 # Refresh region information interval in minutes
 REGIONS_INTERVAL_DEFAULT = 60
 # Multiplier to convert minutes to seconds.
 WAIT_MULTIPLIER = 60
+
 
 class AwsGdPoller():
     """Component that polls for new findings from AWS GuardDuty"""
@@ -42,8 +43,10 @@ class AwsGdPoller():
     def run(self):
         """Run polling thread, alternately check for new data and wait"""
         # Get GuardDuty client.
-
+        # Instantiate GuardDuty client manager object.
         aws_cli_man = AwsGdCliMan(self.opts, self.function_opts)
+        # Instantiate Resilient helper service object.
+        res_svc = ResSvc(self.opts, self.function_opts)
 
         while not config.STOP_THREAD:
             # Set criteria to filter findings results.
@@ -79,14 +82,14 @@ class AwsGdPoller():
                     # Iterate over finding ids to get properties.
                     for fid in findings_list:
                         finding = aws_gd.get("get_findings", DetectorId=detectorid, FindingIds=[fid])[0]
-                        if len(self._find_resilient_incident_for_req(finding, ["Id", "Region"])) == 0:
+                        if len(res_svc.find_resilient_incident_for_req(finding, ["Id", "Region"])) == 0:
                             LOG.info("AWS GuardDuty Finding ID %s in region %s discovered: %s, escalating to Resilient",
                                      finding["Id"], finding["Region"], finding.get("Title", "No Title Provided"))
                             # Get Extra Incident Fields
                             i_fields = self.make_incident_fields(finding)
 
                             # Create incident and return response
-                            i_response = self.create_incident(i_fields)
+                            i_response = res_svc.create_incident(i_fields)
 
                         else:
                             LOG.info("Incident already exists for AWS GuardDuty Incident %d", finding["Id"])
@@ -107,34 +110,6 @@ class AwsGdPoller():
             if (now - aws_cli_man.timestamp).total_seconds() > (self.regions_interval * WAIT_MULTIPLIER):
                 aws_cli_man.refresh_clients()
 
-    def _find_resilient_incident_for_req(self, finding, f_fields):
-        """
-        Check if any Resilient incidents with the GuardDuty finding ID.
-
-        :param finding_id: GuardDuty finding id
-        :return: Return list of incidents with finding id set
-        """
-        r_incidents = []
-        query_uri = "/incidents/query?return_level=partial"
-        query = IQuery(finding, f_fields)
-        try:
-            r_incidents = self.rest_client().post(query_uri, query)
-        except SimpleHTTPException:
-            # Some versions of Resilient 30.2 onward have a bug that prevents query for numeric fields.
-            # To work around this issue, let's try a different query, and filter the results. (Expensive!)
-            query_uri = "/incidents/query?return_level=normal&field_handle={}".format(finding["Id"])
-            query = IQuery(finding, f_fields, alt=True)
-
-            try:
-                r_incidents_tmp = self.rest_client().post(query_uri, query)
-
-            except Exception as err:
-                raise Exception("Exception '{}' while trying to get list of Resilient incidents.".format(err))
-
-            r_incidents = [r_inc for r_inc in r_incidents_tmp
-                           for f in f_fields if r_inc["properties"].get(f) == finding[f]]
-
-        return r_incidents
 
     def make_incident_fields(self, finding):
         """
@@ -207,25 +182,6 @@ class AwsGdPoller():
         elif 7.0 <= finding_severity <= 8.9:
             resilient_severity = "High"
         return resilient_severity
-
-    def create_incident(self, data):
-        """
-        Create Resilient Incident.
-
-        :param data: Formatted DTO for Incident
-        :return: Return response from Resilient
-        """
-        try:
-            resilient_client = self.rest_client()
-
-            uri = "/incidents"
-            # create Incident itself first
-            incident_response = resilient_client.post(uri=uri, payload=data)
-
-            return incident_response
-
-        except SimpleHTTPException as ex:
-            LOG.error("Something went wrong when attempting to create the Incident: %s", ex)
 
     def set_criteria(self):
         """
---
+++ fn_aws_guardduty/fn_aws_guardduty/lib/resilient_service.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+# (c) Copyright IBM Corp. 2010, 2020. All Rights Reserved.
+# pragma pylint: disable=unused-argument, no-self-use
+""" Functions accessing Resilient """
+import logging
+
+from resilient_circuits import ResilientComponent
+from resilient import SimpleHTTPException
+from fn_aws_guardduty.lib.helpers import IQuery
+
+LOG = logging.getLogger(__name__)
+
+
+class ResSvc(ResilientComponent):
+    """
+    Resilient Rest helper service.
+    """
+
+    def __init__(self, opts, options):
+        super(ResSvc, self).__init__(opts)
+        self.opts = opts
+        self.options = options
+
+    def find_resilient_incident_for_req(self, finding, f_fields):
+        """
+        Check if any Resilient incidents with the GuardDuty finding ID.
+
+        :param finding_id: GuardDuty finding id
+        :return: Return list of incidents with finding id set
+        """
+        r_incidents = []
+        query_uri = "/incidents/query?return_level=partial"
+        query = IQuery(finding, f_fields)
+        try:
+            r_incidents = self.rest_client().post(query_uri, query)
+        except SimpleHTTPException:
+            # Some versions of Resilient 30.2 onward have a bug that prevents query for numeric fields.
+            # To work around this issue, let's try a different query, and filter the results. (Expensive!)
+            query_uri = "/incidents/query?return_level=normal&field_handle={}".format(finding["Id"])
+            query = IQuery(finding, f_fields, alt=True)
+
+            try:
+                r_incidents_tmp = self.rest_client().post(query_uri, query)
+
+            except Exception as err:
+                raise Exception("Exception '{}' while trying to get list of Resilient incidents.".format(err))
+
+            r_incidents = [r_inc for r_inc in r_incidents_tmp
+                           for f in f_fields if r_inc["properties"].get(f) == finding[f]]
+
+        return r_incidents
+
+    def create_incident(self, data):
+        """
+        Create Resilient Incident.
+
+        :param data: Formatted DTO for Incident
+        :return: Return response from Resilient
+        """
+        try:
+            resilient_client = self.rest_client()
+
+            uri = "/incidents"
+            # create Incident itself first
+            incident_response = resilient_client.post(uri=uri, payload=data)
+
+            return incident_response
+
+        except SimpleHTTPException as ex:
+            LOG.error("Something went wrong when attempting to create the Incident: %s", ex)
