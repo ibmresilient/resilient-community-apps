697beed73a3ceebde07ebb936be5ad96485fcfb0
Ihor-Husar <ihor.husar@ibm.com>
2020-12-14 16:54:55 -0500

    Create a correct link for package [dev-deps]

--- .scripts/send_current_pr_comment.sh
+++ .scripts/send_current_pr_comment.sh
@@ -19,7 +19,7 @@ if [[ -n "${PUBLISHED_PACKAGES[*]}" ]]; then
 	for package_url in ${PUBLISHED_PACKAGES[@]};
 	do
 		package=$(echo package_url | tr "/" "\n" | tail -n 1)
-		message="${message}\n[package](package_url)"
+		message="${message}\n[$package]($package_url)"
 	done
 	curl -s -H "Authorization: token $GITHUB_AUTH_TOKEN" \
  		-X POST -d "{\"body\": \"Bot: ${message}\"}" \
