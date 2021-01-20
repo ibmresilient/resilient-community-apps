eb759e5eb4ab069c5b5fcf54e5f906a17792b415
Brian Reid <brian.reid@ibm.com>
2021-01-20 10:29:09 -0500

    INT-3357: codegen reload

--- fn_exchange_online/fn_exchange_online/util/customize.py
+++ fn_exchange_online/fn_exchange_online/util/customize.py
@@ -21,9 +21,9 @@ def codegen_reload_data():
     return {
         "package": u"fn_exchange_online",
         "message_destinations": [u"fn_exchange_online"],
-        "functions": [u"exchange_online_write_message_as_attachment", u"exchange_online_send_message", u"exchange_online_move_message_to_folder", u"exchange_online_query_emails", u"exchange_online_get_message", u"exchange_online_delete_messages_from_query_results", u"exchange_online_get_email_user_profile", u"exchange_online_create_meeting", u"exchange_online_delete_email"],
-        "workflows": [u"example_exchange_online_get_message", u"example_exchange_online_get_user_profile", u"example_exchange_online_write_message_as_attachment", u"example_exchange_online_move_message_to_folder", u"example_exchange_online_query_emails", u"example_exchange_online_create_meeting", u"example_exchange_online_query_messages_of_a_group", u"example_exchange_online_send_message", u"example_exchange_online_delete_email", u"example_exchange_online_delete_messages_from_query_results"],
-        "actions": [u"Example: Exchange Online Write Message JSON as Note", u"Example: Exchange Online Write Message EML as Attachment", u"Example: Exchange Online Query Messages", u"Example: Exchange Online Send Message", u"Example: Exchange Online Create Meeting", u"Example: Exchange Online Delete Messages from Query Results", u"Example: Exchange Online Query Messages on Artifact", u"Example: Exchange Online Move Message to Folder", u"Example: Exchange Online Get User Profile", u"Example: Exchange Online Create Artifacts", u"Example: Exchange Online Delete Message"],
+        "functions": [u"exchange_online_send_message", u"exchange_online_delete_messages_from_query_results", u"exchange_online_get_email_user_profile", u"exchange_online_delete_email", u"exchange_online_get_message", u"exchange_online_move_message_to_folder", u"exchange_online_create_meeting", u"exchange_online_query_emails", u"exchange_online_write_message_as_attachment"],
+        "workflows": [u"example_exchange_online_query_emails", u"example_exchange_online_write_message_as_attachment", u"example_exchange_online_send_message", u"example_exchange_online_get_message", u"example_exchange_online_delete_email", u"example_exchange_online_create_meeting", u"example_exchange_online_get_user_profile", u"example_exchange_online_move_message_to_folder", u"example_exchange_online_delete_messages_from_query_results", u"example_exchange_online_query_messages_of_a_group"],
+        "actions": [u"Example: Exchange Online Send Message", u"Example: Exchange Online Write Message EML as Attachment", u"Example: Exchange Online Get User Profile", u"Example: Exchange Online Query Messages", u"Example: Exchange Online Create Artifacts", u"Example: Exchange Online Delete Message", u"Example: Exchange Online Move Message to Folder", u"Example: Exchange Online Write Message JSON as Note", u"Example: Exchange Online Create Meeting", u"Example: Exchange Online Query Messages on Artifact", u"Example: Exchange Online Delete Messages from Query Results"],
         "incident_fields": [],
         "incident_artifact_types": [],
         "datatables": [u"exo_message_query_results_dt"],
@@ -37,44 +37,44 @@ def customization_data(client=None):
     Returns a Generator of ImportDefinitions (Customizations).
     Install them using `resilient-circuits customize`
 
-    IBM Resilient Platform Version: 36.0.5634
+    IBM Resilient Platform Version: 38.1.71
 
     Contents:
     - Message Destinations:
         - fn_exchange_online
     - Functions:
-        - exchange_online_write_message_as_attachment
         - exchange_online_send_message
-        - exchange_online_move_message_to_folder
-        - exchange_online_query_emails
-        - exchange_online_get_message
         - exchange_online_delete_messages_from_query_results
         - exchange_online_get_email_user_profile
-        - exchange_online_create_meeting
         - exchange_online_delete_email
+        - exchange_online_get_message
+        - exchange_online_move_message_to_folder
+        - exchange_online_create_meeting
+        - exchange_online_query_emails
+        - exchange_online_write_message_as_attachment
     - Workflows:
-        - example_exchange_online_get_message
-        - example_exchange_online_get_user_profile
-        - example_exchange_online_write_message_as_attachment
-        - example_exchange_online_move_message_to_folder
         - example_exchange_online_query_emails
-        - example_exchange_online_create_meeting
-        - example_exchange_online_query_messages_of_a_group
+        - example_exchange_online_write_message_as_attachment
         - example_exchange_online_send_message
+        - example_exchange_online_get_message
         - example_exchange_online_delete_email
+        - example_exchange_online_create_meeting
+        - example_exchange_online_get_user_profile
+        - example_exchange_online_move_message_to_folder
         - example_exchange_online_delete_messages_from_query_results
+        - example_exchange_online_query_messages_of_a_group
     - Rules:
-        - Example: Exchange Online Write Message JSON as Note
-        - Example: Exchange Online Write Message EML as Attachment
-        - Example: Exchange Online Query Messages
         - Example: Exchange Online Send Message
-        - Example: Exchange Online Create Meeting
-        - Example: Exchange Online Delete Messages from Query Results
-        - Example: Exchange Online Query Messages on Artifact
-        - Example: Exchange Online Move Message to Folder
+        - Example: Exchange Online Write Message EML as Attachment
         - Example: Exchange Online Get User Profile
+        - Example: Exchange Online Query Messages
         - Example: Exchange Online Create Artifacts
         - Example: Exchange Online Delete Message
+        - Example: Exchange Online Move Message to Folder
+        - Example: Exchange Online Write Message JSON as Note
+        - Example: Exchange Online Create Meeting
+        - Example: Exchange Online Query Messages on Artifact
+        - Example: Exchange Online Delete Messages from Query Results
     - Data Tables:
         - exo_message_query_results_dt
     - Scripts:
