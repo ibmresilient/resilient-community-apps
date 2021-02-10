6de9e04e76fac49e23acc7940f710a4854612179
John Prendergast <johnpren@ie.ibm.com>
2021-02-10 14:47:29 +0000

    INT-3393 - Cleaned up config file.

--- fn_aws_guardduty/fn_aws_guardduty/util/config.py
+++ fn_aws_guardduty/fn_aws_guardduty/util/config.py
@@ -23,16 +23,17 @@ aws_gd_master_region=<AWS_GUARDDUTY_DEFAULT_REGION>
 aws_gd_regions=<AWS_GUARDDUTY_REGION_REGEX>
 # Interval to refresh regions information (in minutes).
 aws_gd_regions_interval=60
-# Interval to poll Guardduty (in minutes).
+# Interval to poll Guardduty for findings (in minutes).
+# To turn the poller off use value 0
 aws_gd_polling_interval=10
 # Optional - severity threshold (int) to use in criterion to filter findings 
 # results. (default 7).
 # Severity ranges: 7.0 - 8.9 -> High, 4.0 - 6.9 -> Medium, 1.0 = 3.9 -> Low
-aws_gd_severity_threshold = 7
-# Optional - Lookback interval in minutes to check if findings updated
-# since last run. Used in criteria for filtering findings retrieval (default 60).
-aws_gd_lookback_interval=60
-# Optional - User defined Json template file to use for closing Resilient iincidents.
+aws_gd_severity_threshold=
+# Optional - How long, in minutes to check for previous findings at startup.
+# Used in criteria for filtering findings retrieval.
+aws_gd_lookback_interval=
+# Optional - User defined JSON template file to use for closing Resilient incidents.
 #aws_gd_close_incident_template=<PATH_TO_TEMPLATE_FILE>
 # Optional settings for access to GuardDuty via a proxy.
 #http_proxy=http://proxy:80
