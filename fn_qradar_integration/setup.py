19373aac740078f17e78b2a9a85629279ca6ac26
Mark-Scherfling <mark.scherfling@ibm.com>
2021-02-12 08:54:42 -0500

    INT-3440 -  bug fix with log stmt

--- fn_qradar_integration/setup.py
+++ fn_qradar_integration/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fn_qradar_integration',
-    version='2.0.8',
+    version='2.1.0',
     license='MIT License',
     author='IBM Resilient',
     author_email='support@resilientsystems.com',
