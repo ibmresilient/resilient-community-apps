aa1e85e8dd560a6b5e1acfc1f188ef5d52787bf2
John Prendergast <johnpren@ie.ibm.com>
2021-02-16 12:09:09 +0000

    INT-3473 - Added missing customizations..

--- fn_aws_guardduty/fn_aws_guardduty/util/customize.py
+++ fn_aws_guardduty/fn_aws_guardduty/util/customize.py
@@ -22,11 +22,11 @@ def codegen_reload_data():
         "package": u"fn_aws_guardduty",
         "message_destinations": [u"fn_aws_gd"],
         "functions": [u"func_aws_guardduty_archive_finding", u"func_aws_guardduty_refresh_finding"],
-        "workflows": [u"wf_aws_guardduty_refresh_finding", u"wf_aws_guardduty_archive_finding"],
-        "actions": [u"Example: AWS GuardDuty: Refresh Finding", u"Example: AWS GuardDuty: Archive Finding"],
-        "incident_fields": [u"aws_guardduty_trigger_refresh", u"aws_guardduty_detector_id", u"aws_guardduty_finding_arn", u"aws_guardduty_count", u"aws_guardduty_resource_type", u"aws_guardduty_finding_updated_at", u"aws_guardduty_archived", u"aws_guardduty_finding_type", u"aws_guardduty_region", u"aws_guardduty_finding_id"],
-        "incident_artifact_types": [u"aws_iam_access_key_id", u"aws_s3_bucket_name", u"aws_iam_user_name"],
-        "datatables": [u"gd_resource_affected", u"gd_action_details", u"gd_s3_bucket_details"],
+        "workflows": [u"wf_aws_guardduty_archive_finding", u"wf_aws_guardduty_refresh_finding"],
+        "actions": [u"Example: AWS GuardDuty: Update Finding Details", u"Example: AWS GuardDuty: Archive Finding", u"Example: AWS GuardDuty: Refresh Finding Details"],
+        "incident_fields": [u"aws_guardduty_region", u"aws_guardduty_resource_type", u"aws_guardduty_finding_id", u"aws_guardduty_finding_arn", u"aws_guardduty_finding_type", u"aws_guardduty_archived", u"aws_guardduty_detector_id", u"aws_guardduty_count", u"aws_guardduty_trigger_refresh", u"aws_guardduty_finding_updated_at"],
+        "incident_artifact_types": [u"aws_iam_user_name", u"aws_s3_bucket_name", u"aws_iam_access_key_id"],
+        "datatables": [u"gd_access_key_details", u"gd_finding_overview", u"gd_instance_details", u"gd_s3_bucket_details", u"gd_action_details", u"gd_resource_affected"],
         "automatic_tasks": [],
         "scripts": []
     }
@@ -46,30 +46,34 @@ def customization_data(client=None):
         - func_aws_guardduty_archive_finding
         - func_aws_guardduty_refresh_finding
     - Workflows:
-        - wf_aws_guardduty_refresh_finding
         - wf_aws_guardduty_archive_finding
+        - wf_aws_guardduty_refresh_finding
     - Rules:
-        - Example: AWS GuardDuty: Refresh Finding
+        - Example: AWS GuardDuty: Update Finding Details
         - Example: AWS GuardDuty: Archive Finding
+        - Example: AWS GuardDuty: Refresh Finding Details
     - Incident Fields:
-        - aws_guardduty_trigger_refresh
-        - aws_guardduty_detector_id
-        - aws_guardduty_finding_arn
-        - aws_guardduty_count
-        - aws_guardduty_resource_type
-        - aws_guardduty_finding_updated_at
-        - aws_guardduty_archived
-        - aws_guardduty_finding_type
         - aws_guardduty_region
+        - aws_guardduty_resource_type
         - aws_guardduty_finding_id
+        - aws_guardduty_finding_arn
+        - aws_guardduty_finding_type
+        - aws_guardduty_archived
+        - aws_guardduty_detector_id
+        - aws_guardduty_count
+        - aws_guardduty_trigger_refresh
+        - aws_guardduty_finding_updated_at
     - Custom Artifact Types:
-        - aws_iam_access_key_id
-        - aws_s3_bucket_name
         - aws_iam_user_name
+        - aws_s3_bucket_name
+        - aws_iam_access_key_id
     - Data Tables:
-        - gd_resource_affected
-        - gd_action_details
+        - gd_access_key_details
+        - gd_finding_overview
+        - gd_instance_details
         - gd_s3_bucket_details
+        - gd_action_details
+        - gd_resource_affected
     """
 
     res_file = os.path.join(os.path.dirname(__file__), RES_FILE)
