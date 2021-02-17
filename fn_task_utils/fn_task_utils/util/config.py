c9a37376d5b03765379bed5f21522f5f16fa1713
Mark-Scherfling <mark.scherfling@ibm.com>
2021-02-17 13:12:46 -0500

    INT-3221 - null commit [dev-deps]

--- fn_task_utils/fn_task_utils/util/config.py
+++ fn_task_utils/fn_task_utils/util/config.py
@@ -11,8 +11,4 @@ def config_section_data():
     """Produce the default configuration section for app.config,
        when called by `resilient-circuits config [-c|-u]`
     """
-#    config_data = u"""[fn_task_utils]
-#setting=xxx
-#"""
-#    return config_data
     return None
