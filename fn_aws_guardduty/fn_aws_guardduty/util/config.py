72b318ee8052d79ef323fd07f8879823bd177d3e
John Prendergast <johnpren@ie.ibm.com>
2021-02-19 16:12:06 +0000

    INT-3499- Updated default config.

--- fn_aws_guardduty/fn_aws_guardduty/util/config.py
+++ fn_aws_guardduty/fn_aws_guardduty/util/config.py
@@ -14,27 +14,27 @@ def config_section_data():
     for fn_aws_guardduty when called by `resilient-circuits config [-c|-u]`
     """
     config_data = u"""[fn_aws_guardduty]
-aws_gd_access_key_id=<AWS_GUARDDUTY_ACCESS_KEY_ID>
-aws_gd_secret_access_key=<AWS_GUARDDUTY_SECRET_ACCESS_KEY>
-# Default or master region for the integration
-aws_gd_master_region=<AWS_GUARDDUTY_DEFAULT_REGION>
+aws_gd_access_key_id = <AWS_GUARDDUTY_ACCESS_KEY_ID>
+aws_gd_secret_access_key = <AWS_GUARDDUTY_SECRET_ACCESS_KEY>
+# Default or master region for the integration e.g us-west-1.
+aws_gd_master_region = <AWS_GUARDDUTY_DEFAULT_REGION>
 # Filter by GuardDuty region names. Can be a string or regular expression.
-# e.g. aws_gd_regions="^(us|eu).*" to get Europe and US regions.
-aws_gd_regions=<AWS_GUARDDUTY_REGION_REGEX>
+# e.g. aws_gd_regions = "^(us|eu).*" for Europe and US regions or ".*" for all regions. 
+# Note: Only regions which have GuardDuty enabled will be processed.
+aws_gd_regions = <AWS_GUARDDUTY_REGION_REGEX>
 # Interval to refresh regions information (in minutes).
-aws_gd_regions_interval=60
+aws_gd_regions_interval = <REGIONS_INTERVAL>
 # Interval to poll Guardduty for findings (in minutes).
 # To turn the poller off use value 0
-aws_gd_polling_interval=10
-# Optional - severity threshold (int) to use in criterion to filter findings 
-# results. (default 7).
+aws_gd_polling_interval = <POLLING_INTERVAL>
+# Optional - severity threshold (integer) criteria to filter findings results.
 # Severity ranges: 7.0 - 8.9 -> High, 4.0 - 6.9 -> Medium, 1.0 = 3.9 -> Low
-aws_gd_severity_threshold=
-# Optional - How long, in minutes to check for previous findings at startup.
+#aws_gd_severity_threshold = <SEVERITY_THRESHOLD>
+# Optional - How long, to check for previous findings at startup (in minutes).
 # Used in criteria for filtering findings retrieval.
-aws_gd_lookback_interval=
-# Optional - User defined JSON template file to use for closing Resilient incidents.
-#aws_gd_close_incident_template=<PATH_TO_TEMPLATE_FILE>
+#aws_gd_lookback_interval = <LOOKBACK_INTERVAL>
+# Optional - User defined JSON template file for closing Resilient incidents.
+#aws_gd_close_incident_template = <PATH_TO_TEMPLATE_FILE>
 # Optional settings for access to GuardDuty via a proxy.
 #http_proxy=http://proxy:80
 #https_proxy=http://proxy:443
