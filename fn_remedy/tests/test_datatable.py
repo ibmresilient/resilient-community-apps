8337ee572c41ed6caea964fcec68acb5acb0bb67
Ryan Gordon <Ryan.Gordon1@ibm.com>
2021-02-24 16:20:34 +0000

    INT-3417 CR comments

--- fn_remedy/tests/test_datatable.py
+++ fn_remedy/tests/test_datatable.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8 -*-
 # (c) Copyright IBM Corp. 2021. All Rights Reserved.
 from fn_remedy.lib.datatable import Datatable
-
-try:
-    from unittest.mock import Mock, patch
-except ImportError:
-    from mock import patch, Mock
-
-import json
 import pytest
 import resilient
 from mocks.datatable_mocked_inputs import *
