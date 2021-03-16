0487f8b61a6a407990f00caa1ef2cc6e3f0df028
Brian Reid <brian.reid@ibm.com>
2021-03-15 20:31:14 -0400

    INT-3362: configs

--- fn_remedy/fn_remedy/util/config.py
+++ fn_remedy/fn_remedy/util/config.py
@@ -8,9 +8,12 @@ def config_section_data():
     Produce add the default configuration section to app.config,
     for fn_remedy when called by `resilient-circuits config [-c|-u]`
     """
-    config_data = None
 
-#    config_data = u"""[fn_remedy]
-#setting=xxx
-#"""
+    config_data = u"""[fn_remedy]
+remedy_host=<example.domain>
+remedy_user=<example_user>
+remedy_password=xxx
+#http_proxy=
+#http_proxy=
+"""
     return config_data
