dc612c7ff2bb03f8549e5660530e51152b381170
Chaitanya Challa <chaitanya.challa@ibm.com>
2021-03-26 11:37:06 -0400

    fix build and export.res

--- .scripts/install_tests_deps.sh
+++ .scripts/install_tests_deps.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
-
+readonly CUR_DIR=$( cd $(dirname $0) ; pwd -P )
 pip install tox
 pip install pylint
 pip install pytest
-pip install resilient-circuits
-pip install pytest-resilient-circuits
+pip install --upgrade attrs
+pip install setuptools-scm==5.0.2
+
+${CUR_DIR}/install_resilient_api.sh
