a1dbb0fa5cf3907f06f6718522d6fea99241c58b
Brian Reid <brian.reid@ibm.com>
2021-03-30 16:06:28 -0400

    INT-3532: changes to close incident tests

---
+++ fn_remedy/tests/mocks/remedy_dt_mock.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# pragma pylint: disable=unused-argument, no-self-use
+
+# (c) Copyright IBM Corp. 2021. All Rights Reserved.
+"""Generate Mock responses to simulate Remedy Datatable for Unit and function tests """
+
+def mock_init():
+    class MockDT(object):
+        def __init__(self):
+            pass
+        def get_data(self, *args, **kwargs):
+            pass
+        def get_rows(self, *args, **kwargs):
+            rows = [{"id":304, "cells": { "remedy_id": { "value": "INC000000000001"}}}]
+            return rows
+        def update_row(self, *args, **kwargs):
+            pass
+
+    return MockDT()
+
+# def get_data():
+#     return {}
+
+# def get_rows():
+#     rows = [{ "cells": { "remedy_id": { "value": "INC000000000001"}}}]
+#     return rows
