2384afb3da57f536e97d3c7b7c87ad0cd4f19c72
Shane Curtin <shane.curtin@ie.ibm.com>
2021-05-06 17:36:22 +0100

    Adding App 'fn_cisco_asa' [ci skip]

---
+++ fn_cisco_asa/fn_cisco_asa/util/config.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""Generate a default configuration-file section for fn_cisco_asa"""
+
+
+def config_section_data():
+    """
+    Produce add the default configuration section to app.config,
+    for fn_cisco_asa when called by `resilient-circuits config [-c|-u]`
+    """
+    config_data = u"""[fn_cisco_asa]
+
+#
+# Optional Global username and password to be used if not defined in the individual firewall
+# sections.
+#username=<asa_username>
+#password=<asa_password> 
+# Optional proxy settings
+#http_proxy=
+#https_proxy=
+
+# Copy this firewall template for each firewall to be managed. 
+# Each firewall_name should be unique. 
+# Each firewall section requires a mandatory "host" parameter and "network_object_lists". 
+[fn_cisco_asa:firewall_name]
+host=<asa_ip>
+username=<asa_username>
+password=<asa_password>
+# Comma separated list of network object groups to manage.
+network_object_groups=BLOCKLIST_IN, BLOCKLIST_OUT
+#cafile=<path to certificate file>
+"""
+    return config_data
