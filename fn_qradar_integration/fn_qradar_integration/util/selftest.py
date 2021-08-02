93ca5d4917a2cffcf983bb5453997b1620a9894a
Richard Swierk <Richard.swierk@ibm.com>
2021-08-02 12:13:27 -0400

    INT-3901 {fn_qradar_integration} Add multi servers in config to selftest

--- fn_qradar_integration/fn_qradar_integration/util/selftest.py
+++ fn_qradar_integration/fn_qradar_integration/util/selftest.py
@@ -5,6 +5,7 @@
 """
 
 import logging
+from fn_qradar_integration.lib.functions_common import QRadarServers
 from fn_qradar_integration.util.qradar_utils import QRadarClient
 
 
@@ -18,47 +19,53 @@ def selftest_function(opts):
     Placeholder for selftest function. An example use would be to test package api connectivity.
     Suggested return values are be unimplemented, success, or failure.
     """
+
+    options = opts.get("fn_qradar_integration", {})
+
+    servers = QRadarServers(opts, options)
+    server_list = servers.get_server_name_list()
+
     try:
+        for server_name in server_list:
+            options = opts.get("fn_qradar_integration:"+server_name, {})
 
-        options = opts.get("fn_qradar_integration", {})
+            log.info("Verifying app.config values for fn_qradar_integration")
 
-        log.info("Verifying app.config values for fn_qradar_integration")
+            cafile = False if options.get("verify_cert", "").lower() == "false" else options["verify_cert"]
+            qradar_client = QRadarClient(host=options["host"],
+                                        username=options.get("username", None),
+                                        password=options.get("qradarpassword", None),
+                                        token=options.get("qradartoken", None),
+                                        cafile=cafile,
+                                        opts=opts,
+                                        function_opts=options)
 
-        cafile = False if options.get("verify_cert", "").lower() == "false" else options["verify_cert"]
-        qradar_client = QRadarClient(host=options["host"],
-                                     username=options.get("username", None),
-                                     password=options.get("qradarpassword", None),
-                                     token=options.get("qradartoken", None),
-                                     cafile=cafile,
-                                     opts=opts,
-                                     function_opts=options)
+            connected = qradar_client.verify_connect()
 
-        connected = qradar_client.verify_connect()
+            log.info("Verifying QRadar connection...")
 
-        log.info("Verifying QRadar connection...")
-
-        log.info("Test was successful")
+            log.info("Test for {} was successful".format(options["host"]))
 
         return {
-            "state": "success"
+            "state" : "success"
         }
 
     except Exception as err:
-        err_reason_msg = """Could not connect to QRadar.
-            error: {0}
-            ---------
-            Current Configs in app.config file::
-            ---------
-            host: {1}
-            credentials: {2}\n""".format(
-            err,
-            options["host"],
-            options.get("username") or "service token"
-        )
+            err_reason_msg = """Could not connect to QRadar.
+                error: {0}
+                ---------
+                Current Configs in app.config file::
+                ---------
+                host: {1}
+                credentials: {2}\n""".format(
+                err,
+                options["host"],
+                options.get("username") or "service token"
+            )
 
-        log.error(err_reason_msg)
+            log.error(err_reason_msg)
 
-        return {
-            "state": "failure",
-            "reason": err_reason_msg
-        }
+            return {
+                "state": "failure",
+                "reason": err_reason_msg
+            }
