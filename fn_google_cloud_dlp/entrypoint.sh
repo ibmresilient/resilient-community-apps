ac90ceb9ca4cb8f2a576da21897f3364964cfe81
Christopher Chang <chris.chang@ibm.com>
2021-09-30 10:33:03 -0400

    INT-4006 {fn_google_cloud_dlp} Update Dockerfile and entrypoint for env variable - [build-dev-app]

--- fn_google_cloud_dlp/entrypoint.sh
+++ fn_google_cloud_dlp/entrypoint.sh
@@ -4,7 +4,6 @@
 # container in the Pod and this new container will use the new configuration
 # In a plain old docker system the container will exit
 APP_CONFIG_SHA=`sha256sum /etc/rescircuits/app.config`
-export GOOGLE_APPLICATION_CREDENTIALS=/var/rescircuits/service_account_key.json
 resilient-circuits run & CIRCUITS_PID=`echo $!`
 while true
 do
