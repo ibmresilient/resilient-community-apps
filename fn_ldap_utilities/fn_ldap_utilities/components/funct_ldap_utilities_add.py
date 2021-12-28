e592dd74a3820833e0f84e0ee77589b451dcd538
Mark-Scherfling <mark.scherfling@ibm.com>
2021-12-28 17:03:54 -0500

    INT-4204 {fn_ldap_utilities} initial changes for Add function

---
+++ fn_ldap_utilities/fn_ldap_utilities/components/funct_ldap_utilities_add.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+"""AppFunction implementation"""
+
+from resilient_circuits import AppFunctionComponent, app_function, FunctionResult
+from resilient_lib import IntegrationError, validate_fields
+from fn_ldap_utilities.util.helper import LDAPUtilitiesHelper
+from ast import literal_eval
+from ldap3.extend.microsoft.addMembersToGroups import ad_add_members_to_groups
+
+PACKAGE_NAME = "fn_ldap_utilities"
+FN_NAME = "ldap_utilities_add"
+
+
+class FunctionComponent(AppFunctionComponent):
+    """Component that implements function 'ldap_utilities_add'"""
+
+    def __init__(self, opts):
+        super(FunctionComponent, self).__init__(opts, PACKAGE_NAME)
+
+    @app_function(FN_NAME)
+    def _app_function(self, fn_inputs):
+        """
+        Function: None
+        Inputs:
+            -   fn_inputs.ldap_attribute_name_values
+            -   fn_inputs.ldap_dn
+            -   fn_inputs.
+        """
+
+        yield self.status_message("Starting App Function: '{0}'".format(FN_NAME))
+
+        validate_fields(["ldap_dn"], fn_inputs)
+
+        helper = LDAPUtilitiesHelper(self.app_configs._asdict())
+
+        inputs = fn_inputs._asdict()
+
+        try:
+            # Try converting input to an array
+            attribute_list = literal_eval("{{ {0} }}".format(inputs.get("ldap_attribute_name_values"))) \
+                if inputs.get("ldap_attribute_name_values") else {}
+        except Exception:
+            raise ValueError("""ldap_attribute_name_values incorrectly specified e.g. "attribute1": "value1", "attribute2": "value2" """)
+
+        try:
+            group_list = literal_eval(inputs.get("ldap_multiple_group_dn")) \
+                if inputs.get("ldap_multiple_group_dn") else []
+        except Exception:
+            raise ValueError("""ldap_multiple_group_dn incorrectly specified e.g. "['cn=Accounts Group,dc=example,dc=com', 'dn=IT Group,dc=example,dc=com']" """)
+
+        self.LOG.info("attribute_list: %s", attribute_list)
+        self.LOG.info("group_list: %s", group_list)
+        # Instantiate LDAP Server and Connection
+        c = helper.get_ldap_connection()
+
+        try:
+            # Bind to the connection
+            c.bind()
+        except Exception as err:
+            raise ValueError("Cannot connect to LDAP Server. Ensure credentials are correct. Error: %s", str(err))
+
+        # Inform user
+        msg = "Connected to server {0}".format(self.app_configs.ldap_server)
+        yield self.status_message(msg)
+
+        try:
+            yield self.status_message("Attempting to execute add")
+
+            c.add(fn_inputs.ldap_dn, attributes=attribute_list)
+            result = c.result
+        except Exception as err:
+            raise ValueError("Unable to add user: %s", fn_inputs.ldap_dn)
+
+        try:
+            if result.get('description', '') == 'success' and group_list:
+                yield self.status_message("Attempting to execute group add")
+                ad_add_members_to_groups(c, [fn_inputs.ldap_dn], group_list, True)
+        except Exception as err:
+            raise ValueError("Unable to add user: %s to group(s): %s", fn_inputs.ldap_dn, group_list)
+
+        finally:
+            # Unbind connection
+            c.unbind()
+
+        yield self.status_message("Finished running App Function: '{0}'".format(FN_NAME))
+
+        yield FunctionResult(result)
+        # yield FunctionResult({}, success=False, reason="Bad call")
--- fn_ldap_utilities/setup.py
+++ fn_ldap_utilities/setup.py
@@ -3,15 +3,36 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+import glob
+import ntpath
+
+def get_module_name(module_path):
+    """
+    Return the module name of the module path
+    """
+    return ntpath.split(module_path)[1].split(".")[0]
+
+def snake_to_camel(word):
+    """
+    Convert a word from snake_case to CamelCase
+    """
+    return ''.join(x.capitalize() or '_' for x in word.split('_'))
+
 
 setup(
     name='fn_ldap_utilities',
-    version='1.1.1',
+    version='1.2.0',
     license='MIT',
-    author='IBM Resilient',
-    author_email='support@resilientsystems.com',
-    description="Resilient Circuits LDAP Utilities'",
-    long_description="Resilient Circuits components to allow reading and manipulation of your LDAP Server'",
+    author='IBM SOAR',
+    author_email='',
+    display_name="LDAP Utilities",
+    description="LDAP Utilities for IBM SOAR",
+    long_description="""Perform several operations on LDAP Server including
+* Change user password
+* Change user group (add or remove group)
+* Set user attributes
+* Toggle access
+* LDAP search""",
     install_requires=[
         'resilient_circuits>=30.0.0',
         'ldap3>=2.0.0'
@@ -24,14 +45,11 @@ setup(
     ],
     entry_points={
         "resilient.circuits.components": [
-            "LdapUtilitiesToggleAccessFunctionComponent = fn_ldap_utilities.components.ldap_utilities_toggle_access:FunctionComponent",
-            "LdapUtilitiesUpdateFunctionComponent = fn_ldap_utilities.components.ldap_utilities_update:FunctionComponent",
-            "LdapUtilitiesRemoveFromGroupsFunctionComponent = fn_ldap_utilities.components.ldap_utilities_remove_from_groups:FunctionComponent",
-            "LdapUtilitiesSearchFunctionComponent = fn_ldap_utilities.components.ldap_utilities_search:FunctionComponent",
-            "LdapUtilitiesAddToGroupsFunctionComponent = fn_ldap_utilities.components.ldap_utilities_add_to_groups:FunctionComponent",
-            "LdapUtilitiesSetPasswordFunctionComponent = fn_ldap_utilities.components.ldap_utilities_set_password:FunctionComponent"
+            # When setup.py is executed, loop through the .py files in the components directory and create the entry points.
+            "{}FunctionComponent = fn_ldap_utilities.components.{}:FunctionComponent".format(snake_to_camel(get_module_name(filename)), get_module_name(filename)) for filename in glob.glob("./fn_ldap_utilities/components/[a-zA-Z]*.py")
         ],
         "resilient.circuits.configsection": ["gen_config = fn_ldap_utilities.util.config:config_section_data"],
-        "resilient.circuits.customize": ["customize = fn_ldap_utilities.util.customize:customization_data"]
+        "resilient.circuits.customize": ["customize = fn_ldap_utilities.util.customize:customization_data"],
+        "resilient.circuits.selftest": ["selftest = fn_ldap_utilities.util.selftest:selftest_function"]
     }
 )
