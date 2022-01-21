3ec2f60b56a47b27e6419d8464a7fceb4f4149ea
John Prendergast <johnpren@ie.ibm.com>
2022-01-21 14:25:50 +0000

    INT-3815 {fn_outbound_email} OAuth2 support

--- fn_outbound_email/fn_outbound_email/lib/smtp_mailer.py
+++ fn_outbound_email/fn_outbound_email/lib/smtp_mailer.py
@@ -1,9 +1,9 @@
 # (c) Copyright IBM Corp. 2010, 2020. All Rights Reserved.
 # -*- coding: utf-8 -*-
 # pragma pylint: disable=unused-argument, no-self-use
-
-
+import base64
 import errno
+import json
 import os
 import smtplib
 import ssl
@@ -14,6 +14,8 @@ from email.mime.text import MIMEText
 from ssl import Purpose
 from jinja2 import Environment, select_autoescape
 from resilient_circuits import ResilientComponent
+from resilient_lib import RequestsCommon
+from resilient_lib.components.integration_errors import IntegrationError
 from fn_outbound_email.lib.template_helper import TemplateHelper
 
 log = logging.getLogger(__name__)
@@ -44,9 +46,21 @@ class SendSMTPEmail(ResilientComponent):
             self.smtp_cafile = False
         else:
             self.smtp_cafile = self.smtp_config_section.get("smtp_ssl_cafile")
-
+        if self.smtp_config_section.get("smtp_password", None) and self.smtp_config_section.get("oauth2_client_id", None):
+            log.warning("The user and api key configuration settings are both enabled. Credentials will default to the "
+                        "api key settings.")
         self.smtp_user = self.smtp_config_section.get("smtp_user")
+        # Basic authentication property
         self.smtp_password = self.smtp_config_section.get("smtp_password")
+        # OAuth2 authentication properties
+        self.refresh_token = self.smtp_config_section.get("refresh_token")
+        self.client_id = self.smtp_config_section.get("client_id")
+        self.client_secret = self.smtp_config_section.get("client_secret")
+        self.tenant_id = self.smtp_config_section.get("tenant_id")
+        self.token_url = self.smtp_config_section.get("token_url")
+        if self.tenant_id:
+            self.token_url = self.token_url.format(tenant_id=self.tenant_id)
+        self.scope = self.smtp_config_section.get("scope")
 
         self.smtp_conn_timeout = int(self.smtp_config_section.get("smtp_conn_timeout", SMTP_DEFAULT_CONN_TIMEOUT))
         self.options = opts.get(CONFIG_DATA_SECTION, {})
@@ -54,6 +68,45 @@ class SendSMTPEmail(ResilientComponent):
         self.jinja_env = Environment(autoescape=select_autoescape(['html']),
                                      extensions=['jinja2.ext.do'])
         self.jinja_env.globals['template_helper'] = TemplateHelper(self)
+        self.rc = RequestsCommon(opts=opts, function_opts=opts.get(CONFIG_DATA_SECTION, {}))
+        if self.client_id:
+            # Using OAuth2 authentication.
+            self.get_oauth2_token()
+            self.generate_oauth2_string()
+
+    def get_oauth2_token(self):
+        """"Get a new OAuth2 token.
+
+        Args:
+            None
+        Returns:
+            Set OAuth2 access token attribute [str]
+        Raises:
+            IntegrationError: [description]
+        """
+        url = self.token_url
+
+        post_data = {
+            "client_id": self.client_id,
+            "scope": [self.scope],
+            "client_secret": self.client_secret,
+        }
+        if self.refresh_token:
+            post_data.update({"refresh_token": self.refresh_token})
+            post_data.update({"grant_type": "refresh_token"})
+        else:
+            post_data.update({"grant_type": "client_credentials"})
+
+        result = self.rc.execute_call_v2("POST", url, data=post_data)
+        r_json = result.json()
+
+        if "access_token" in r_json:
+            self.oauth2_token = r_json['access_token']
+            return True
+
+        msg = u"Unable to authenticate: Error: {}\nDescription: {}\nCorrelation_id: {}"\
+                .format(r_json.get("error"), r_json.get("error_description"), r_json.get("correlation_id"))
+        raise IntegrationError(msg)
 
     def send(self, body_html="", body_text=""):
         if not self.opts:
@@ -106,6 +159,7 @@ class SendSMTPEmail(ResilientComponent):
         log.info("Starting email connection...")
         smtp_connection = None
         try:
+
             if self.smtp_config_section.get("smtp_ssl_mode") == "ssl":
                 log.info("Building SSL connection object")
                 smtp_connection = smtplib.SMTP_SSL(host=self.smtp_server,
@@ -123,16 +177,22 @@ class SendSMTPEmail(ResilientComponent):
                     log.info("Starting TLS...")
                     smtp_connection.ehlo()
                     smtp_connection.starttls()
-                    smtp_connection.ehlo()
 
-
-            if self.smtp_user:
+            if self.smtp_user and not self.client_id:
+                # Using Basic authentication.
                 if not self.smtp_password:
                     raise SimpleSendEmailException('An SMTP user has been set; '
                                                    'the SMTP password from app.config cannot be null')
-
                 log.info("Logging in to SMTP...")
                 smtp_connection.login(user=self.smtp_user, password=self.smtp_password)
+
+            if self.client_id:
+                # Using OAuth2 authentication.
+                smtp_connection.set_debuglevel(True)
+                smtp_connection.ehlo()
+                log.info("Authenticating with SMTP server...")
+                smtp_connection.docmd('AUTH', 'XOAUTH2 ' + base64.b64encode(bytes(self.oauth2_string, "utf-8"))
+                                      .decode("utf-8"))
 
             log.info("Sending mail")
             smtp_connection.sendmail(self.from_address,
@@ -189,8 +249,56 @@ class SendSMTPEmail(ResilientComponent):
         template = self.jinja_env.from_string(template_string)
         return template.render(incident=incident_data, mail=mail_data)
 
+    def get_oauth2_token(self):
+        """"Get a new OAuth2 token.
+        If a refresh token is defined run the refresh_token flow else
+        use client_credentials flow.
+
+        Args:
+            None
+        Returns:
+            Set OAuth2 access token attribute [str]
+        Raises:
+            IntegrationError: [description]
+        """
+        url = self.token_url
+
+        post_data = {
+            "client_id": self.client_id,
+            "scope": [self.scope, "https://outlook.office.com/SMTP.Send"],
+            "client_secret": self.client_secret,
+        }
+        if self.refresh_token:
+            # Use refresh token to renew access token.
+            post_data.update({"refresh_token": self.refresh_token})
+            post_data.update({"grant_type": "refresh_token"})
+        else:
+            post_data.update({"grant_type": "client_credentials"})
+
+        result = self.rc.execute_call_v2("POST", url, data=post_data)
+        r_json = result.json()
+
+        if "access_token" in r_json:
+            self.oauth2_token = r_json['access_token']
+            return True
+
+        msg = u"Unable to authenticate: Error: {}\nDescription: {}"\
+            .format(r_json.get("error"), r_json.get("error_description"))
+        raise IntegrationError(msg)
+
+    def generate_oauth2_string(self):
+        """Generate an OAuth2 authentication string.
+
+        Args:
+            None
+        Returns:
+          Sets the string for the OAuth2 mechanism.
+        """
+        auth_string = "user={0}\x01auth=Bearer {1}\x01\x01".format(self.smtp_user, self.oauth2_token)
+        self.oauth2_string = auth_string
 
 class SimpleSendEmailException(Exception):
     """Exception for Send Email errors"""
     def __init__(self, message):
         log.error("SimpleSendEmailException %s", message)
+
--- fn_outbound_email/fn_outbound_email/util/config.py
+++ fn_outbound_email/fn_outbound_email/util/config.py
@@ -14,9 +14,28 @@ def config_section_data():
     return u"""[fn_outbound_email]
 # SMTP SERVER (IP ADDRESS or FQDN)
 smtp_server=xxx.xxx.xxx.xxx
-
-#Blank for initial selftest, both required for TLS 
+#Blank for initial selftest, required for TLS
 smtp_user=
+
+## OAuth2 authentication settings
+scope=
+client_id=
+client_secret=
+# Note that the token_url below, for some applications e.g. using Microsoft, contains a placeholder 
+# {tenant} for the tenant ID.
+# Do not place the tenant id in the place holder as the integration will do this at run time.
+# e.g. token_url=https://login.microsoftonline.com/{tenant_id}/oauth2/V2.0/token and 
+# tenant_id=1234567a-abc8-90d1-2efa3-123456789abcd
+# Do not place '/' at the end of the url strings.
+#
+token_url=
+# Optional setting: A seperate value for the tenant_id parameter see {tenant_id} placeholder above.
+tenant_id=
+# Optional setting: Certain applications may require a refresh_token.
+refresh_token=
+
+##Basic Authentication setting
+#Blank for initial selftest, required for TLS in Basic Authentication mode.
 smtp_password=
 
 #If smtp_user is not an email address then from_email_address should equal the email address
