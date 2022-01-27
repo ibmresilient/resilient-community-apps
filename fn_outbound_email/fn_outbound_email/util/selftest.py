86944e78e3f09796b73a91160de63c72a3684507
John Prendergast <johnpren@ie.ibm.com>
2022-01-27 14:38:54 +0000

    INT-3815 {fn_outbound_email} Updated selftest

--- fn_outbound_email/fn_outbound_email/util/selftest.py
+++ fn_outbound_email/fn_outbound_email/util/selftest.py
@@ -1,4 +1,4 @@
-# (c) Copyright IBM Corp. 2010, 2020. All Rights Reserved.
+# (c) Copyright IBM Corp. 2010, 2022. All Rights Reserved.
 # -*- coding: utf-8 -*-
 # pragma pylint: disable=unused-argument, no-self-use
 """Function implementation
@@ -21,48 +21,34 @@ def selftest_function(opts):
 
     try:
         smtp_config_section = opts.get(CONFIG_DATA_SECTION, {})
-        smtp_server = smtp_config_section.get("smtp_server")
-        smtp_port = str(smtp_config_section.get("smtp_port", SMTP_DEFAULT_PORT))
-        smtp_cafile = smtp_config_section.get("smtp_ssl_cafile", False)
         smtp_user = smtp_config_section.get("smtp_user")
-        smtp_password = smtp_config_section.get("smtp_password")
         from_email_address = smtp_config_section.get("from_email_address", smtp_user)
-        smtp_ssl_mode = smtp_config_section.get("smtp_ssl_mode")
 
-        smtp_conn_timeout = int(smtp_config_section.get("smtp_conn_timeout", SMTP_DEFAULT_CONN_TIMEOUT))
+        mail_data = {}
+        mail_data['mail_from'] = from_email_address
+        mail_data['mail_to'] = [from_email_address]
+        mail_data['mail_subject'] = 'This is a test email'
+        mail_data['mail_cc'] = None
+        mail_data['mail_bcc'] = None
+        mail_data['mail_attachments'] = None
 
-        validate_fields(["smtp_server", "smtp_port"], smtp_config_section)
-        LOG.info("Validating connection to mail server")
+        send_smtp_email = SendSMTPEmail(opts, mail_data)
+        mail_body_text = "This is a test body"
+        error_msg = send_smtp_email.send(body_text=mail_body_text)
 
-        if smtp_ssl_mode == "ssl":
-            LOG.info("Building SSL connection object")
-            smtp_connection = smtplib.SMTP_SSL(host=smtp_server,
-                                               port=smtp_port,
-                                               certfile=smtp_cafile,
-                                               context=SendSMTPEmail.get_smtp_ssl_context,
-                                               timeout=smtp_conn_timeout)
+        if error_msg:
+            return {
+                "state": "failure",
+                "reason": "Failed to send test email with error:{}".format(error_msg)
+            }
         else:
-            LOG.info("Building generic connection object")
-            smtp_connection = smtplib.SMTP(host=smtp_server,
-                                                port=smtp_port,
-                                                timeout=smtp_conn_timeout)
-            if smtp_ssl_mode == "starttls" and smtp_user:
-                LOG.info("Starting TLS...")
-                smtp_connection.ehlo()
-                smtp_connection.starttls()
-                smtp_connection.ehlo()
-                LOG.info("Logging in to SMTP...")
-                if not smtp_password:
-                    raise Exception('An SMTP user has been set; the SMTP password from app.config cannot be null')
-                
-                smtp_connection.login(user=smtp_user, password=smtp_password)
-                if from_email_address:
-                    smtp_connection.sendmail(from_email_address, from_email_address, 'this is a test email')
+            return {
+                "state": "success",
+                "reason": "Send test email Successful"
+            }
 
-        return {"state": "success"}
     except Exception as err:
-        LOG.error(err)
-        return {"state": "failure"}
-    finally:
-        if smtp_connection:
-            smtp_connection.quit()
+        return {
+            "state": "failure",
+            "status_code": err
+        }
