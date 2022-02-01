4e083c1e4a91a450da7aa99dea4c41ef0a26523c
Shane Curtin <shane.curtin@ie.ibm.com>
2022-02-01 12:00:00 +0000

    INT-4144 {rc_data_feed_plugin_odbcfeed} update build script - Update Dockerfile - [build-dev-app]

--- rc_data_feed_plugin_odbcfeed/.travis_extra_setup.sh
+++ rc_data_feed_plugin_odbcfeed/.travis_extra_setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-# copy drivers needed to build the docker image
 
-cp ../.odbc_drivers/* drivers/drivers/
+# This script is used to bring files from the top level of resilient-community-apps into the integration directory
+# to be used during the docker build process. We need to copy the files into the inegration directory because docker
+# is unable to access files outside of the build process (anything in the filestructure above the working directory).
+
+# When ran in Travis this script will have access to all
+# env vars specifically $PACKAGE_PATH and $TRAVIS_BUILD_DIR
+
+echo "copying drivers into the rc_data_feed_plugin_odbcfeed/.drivers directory"
+cp -R $TRAVIS_BUILD_DIR/.drivers $PACKAGE_PATH/.drivers
