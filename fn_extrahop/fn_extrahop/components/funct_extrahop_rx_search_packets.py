ec0f72d768977c81a1d2c3d8b8e83c4d34131c8e
John Prendergast <johnpren@ie.ibm.com>
2022-02-14 17:23:59 +0000

    INT-4375 {fn_extrahop} Added missingf/refreshed copywrite notices

--- fn_extrahop/fn_extrahop/components/funct_extrahop_rx_search_packets.py
+++ fn_extrahop/fn_extrahop/components/funct_extrahop_rx_search_packets.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# (c) Copyright IBM Corp. 2010, 2022. All Rights Reserved.
+# pragma pylint: disable=unused-argument, no-self-use
 
 """AppFunction implementation"""
 import re
