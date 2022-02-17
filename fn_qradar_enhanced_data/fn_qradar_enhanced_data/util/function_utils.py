055cff76c430cd30929bc9c2373d20b1b0e66d02
Richard Swierk <Richard.swierk@ibm.com>
2022-02-17 15:32:18 -0500

    INT-4406 {fn_qradar_enhanced_data}

--- fn_qradar_enhanced_data/fn_qradar_enhanced_data/util/function_utils.py
+++ fn_qradar_enhanced_data/fn_qradar_enhanced_data/util/function_utils.py
@@ -3,7 +3,6 @@
 # (c) Copyright IBM Corp. 2022. All Rights Reserved.
 #
 # Util functions
-
 import six
 from resilient_lib import validate_fields, IntegrationError
 from fn_qradar_enhanced_data.util.qradar_constants import PACKAGE_NAME, GLOBAL_SETTINGS
