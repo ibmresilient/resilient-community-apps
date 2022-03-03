5c528d04e3ead40144a17371da7a9b665bf70a9d
Richard Swierk <Richard.swierk@ibm.com>
2022-03-03 15:33:43 -0500

    INT-4474 {fn_qradar_enhanced_data} doc change [build-dev-app]

--- fn_qradar_enhanced_data/fn_qradar_enhanced_data/util/config.py
+++ fn_qradar_enhanced_data/fn_qradar_enhanced_data/util/config.py
@@ -9,7 +9,10 @@ def config_section_data():
        when called by `resilient-circuits config [-c|-u]`
     """
     return u'''
-# Note: the QRadar instance name that you want to communicate with, must equal the
+# Note: If [fn_qradar_integration] is present without a label then all labeled servers will
+# be disregarded and only the server under [fn_qradar_integration] will be used
+#
+# The QRadar instance name that you want to communicate with, must equal the
 # QRadar Destination Name that is set when configuring the SOAR Plugin
 # Example: SOAR_Plugin_Destination_Name1
 [fn_qradar_integration:SOAR_Plugin_Destination_Name1]
