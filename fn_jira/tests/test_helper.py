3278bb691ca776a7fe17f502d5644b7303242f87
Bo Bleckel <bo.bleckel@ibm.com>
2022-03-28 11:38:55 -0400

    INT-3047 {fn_jira} fix tests for new app configs

--- fn_jira/tests/test_helper.py
+++ fn_jira/tests/test_helper.py
@@ -14,6 +14,7 @@ from fn_jira.util.helper import (build_url_to_resilient, extract_images,
 def test_validate_app_configs():
     mock_app_configs = {
         "url": "www.example.com",
+        "auth_method": "BASIC",
         "user": "exampleuser",
         "password": "examplepassword",
         "verify_cert": "true"
@@ -23,6 +24,7 @@ def test_validate_app_configs():
 
     assert valid_app_configs.get("url") == "www.example.com"
     assert valid_app_configs.get("user") == "exampleuser"
+    assert valid_app_configs.get("auth_method") == "BASIC"
     assert valid_app_configs.get("password") == "examplepassword"
     assert valid_app_configs.get("verify_cert") is True
 
