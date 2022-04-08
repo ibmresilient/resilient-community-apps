d92f706a4c2eb935a602c68df77f8f409eb16b23
John Prendergast <johnpren@ie.ibm.com>
2022-04-08 13:07:22 +0100

    INT-4082 {fn_extrahop} Fixed pylint issue

--- fn_extrahop/fn_extrahop/util/config.py
+++ fn_extrahop/fn_extrahop/util/config.py
@@ -33,6 +33,13 @@ polling_filters =
 # If your ExtraHop server uses a self-signed TLS certificate, or some
 # other certificate that is not automatically trusted by your machine,
 # you can set the CA bundle using the extrahop_cafile setting.
+
+
+
+
+
+
+
 # If you don't want to use a cert you can set extrahop_cafile=false.
 #
 #extrahop_cafile=<path to cert file>|false
@@ -40,4 +47,3 @@ polling_filters =
 #https_proxy=https://proxy:443
 """
     return config_data
-
