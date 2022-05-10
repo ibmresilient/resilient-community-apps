474852226dc51525b49be6de90f36155d0f327e1
John Prendergast <johnpren@ie.ibm.com>
2022-05-10 16:31:28 +0100

    INT-4734 {fn_extrahop} Uncommented cafile and setting moved up

--- fn_extrahop/fn_extrahop/util/config.py
+++ fn_extrahop/fn_extrahop/util/config.py
@@ -13,17 +13,23 @@ def config_section_data():
     config_data = u"""[fn_extrahop]
 # The ExtraHop rest api url.
 extrahop_rx_host_url = <EXTRAHOP_RX_HOST_URL>
-# Cloud service console url used to create linkbacks when executing against the
-# ExtraHop cloud-based service. Not required if connecting to a standalone sensor.
-extrahop_rx_cloud_console = <EXTRAHOP_RX_CLOUD_CONSOLE>
 extrahop_rx_api_version = v1
 # The ExtraHop functions can be executed against the ExtraHop cloud-based service
 # or against an ExtraHop standalone sensor.
-# Authentication settings for ExtraHop Cloud service are an api key id and key secret.
+# Cloud service console & authentication settings - for cloud-based ExtraHop instance
+# The Cloud service console url is used to create linkbacks when executing against the
+# ExtraHop cloud-based service.
+extrahop_rx_cloud_console = <EXTRAHOP_RX_CLOUD_CONSOLE>
 extrahop_rx_key_id = <EXTRAHOP_RX_API_KEY_ID>
 extrahop_rx_key_secret = <EXTRAHOP_RX_API_KEY_SECRET>
-# Authentication setting for ExtraHop standalone sensor is a single api key
+# Standalone sensor authentication setting - for a standalone ExtraHop sensor
 extrahop_rx_api_key = <EXTRAHOP_RX_API_KEY>
+# If your ExtraHop server uses a self-signed TLS certificate, or some
+# other certificate that is not automatically trusted by your machine,
+# you can set the CA bundle using the extrahop_cafile setting.
+# If you don't want to use a cert you can set extrahop_cafile=false.
+# Note: This setting is mandatory for a standalone ExtraHop sensor.
+extrahop_cafile=<path to cert file>|false
 # Interval to poll ExtraHop for detections (in seconds).
 # To turn the poller off use value 0.
 # Set value to 0 when running selftest/Test Configuration.
@@ -35,12 +41,6 @@ polling_interval = <POLLING_INTERVAL>
 # sec.dos, sec.exploit, sec.exfil, sec.lateral, sec.ransomware, sec.recon
 # Risk score ranges: red (80-99), orange (31-79), or yellow (1-30).
 polling_filters =
-# If your ExtraHop server uses a self-signed TLS certificate, or some
-# other certificate that is not automatically trusted by your machine,
-# you can set the CA bundle using the extrahop_cafile setting.
-# If you don't want to use a cert you can set extrahop_cafile=false.
-#
-#extrahop_cafile=<path to cert file>|false
 #http_proxy=http://proxy:80
 #https_proxy=https://proxy:443
 """
