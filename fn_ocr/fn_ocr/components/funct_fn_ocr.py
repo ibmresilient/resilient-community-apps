edeabc7f3cfb21249b2ac5bc1f26e9fa21b115ea
Sami Amer <contact@sami.ps>
2022-06-09 15:16:22 -0400

    INT-4862 {fn_ocr} removed unused functions and commented code from template

--- fn_ocr/fn_ocr/components/funct_fn_ocr.py
+++ fn_ocr/fn_ocr/components/funct_fn_ocr.py
@@ -22,36 +22,6 @@ class FunctionComponent(AppFunctionComponent):
     def __init__(self, opts):
         super(FunctionComponent, self).__init__(opts, PACKAGE_NAME)
 
-    # def threshold_image(self,image):
-    #     """Thresholds and OpenCV Image"""
-    #     img_grey = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    #     img_blurred = cv2.GaussianBlur(img_grey,(7,7), 0)
-    #     thresh = cv2.adaptiveThreshold(img_blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 21, 10)
-    #     return thresh
-
-    def orient_image(self,image):
-        # try:
-        #     or_data = pytesseract.image_to_osd(image,output_type="dict")
-        # except Exception as err:
-        #    self.LOG.warning("orienting image errored, returning original image for thresholding!")
-        #    self.LOG.error(err)
-        #    return image
-        # img_rotated = cv2.rotate(image, angle=or_data["rotate"])
-        # return img_rotated
-        img_grey = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-        img_grey = cv2.bitwise_not(img_grey)
-        img_thresh = cv2.threshold(img_grey, 0 , 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-        coord = np.column_stack(np.where(img_thresh > 0))
-        angle = cv2.minAreaRect(coord)[-1]
-        angle = (90 + angle) if angle < -45 else -angle
-
-        (h,w) = image.shape[:2]
-        center = (w // 2, h //2 )
-        M = cv2.getRotationMatrix2D(center, angle, 1.0)
-        rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-        self.LOG.debug(angle)
-        return rotated
-
     @app_function(FN_NAME)
     def _app_function(self, fn_inputs):
         """
@@ -129,51 +99,6 @@ class FunctionComponent(AppFunctionComponent):
 
             # output_text += '"' + text + '",' +"\t\t" + f"Confidence: {round(conf,2)}%\n"
             line_dicts.append({"text":text,"confidence":conf})
-        # Example validating app_configs
-        # validate_fields([
-        #     {"name": "api_key", "placeholder": "<your-api-key>"},
-        #     {"name": "base_url", "placeholder": "<api-base-url>"}],
-        #     self.app_configs)
-
-        # Example validating required fn_inputs
-        # validate_fields(["required_input_one", "required_input_two"], fn_inputs)
-
-        # Example accessing optional attribute in fn_inputs (this is similar for app_configs)
-        # optional_input = fn_inputs.optional_input if hasattr(fn_inputs, "optional_input") else "Default Value"
-
-        # Example getting access to self.get_fn_msg()
-        # fn_msg = self.get_fn_msg()
-        # self.LOG.debug("fn_msg: %s",  fn_msg)
-
-        # Example interacting with REST API
-        # res_client = self.rest_client()
-        # function_details = res_client.get("/functions/{0}?handle_format=names".format(FN_NAME))
-
-        # Example raising an exception
-        # raise IntegrationError("Example raising custom error")
-
-        ##############################################
-        # PUT YOUR FUNCTION IMPLEMENTATION CODE HERE #
-        ##############################################
-
-        # Call API implementation example:
-        # params = {
-        #     "api_key": self.app_configs.api_key,
-        #     "ip_address": fn_inputs.artifact_value
-        # }
-        #
-        # response = self.rc.execute(
-        #     method="get",
-        #     url=self.app_configs.api_base_url,
-        #     params=params
-        # )
-        #
-        # results = response.json()
-        #
-        # yield self.status_message("Endpoint reached successfully and returning results for App Function: '{0}'".format(FN_NAME))
-        #
-        # yield FunctionResult(results)
-        ##############################################
 
         yield self.status_message("Finished running App Function: '{0}'".format(FN_NAME))
 
