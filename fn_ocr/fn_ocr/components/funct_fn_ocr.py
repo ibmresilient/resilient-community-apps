cf3f1e4e114f11e3de08b99995d70f768d01488f
Sami Amer <contact@sami.ps>
2022-06-09 15:44:24 -0400

    INT-4862 {fn_ocr} added comments

--- fn_ocr/fn_ocr/components/funct_fn_ocr.py
+++ fn_ocr/fn_ocr/components/funct_fn_ocr.py
@@ -25,7 +25,7 @@ class FunctionComponent(AppFunctionComponent):
     @app_function(FN_NAME)
     def _app_function(self, fn_inputs):
         """
-        Function: runs OCR on an image in byte string format and returns the relevant results
+        Function: runs OCR on an image in byte string format and returns a list with one {"text":text,"confidence":conf} per line
         Inputs:
             -   fn_inputs.ocr_artifact_id
             -   fn_inputs.ocr_incident_id
@@ -33,16 +33,13 @@ class FunctionComponent(AppFunctionComponent):
         """
 
         yield self.status_message("Starting App Function: '{0}'".format(FN_NAME))
-        # self.LOG.debug(fn_inputs)
 
         incident_id = fn_inputs.ocr_incident_id  # number
         task_id = fn_inputs.ocr_task_id  # number
         artifact_id = fn_inputs.ocr_artifact_id  # number
-        attachment_id = None # Need to fix this in SOAR
         confidence_threshold = fn_inputs.ocr_confidence_threshold if fn_inputs.ocr_confidence_threshold else 49
         lang = fn_inputs.ocr_language if fn_inputs.ocr_language else 'eng'
 
-        self.LOG.debug(incident_id, task_id, artifact_id)
         self.LOG.info("incident_id: %s", incident_id)
         self.LOG.info("task_id: %s", task_id)
         self.LOG.info("artifact_id: %s", artifact_id)
@@ -60,50 +57,32 @@ class FunctionComponent(AppFunctionComponent):
         yield self.status_message("> Reading attachment...")
 
         client = self.rest_client()
-        data = get_file_attachment(client, incident_id, artifact_id=artifact_id, task_id=task_id, attachment_id=attachment_id)
-        metadata = get_file_attachment_metadata(client, incident_id, artifact_id=artifact_id, task_id=task_id, attachment_id=attachment_id)
-        # self.LOG.debug(data)
+        data = get_file_attachment(client, incident_id, artifact_id=artifact_id, task_id=task_id, attachment_id=None)
+        metadata = get_file_attachment_metadata(client, incident_id, artifact_id=artifact_id, task_id=task_id, attachment_id=None)
         
-        arr_from_string = np.frombuffer(data, np.uint8)
-        img_from_arr = cv2.imdecode(arr_from_string, cv2.IMREAD_COLOR)
-        img_rgb = cv2.cvtColor(img_from_arr,cv2.COLOR_BGR2RGB)
-        # img_oriented = self.orient_image(img_rgb)
-        # img_thresh = self.threshold_image(img_oriented) 
+        arr_from_string = np.frombuffer(data, np.uint8) # data comes as a bytestring, need to read it into an array
+        img_from_arr = cv2.imdecode(arr_from_string, cv2.IMREAD_COLOR) # use cv2 to open the array as a color image
+        img_rgb = cv2.cvtColor(img_from_arr,cv2.COLOR_BGR2RGB) # open-cv (cv2) defaults to BGR colors, we convert just in case
+        
+        # page segmentation mode is set to --psm 1 to account for orientation 
+        detected_data = pytesseract.image_to_data(img_rgb, output_type="data.frame", config=f'-l {lang} --psm 1') # runs Tesseract OCR
+        detected_text = detected_data[detected_data.conf != -1] # Drops empty entries in the dataframe
+        detected_lines = detected_text.groupby('block_num')['text'].apply(list) # turn the dataframe into a list of lines
+        line_conf = detected_text.groupby(['block_num'])['conf'].mean() # gets an equally long list as above, but of average confidences
 
-        # detected_text = pytesseract.image_to_string(img_rgb)
-        # cleaned_text  = detected_text.strip()
-        
-        # default page segmentaion is `--psm 3`, which expects a page of text and automatically looks for lines/paragraphs etc 
-        detected_data = pytesseract.image_to_data(img_rgb, output_type="data.frame", config=f'-l {lang} --psm 1')
-        detected_text = detected_data[detected_data.conf != -1]
-        detected_lines = detected_text.groupby('block_num')['text'].apply(list)
-        line_conf = detected_text.groupby(['block_num'])['conf'].mean()
-        # output_text = "Below are the lines detected by OCR, along with their confidence scores:\n\n" 
         line_dicts = []
 
-        # self.LOG.debug(detected_data)
-        # self.LOG.debug(detected_lines)
-        # self.LOG.debug(line_conf)
 
         for text,conf in zip(detected_lines,line_conf):
-            text = ' '.join(text).strip()
+            text = ' '.join(text).strip() # strip \n and similar characters so we can control the formatting ourselves
             
-            if text == "": continue
+            if text == "":self.LOG.debug("Line Empty, Skipping"); continue
+            if conf < confidence_threshold: self.LOG.debug("Confidence Low, Skipping"); continue
 
-            # self.LOG.debug('\n'*4)
-            # self.LOG.debug(text)
-            # self.LOG.debug('\n'*4)
-
-            # conf = str(round(conf,2))
-            if conf < confidence_threshold: self.LOG.debug("Confidence low, skipping line"); continue
-
-            # output_text += '"' + text + '",' +"\t\t" + f"Confidence: {round(conf,2)}%\n"
             line_dicts.append({"text":text,"confidence":conf})
 
         yield self.status_message("Finished running App Function: '{0}'".format(FN_NAME))
 
-        # Note this is only used for demo purposes! Put your own key/value pairs here that you want to access on the Platform
-        # results = {"text": detected_text}
         results = line_dicts 
         self.LOG.debug(json.dumps(results, indent=2))
 
