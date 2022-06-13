f9764f245a61f367bdfefd60110270bd22ec566f
Sami Amer <contact@sami.ps>
2022-06-13 16:22:02 -0400

    INT-4862 {fn_ocr} pinning major version [build-dev-app]

--- fn_ocr/setup.py
+++ fn_ocr/setup.py
@@ -32,10 +32,10 @@ setup(
     long_description="""Resilient Circuits Components for 'fn_ocr'""",
     install_requires=[
         "resilient-circuits>=45.0.0",
-        "pytesseract==0.3.8",
-        "numpy==1.22.4", 
-        "opencv-python-headless==4.5.5.64",
-        "pandas==1.4.2"
+        "pytesseract~=0.3.8",
+        "numpy~=1.19", 
+        "opencv-python-headless~=4.5.5.64",
+        "pandas~=1.1.5"
     ],
     python_requires='>=3.6',
     packages=find_packages(),
