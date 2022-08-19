be3fc2269f6710325d157d10183264e430642eb4
calvin wynne <calvin.wynne@ibm.com>
2022-08-19 11:34:39 +0100

    INT-5013 {fn_webex} Rooms Fixed review comments

--- fn_webex/fn_webex/util/config.py
+++ fn_webex/fn_webex/util/config.py
@@ -7,6 +7,7 @@ def config_section_data():
 
     config_data = u"""[fn_webex]
 # this may also be the conference id for the developer sandbox
+webex_site_url = https://webexapis.com
 webex_timezone = GMT 00:00
 client_id = <Issued when creating the integration>
 client_secret = <Issued when creating the integration>
