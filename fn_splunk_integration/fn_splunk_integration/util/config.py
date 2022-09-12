b4a2d2a466f515fb57d1fc0be282d1bf01ec667b
Richard Swierk <Richard.swierk@ibm.com>
2022-09-12 13:15:44 -0400

    INT-5241 {fn_splunk_integration} Update

--- fn_splunk_integration/fn_splunk_integration/util/config.py
+++ fn_splunk_integration/fn_splunk_integration/util/config.py
@@ -10,14 +10,14 @@ def config_section_data():
     """
     return u'''
 # V1.1.0+ have the option to have multiple servers configured.
-# By default two examples of servers are given, example one is labeled `splunk_label1` and example two is labled `splunk_label2`.
+# By default two examples of servers are given, example one is labeled `splunk_label1` and example two is labeled `splunk_label2`.
 # The label for a server is placed after `[fn_splunk_integration:` and then followed by `]`.
 # To add additional servers copy the below example server configuration from `[fn_splunk_integration:splunk_label1]` to `verify_cert=false|/path/to/cert`.
 # Then paste it at the bottom of the app.config.
 # Change the server label, `splunk_label1`, to a label helpful to define that server.
 # Then change the setting to those of the server you wish to add.
 
-# If token given then username/password will be disregarded
+# If token is given then username/password will be disregarded
 [fn_splunk_integration:splunk_label1]
 host=localhost
 port=8089
