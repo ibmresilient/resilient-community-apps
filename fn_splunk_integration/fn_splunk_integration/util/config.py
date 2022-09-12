afb90b6f02d30bfd07c23131dad1ad79768eaf7a
Richard Swierk <Richard.swierk@ibm.com>
2022-09-12 12:47:29 -0400

    INT-5241 {fn_splunk_integration} Update [build-dev-app]

--- fn_splunk_integration/fn_splunk_integration/util/config.py
+++ fn_splunk_integration/fn_splunk_integration/util/config.py
@@ -9,11 +9,11 @@ def config_section_data():
        when called by `resilient-circuits config [-c|-u]`
     """
     return u'''
-# V2.0.0+ have the option to have multiple servers configured.
+# V1.1.0+ have the option to have multiple servers configured.
 # By default two examples of servers are given, example one is labeled `splunk_label1` and example two is labled `splunk_label2`.
 # The label for a server is placed after `[fn_splunk_integration:` and then followed by `]`.
-# To add additional servers copy the below example server configuration from `[[fn_splunk_integration:splunk_label1]` to `verify_cert=false|/path/to/cert`.
-# Then paste it at the bottom on the app.config.
+# To add additional servers copy the below example server configuration from `[fn_splunk_integration:splunk_label1]` to `verify_cert=false|/path/to/cert`.
+# Then paste it at the bottom of the app.config.
 # Change the server label, `splunk_label1`, to a label helpful to define that server.
 # Then change the setting to those of the server you wish to add.
 
