5ca6faeb690df717bb349a0c3e42497e9a41d429
Travis CI User <travis@example.org>
2022-09-19 14:51:44 +0000

    Syncing external repository on 2022_09_19

---
+++ fn_microsoft_sentinel/fn_microsoft_sentinel/lib/constants.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# pragma pylint: disable=unused-argument, no-self-use
+# (c) Copyright IBM Corp. 2010, 2021. All Rights Reserved.
+
+# headers for comments
+FROM_SENTINEL_COMMENT_HDR = "From Sentinel"
+FROM_SOAR_COMMENT_HDR = "From IBM SOAR"
+
+SENT_TO_SENTINEL_HDR = "Sent to Sentinel"
+
+SENTINEL_INCIDENT_NUMBER = "sentinel_incident_number"
---
+++ fn_playbook_utils/fn_playbook_utils/util/selftest.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#(c) Copyright IBM Corp. 2010, 2021. All Rights Reserved.
+#pragma pylint: disable=unused-argument, no-self-use, line-too-long
+
+from resilient import get_client
+
+"""
+Function implementation test.
+Usage:
+    resilient-circuits selftest -l fn_playbook_utils
+    resilient-circuits selftest --print-env -l fn_playbook_utils
+
+Return examples:
+    return {
+        "state": "success",
+        "reason": "Successful connection to third party endpoint"
+    }
+
+    return {
+        "state": "failure",
+        "reason": "Failed to connect to third party endpoint"
+    }
+"""
+
+import logging
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+log.addHandler(logging.StreamHandler())
+
+def selftest_function(opts):
+    """
+    Test connectivity back to SOAR
+    """
+    try:
+        rest_client = get_client(opts)
+
+        return {
+            "state": "success",
+            "reason": None
+        }
+    except Exception as err:
+        return {
+            "state": "failure",
+            "reason": str(err)
+        }
