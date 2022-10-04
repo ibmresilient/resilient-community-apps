e4259e5768a0231636a412ff3fa6648b8ff09ccc
Mark Scherfling <mark.scherfling@ibm.com>
2022-10-04 11:51:42 -0400

    INT-4828 {sc_email_parser} line num fix

--- sc_email_parser/EmailParser.py
+++ sc_email_parser/EmailParser.py
@@ -13,7 +13,7 @@ SAVE_CONVERSATION = False
 # References to 'unicode' were removed which is a keyword that does not exist in Python 3.
 # Attempting to access an attribute that does not exist results in an error in the Python 3 scripting engine.
 # Use the hasattr function or a try/except block to check if an attribute exists before accessing it.
-# An example can be found on line 575
+# An example can be found on line 579
 
 # A script to create an incident from an email message, add artifacts to the incident based on information
 # present in the body of the message, and add any email attachments to the incident.
