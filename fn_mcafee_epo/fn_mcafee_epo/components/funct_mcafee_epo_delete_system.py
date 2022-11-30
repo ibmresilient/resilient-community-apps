3c484dac220d707797794c19639920df9599f5e2
Travis CI User <travis@example.org>
2022-11-30 23:05:40 +0000

    Syncing external repository on 2022_11_30

---
+++ fn_mcafee_epo/fn_mcafee_epo/components/funct_mcafee_epo_delete_system.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# pragma pylint: disable=unused-argument, no-self-use
+# (c) Copyright IBM Corp. 2010, 2022. All Rights Reserved.
+"""AppFunction implementation"""
+
+from resilient_circuits import AppFunctionComponent, app_function, FunctionResult
+from fn_mcafee_epo.lib.epo_helper import init_client, PACKAGE_NAME
+from resilient_lib import validate_fields
+
+FN_NAME = "mcafee_epo_delete_system"
+
+class FunctionComponent(AppFunctionComponent):
+    """Component that implements function 'mcafee_epo_delete_system'"""
+
+    def __init__(self, opts):
+        super(FunctionComponent, self).__init__(opts, PACKAGE_NAME)
+
+    @app_function(FN_NAME)
+    def _app_function(self, fn_inputs):
+        """
+        Function: Delete a system from the ePO server
+        Inputs:
+            -   fn_inputs.mcafee_epo_system_name_or_id
+            -   fn_inputs.mcafee_epo_uninstall_software
+            -   fn_inputs.mcafee_epo_uninstall
+        """
+
+        yield self.status_message(f"Starting App Function: '{FN_NAME}'")
+
+        # Validate parameter
+        validate_fields(["mcafee_epo_system_name_or_id"], fn_inputs)
+
+        # Log parameters
+        self.LOG.info(str(fn_inputs))
+
+        # Connect to ePO server
+        client = init_client(self.opts, self.options)
+
+        response = client.request(
+            "system.delete",
+            {"names": fn_inputs.mcafee_epo_system_name_or_id,
+            "uninstall": getattr(fn_inputs, "mcafee_epo_uninstall", False),
+            "uninstallSoftware": getattr(fn_inputs, "mcafee_epo_uninstall_software", False)}
+        )
+
+        yield self.status_message(f"Finished running App Function: '{FN_NAME}'")
+
+        # Produce a FunctionResult with the results
+        yield FunctionResult(response)
---
+++ fn_qradar_integration/fn_qradar_integration/components/qradar_find_reference_set_item.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+#
+# (c) Copyright IBM Corp. 2022. All Rights Reserved.
+#
+# pragma pylint: disable=unused-argument, no-self-use
+"""Function implementation"""
+
+import logging
+from resilient_circuits import ResilientComponent, function, handler, StatusMessage, FunctionResult, FunctionError
+from resilient_lib import validate_fields
+from fn_qradar_integration.util.qradar_utils import QRadarClient, QRadarServers
+import fn_qradar_integration.util.function_utils as function_utils
+
+LOG = logging.getLogger(__name__)
+
+class FunctionComponent(ResilientComponent):
+    """Component that implements Resilient function 'qradar_find_reference_set_item"""
+
+    def __init__(self, opts):
+        """constructor provides access to the configuration options"""
+        super(FunctionComponent, self).__init__(opts)
+        self.opts = opts
+        self.servers_list = function_utils.get_servers_list(opts)
+
+    @handler("reload")
+    def _reload(self, event, opts):
+        """Configuration options have changed, save new values"""
+        self.opts = opts
+        self.servers_list = function_utils.get_servers_list(opts)
+
+    @function("qradar_find_reference_set_item")
+    def _qradar_find_reference_set_item_function(self, event, *args, **kwargs):
+        """Function: Find an item from a given QRadar reference set"""
+        try:
+            # Get the wf_instance_id of the workflow this Function was called in, if not found return a backup string
+            wf_instance_id = event.message.get("workflow_instance", {}).get("workflow_instance_id", "no instance id found")
+            yield StatusMessage("Starting 'qradar_gfind_reference_set_item' that was running in workflow '{0}'".format(wf_instance_id))
+
+            validate_fields(["qradar_reference_set_name", "qradar_reference_set_item_value"], kwargs)
+            # Get the function parameters:
+            qradar_reference_set_name = kwargs.get("qradar_reference_set_name")  # text
+            qradar_reference_set_item_value = kwargs.get("qradar_reference_set_item_value")  # text
+            qradar_label = kwargs.get("qradar_label")  # text
+
+            LOG.info("qradar_reference_set_name: %s", qradar_reference_set_name)
+            LOG.info("qradar_reference_set_item_value: %s", qradar_reference_set_item_value)
+            LOG.info("qradar_label: %s", qradar_label)
+
+            options = QRadarServers.qradar_label_test(qradar_label, self.servers_list)
+            qradar_verify_cert = False if options.get("verify_cert", "false").lower() == "false" else options.get("verify_cert")
+
+            LOG.debug("Connection to {} using {}".format(options.get("host"),
+                                                         options.get("username") or "service token"))
+
+            qradar_client = QRadarClient(host=options.get("host"),
+                                         username=options.get("username", None),
+                                         password=options.get("qradarpassword", None),
+                                         token=options.get("qradartoken", None),
+                                         cafile=qradar_verify_cert,
+                                         opts=self.opts, function_opts=options)
+
+            results= qradar_client.search_ref_set(qradar_reference_set_name,
+                                                  qradar_reference_set_item_value)
+
+            yield StatusMessage("Finished 'qradar_find_reference_set_item' that was running in workflow '{0}'".format(wf_instance_id))
+
+            results["inputs"] = {
+                "qradar_reference_set_name": qradar_reference_set_name,
+                "qradar_reference_set_item_value": qradar_reference_set_item_value,
+                "qradar_label": qradar_label
+            }
+
+            # Produce a FunctionResult with the results
+            yield FunctionResult(results)
+        except Exception as e:
+            LOG.error(str(e))
+            yield FunctionError()
