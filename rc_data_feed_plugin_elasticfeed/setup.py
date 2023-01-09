7b078b3ccedaf189fb3127445f1f60cae9e0d2fb
Travis CI User <travis@example.org>
2023-01-09 21:51:45 +0000

    Syncing external repository on 2023_01_09

---
+++ rc_data_feed_plugin_elasticfeed/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='rc_data_feed_plugin_elasticfeed',
+    display_name='Data Feeder Elastic Plugin for SOAR',
+    version='1.1.0',
+    license='MIT',
+    author='IBM SOAR',
+    author_email='',
+    url='http://ibm.biz/resilientcommunity',
+    description="IBM SOAR Components for sending data feeds to other (BI) systems",
+    long_description="IBM SOAR Components for sending data feeds to other (BI) systems",
+    install_requires=[
+        'rc_data_feed>=2.2.1',
+        'elasticsearch>=8.3',
+        'resilient'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    classifiers=[
+        'Programming Language :: Python',
+    ],
+    entry_points={
+        "resilient.circuits.configsection": ["gen_config = data_feeder_plugins.elasticfeed.util.config:config_section_data"],
+        "resilient.circuits.apphost.configsection": ["gen_config = data_feeder_plugins.elasticfeed.util.config:apphost_config_section_data"],
+        "resilient.circuits.customize": ["customize = data_feeder_plugins.elasticfeed.util.customize:customization_data"],
+        "resilient.circuits.selftest": ["selftest = data_feeder_plugins.elasticfeed.util.selftest:selftest_function"],
+    },
+    tests_require=["pytest < 4.0.0",
+                   "pytest_resilient_circuits"]
+)
