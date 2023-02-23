4a46946d440646c014c3eb4572fa52a04c786458
Richard Swierk <Richard.swierk@ibm.com>
2023-02-23 10:55:51 -0500

    SOARAPPS-6301 {fn_jira} Update

--- fn_jira/fn_jira/poller/poller.py
+++ fn_jira/fn_jira/poller/poller.py
@@ -340,6 +340,8 @@ class PollerComponent(AppFunctionComponent):
         :param jira_issue: Dict of Jira issue data
         :return: None
         """
+        self.set_poller_templates(jira_issue.get("jira_server"))
+
         soar_create_payload = make_payload_from_template(
             self.set_poller_templates(jira_issue.get("jira_server"), "create_case"),
             CREATE_CASE_TEMPLATE,
@@ -394,6 +396,8 @@ class PollerComponent(AppFunctionComponent):
             jira = update[0]
             soar = update[1]
 
+            self.set_poller_templates(jira.get("jira_server"))
+
             payload = make_payload_from_template(
                 self.set_poller_templates(jira.get("jira_server"), "update_case"),
                 UPDATE_CASE_TEMPLATE,
@@ -431,6 +435,8 @@ class PollerComponent(AppFunctionComponent):
         for update in soar_tasks_to_update:
             jira = update[0]
             task = update[1]
+
+            self.set_poller_templates(jira.get("jira_server"))
 
             jira_issue_description = jira.get("fields").get("description")
             link_end_index = jira_issue_description.index("\n\n")+2
