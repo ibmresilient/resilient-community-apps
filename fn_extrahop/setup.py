0953fee456c46c6d052ce6d019531f0340fac1ac
AnnMarie Norcross <annmarie.meier.norcross@ibm.com>
2023-05-17 08:10:41 -0400

    SOARAPPS-6611 {fn_extrahop} version update [build-dev-app][dev-deps]

--- fn_extrahop/setup.py
+++ fn_extrahop/setup.py
@@ -23,7 +23,7 @@ def snake_to_camel(word):
 setup(
     name="fn_extrahop",
     display_name="ExtraHop for IBM SOAR",
-    version="1.0.0",
+    version="1.1.0",
     license="MIT",
     author="IBM SOAR",
     author_email="",
