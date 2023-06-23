5e02b9246c9e7eaae028e9552f3cf1e89e907ade
Travis CI User <travis@example.org>
2023-06-23 16:33:58 +0000

    Syncing external repository on 2023_06_23

---
+++ fn_qradar_enhanced_data/fn_qradar_enhanced_data/util/qradar_constants.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# (c) Copyright IBM Corp. 2010, 2023. All Rights Reserved.
+#   Qradar constants
+
+# URL from https://qradar_instance/api/
+
+ARIEL_SEARCHES = "ariel/searches"
+ARIEL_SEARCHES_RESULT = "ariel/searches/{}/results"
+ARIEL_SEARCHES_DELETE = "ariel/searches/{}"
+GRAPHQL_ARIEL_SEARCHES = "console/graphql/api/ariel/searches"
+GRAPHQL_ARIEL_SEARCHES_RESULT = "console/graphql/api/ariel/searches/{}/results"
+REFERENCE_SET_URL = "reference_data/sets"
+OFFENSES_URL = "siem/offenses"
+RULES_EXPLORER_URL = "console/plugins/{}/app_proxy/api/rules_explorer"
+GRAPHQL_URL = "console/graphql"
+GRAPHQL_BASICAUTH = "console/login"
+PACKAGE_NAME = "fn_qradar_integration"
+GLOBAL_SETTINGS = "fn_qradar_integration:edm_global_settings"
+
+SEARCH_STATUS_COMPLETED = "COMPLETED"
+SEARCH_STATUS_WAIT = "WAIT"
+
+ARIEL_SEARCH_EVENTS ="events"
+
+ARIEL_SEARCH_FLOWS ="flows"
---
+++ fn_slack/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import glob
+import ntpath
+
+
+def get_module_name(module_path):
+    """
+    Return the module name of the module path
+    """
+    return ntpath.split(module_path)[1].split(".")[0]
+
+
+def snake_to_camel(word):
+    """
+    Convert a word from snake_case to CamelCase
+    """
+    return ''.join(x.capitalize() or '_' for x in word.split('_'))
+
+
+setup(
+    name='fn_slack',
+    display_name='Slack Integration for SOAR',
+    version='2.0.0',
+    license='MIT',
+    author='IBM SOAR',
+    author_email='',
+    url='https://ibm.com/mysupport',
+    description="SOAR App for 'fn_slack'",
+    long_description="Function creates a Slack message based on a SOAR Incident, it's Tasks, Notes, Artifacts "
+                     "and Attachments, exports conversation history from Slack channel to a text file, saves the "
+                     "text file as an Attachment and archives the Slack channel.",
+    install_requires=[
+        'resilient_circuits>=45.0.0',
+        'slackclient~=2.9.4'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    classifiers=[
+        'Programming Language :: Python',
+    ],
+    entry_points={
+        "resilient.circuits.components": [
+            "{}FunctionComponent = fn_slack.components.{}:FunctionComponent".format(snake_to_camel(get_module_name(filename)), get_module_name(filename)) for filename in glob.glob("./fn_slack/components/[a-zA-Z]*.py")
+        ],
+        "resilient.circuits.configsection": ["gen_config = fn_slack.util.config:config_section_data"],
+        "resilient.circuits.customize": ["customize = fn_slack.util.customize:customization_data"],
+        "resilient.circuits.selftest": ["selftest = fn_slack.util.selftest:selftest_function"]
+    }
+)
