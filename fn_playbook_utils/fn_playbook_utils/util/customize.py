9c9ab60c304a60c07d94faa65571a65b8fa7a696
Allen Lee <Allen.CH.Lee@ibm.com>
2023-07-11 10:51:12 +0800

    SOARAPPS-6680 {fn_playbook_utils} add api key permission [build-dev-app]

--- fn_playbook_utils/fn_playbook_utils/util/customize.py
+++ fn_playbook_utils/fn_playbook_utils/util/customize.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# Generated with resilient-sdk v49.0.4423
 
 """Generate the Resilient customizations required for fn_playbook_utils"""
 
@@ -22,15 +23,15 @@ def codegen_reload_data():
         "package": u"fn_playbook_utils",
         "message_destinations": [u"fn_playbook_utils"],
         "functions": [u"pb_export_playbook", u"pb_get_playbook_data", u"pb_get_playbooks", u"pb_get_workflow_content", u"pb_get_workflow_data", u"pb_import_playbook"],
-        "workflows": [u"pb_get_workflows_by_artifact_value_for_last_30_days", u"wf_get_workflow_data", u"wf_get_workflow_frequency", u"wf_get_workflow_usage_at_incident_close", u"wf_get_workflows_by_artifact_value", u"wf_get_workflows_by_attachment_filename", u"wf_get_workflows_by_task_name"],
-        "actions": [u"PB: Get workflow/playbook frequency", u"PB: Get workflow/playbook usage", u"PB: Get workflow/playbook usage at incident close", u"PB: Get workflows/playbooks by artifact value", u"PB: Get workflows/playbooks by artifact value for last 30 days", u"PB: Get workflows/playbooks by attachment name", u"PB: Get workflows/playbooks by task name"],
+        "workflows": [],
+        "actions": [],
         "incident_fields": [],
         "incident_artifact_types": [],
         "incident_types": [],
         "datatables": [u"workflow_usage"],
         "automatic_tasks": [],
         "scripts": [u"PB: Display playbook data", u"PB: Display workflow data"],
-        "playbooks": []
+        "playbooks": [u"pb_get_workflowplaybook_usage_at_incident_close_pb", u"pb_get_workflowplaybook_usage_pb", u"pb_get_workflowplaybooks_frequency_pb", u"pb_get_workflowsplaybooks_by_artifact_value_for_last_30_days_pb", u"pb_get_workflowsplaybooks_by_artifact_value_pb", u"pb_get_workflowsplaybooks_by_attachment_filename_pb", u"pb_get_workflowsplaybooks_by_task_name_pb"]
     }
 
 
@@ -39,7 +40,7 @@ def customization_data(client=None):
     Returns a Generator of ImportDefinitions (Customizations).
     Install them using `resilient-circuits customize`
 
-    IBM SOAR Platform Version: 45.0.7899
+    IBM SOAR Platform Version: 46.0.8131
 
     Contents:
     - Message Destinations:
@@ -51,22 +52,14 @@ def customization_data(client=None):
         - pb_get_workflow_content
         - pb_get_workflow_data
         - pb_import_playbook
-    - Workflows:
-        - pb_get_workflows_by_artifact_value_for_last_30_days
-        - wf_get_workflow_data
-        - wf_get_workflow_frequency
-        - wf_get_workflow_usage_at_incident_close
-        - wf_get_workflows_by_artifact_value
-        - wf_get_workflows_by_attachment_filename
-        - wf_get_workflows_by_task_name
-    - Rules:
-        - PB: Get workflow/playbook frequency
-        - PB: Get workflow/playbook usage
-        - PB: Get workflow/playbook usage at incident close
-        - PB: Get workflows/playbooks by artifact value
-        - PB: Get workflows/playbooks by artifact value for last 30 days
-        - PB: Get workflows/playbooks by attachment name
-        - PB: Get workflows/playbooks by task name
+    - Playbooks:
+        - pb_get_workflowplaybook_usage_at_incident_close_pb
+        - pb_get_workflowplaybook_usage_pb
+        - pb_get_workflowplaybooks_frequency_pb
+        - pb_get_workflowsplaybooks_by_artifact_value_for_last_30_days_pb
+        - pb_get_workflowsplaybooks_by_artifact_value_pb
+        - pb_get_workflowsplaybooks_by_attachment_filename_pb
+        - pb_get_workflowsplaybooks_by_task_name_pb
     - Data Tables:
         - workflow_usage
     - Scripts:
