b996aa3fb88f17b1c0b7c75ba723ec1909de1ae1
AnnMarie Norcross <annmarie.meier.norcross@ibm.com>
2023-07-18 10:36:50 -0400

    SOARAPPS-6896 {fn_salesforce} add custom fields for account and contact name [build-dev-app][dev-deps]

--- fn_salesforce/fn_salesforce/util/customize.py
+++ fn_salesforce/fn_salesforce/util/customize.py
@@ -25,7 +25,7 @@ def codegen_reload_data():
         "functions": [u"salesforce_get_account", u"salesforce_get_case", u"salesforce_get_case_comments", u"salesforce_get_contact", u"salesforce_get_user", u"salesforce_update_case_status"],
         "workflows": [],
         "actions": [],
-        "incident_fields": [u"salesforce_account_id", u"salesforce_case_id", u"salesforce_case_link", u"salesforce_case_number", u"salesforce_case_type", u"salesforce_contact_email", u"salesforce_contact_fax", u"salesforce_contact_id", u"salesforce_contact_phone", u"salesforce_origin", u"salesforce_owner_id", u"salesforce_reason", u"salesforce_status", u"salesforce_supplied_company", u"salesforce_supplied_email", u"salesforce_supplied_name", u"salesforce_supplied_phone"],
+        "incident_fields": [u"salesforce_account_name",u"salesforce_contact_name",u"salesforce_account_id", u"salesforce_case_id", u"salesforce_case_link", u"salesforce_case_number", u"salesforce_case_type", u"salesforce_contact_email", u"salesforce_contact_fax", u"salesforce_contact_id", u"salesforce_contact_phone", u"salesforce_origin", u"salesforce_owner_id", u"salesforce_reason", u"salesforce_status", u"salesforce_supplied_company", u"salesforce_supplied_email", u"salesforce_supplied_name", u"salesforce_supplied_phone"],
         "incident_artifact_types": [],
         "incident_types": [],
         "datatables": [],
