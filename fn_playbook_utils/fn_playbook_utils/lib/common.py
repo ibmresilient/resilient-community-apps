d029c10144c90b21ef5d5e63a1421b57ac80529b
Allen Lee <Allen.CH.Lee@ibm.com>
2023-07-28 16:41:55 +0800

    SOARAPPS-6680 {fn_playbook_utils} update [build-dev-app]

--- fn_playbook_utils/fn_playbook_utils/lib/common.py
+++ fn_playbook_utils/fn_playbook_utils/lib/common.py
@@ -64,7 +64,7 @@ def parse_inputs(restclient, fn_inputs):
     min_date = fn_inputs.pb_min_incident_date if hasattr(fn_inputs, 'pb_min_incident_date') else None
     max_date = fn_inputs.pb_max_incident_date if hasattr(fn_inputs, 'pb_max_incident_date') else None
     
-    if min_id and max_id == None:
+    if not (min_id and max_id):
         min_id == 0
 
     # need at least min_id or min_date to avoid excessive calls
