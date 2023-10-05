18b245784b89e6acb5d53ba61b6c1dc2f011b2d9
Mark Scherfling <mark.scherfling@ibm.com>
2023-10-05 11:13:37 -0400

    SOARAPPS-383 fix for test send_email2

--- fn_outbound_email/tests/test_funct_send_email2.py
+++ fn_outbound_email/tests/test_funct_send_email2.py
@@ -5,7 +5,7 @@ from mock import patch
 import pytest
 from resilient_circuits.util import get_config_data, get_function_definition
 from resilient_circuits import SubmitTestFunction, FunctionResult
-from mock_incident import IncidentMock
+from .mock_incident import IncidentMock
 
 PACKAGE_NAME = "fn_outbound_email"
 FUNCTION_NAME = "send_email2"
