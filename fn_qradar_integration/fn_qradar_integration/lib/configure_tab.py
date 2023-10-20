8b1f650072035ac52d8e8e223ad7638d5eb78e42
Henry Chuang <hychuang@tw.ibm.com>
2023-10-20 12:01:55 +0800

    SOARAPPS-6999 Convert rules/workflows to playbooks - [build-dev-app]

---
+++ fn_qradar_integration/fn_qradar_integration/lib/configure_tab.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# pragma pylint: disable=unused-argument, no-self-use
+# (c) Copyright IBM Corp. 2010, 2022. All Rights Reserved.
+
+from resilient_lib.ui import Datatable, Tab, Field, create_tab
+from resilient_circuits.app import AppArgumentParser
+from fn_qradar_integration.lib.app_common import PACKAGE_NAME
+
+import logging
+
+LOG = logging.getLogger(__name__)
+
+TAB_NAME = "QRadar SIEM"
+
+class QRadarSIEMTab(Tab):
+    SECTION = PACKAGE_NAME
+    NAME = TAB_NAME
+
+    UUID = "781f26c5-1ae2-51c5-1f46-fc27a3fb5466"
+    CONTAINS = [
+        Field("qradar_id"),
+        Datatable("qradar_offense_event"),
+        Datatable("qradar_reference_set"),
+        Datatable("qradar_reference_table_queried_rows"),
+        Datatable("qradar_reference_table")
+    ]
+
+    SHOW_IF = [
+        Field("reaqta_id").conditions.has_value()
+    ]
+
+# Continue if exception is thrown
+def init_qradar_siem_tab():
+    try:
+        create_tab(QRadarSIEMTab, AppArgumentParser().parse_args(), update_existing=True)
+        LOG.info("Tab created: %s", TAB_NAME)
+    except SystemExit as e:
+        LOG.warning("Failed to create tab: %s.\nERROR: %s", TAB_NAME, e)
