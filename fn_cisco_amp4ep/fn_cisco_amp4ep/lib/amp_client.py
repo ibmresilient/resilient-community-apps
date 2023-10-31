4fb65fc1ef9026272b11e2fbea3c5a3e13e0ff89
Allen Lee <Allen.CH.Lee@ibm.com>
2023-10-31 15:39:26 +0800

    SOARAPPS-7232 fn_cisco_amp4ep 1.0.2 convert workflows/script to py3 [build-dev-app]

--- fn_cisco_amp4ep/fn_cisco_amp4ep/lib/amp_client.py
+++ fn_cisco_amp4ep/fn_cisco_amp4ep/lib/amp_client.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # pragma pylint: disable=unused-argument, no-self-use
 
-# (c) Copyright IBM Corp. 2010, 2018. All Rights Reserved.
+# (c) Copyright IBM Corp. 2010, 2023. All Rights Reserved.
 
 """ Class for Resilient circuits Functions supporting REST API client for Cisco AMP for endpoints  """
 import logging
