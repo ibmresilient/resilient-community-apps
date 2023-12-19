2c27a9b0c24872d08417c29a89fbae381362f05d
Travis CI User <travis@example.org>
2023-12-19 15:45:51 +0000

    Syncing external repository on 2023_12_19

---
+++ fn_aws_iam/fn_aws_iam/util/customize.py
@@ -0,0 +1,211 @@
+# -*- coding: utf-8 -*-
+# (c) Copyright IBM Corp. 2010, 2023. All Rights Reserved.
+# Generated with resilient-sdk v51.0.0.0.430
+
+"""Generate the SOAR customizations required for fn_aws_iam"""
+
+import base64
+import os
+import io
+try:
+    from resilient import ImportDefinition
+except ImportError:
+    # Support Apps running on resilient-circuits < v35.0.195
+    from resilient_circuits.util import ImportDefinition
+
+RES_FILE = "data/export.res"
+
+
+def codegen_reload_data():
+    """
+    Parameters required reload codegen for the fn_aws_iam package
+    """
+    return {
+        "package": u"fn_aws_iam",
+        "message_destinations": [
+            u"fn_aws_iam"
+        ],
+        "functions": [
+            u"fn_aws_iam_add_user_to_groups",
+            u"fn_aws_iam_attach_user_policies",
+            u"fn_aws_iam_deactivate_mfa_devices",
+            u"fn_aws_iam_delete_access_keys",
+            u"fn_aws_iam_delete_login_profile",
+            u"fn_aws_iam_delete_mfa_devices",
+            u"fn_aws_iam_delete_signing_certs",
+            u"fn_aws_iam_delete_ss_creds",
+            u"fn_aws_iam_delete_ssh_keys",
+            u"fn_aws_iam_delete_user",
+            u"fn_aws_iam_detach_user_policies",
+            u"fn_aws_iam_list_mfa_devices",
+            u"fn_aws_iam_list_signing_certs",
+            u"fn_aws_iam_list_ss_creds",
+            u"fn_aws_iam_list_ssh_keys",
+            u"fn_aws_iam_list_user_access_key_ids",
+            u"fn_aws_iam_list_user_groups",
+            u"fn_aws_iam_list_user_policies",
+            u"fn_aws_iam_list_users",
+            u"fn_aws_iam_remove_user_from_groups",
+            u"fn_aws_iam_update_access_key",
+            u"fn_aws_iam_update_login_profile"
+        ],
+        "workflows": [
+            u"wf_aws_iam_add_user_to_group",
+            u"wf_aws_iam_attach_user_policy",
+            u"wf_aws_iam_change_profile_password",
+            u"wf_aws_iam_deactivate_access_key",
+            u"wf_aws_iam_delete_access_key",
+            u"wf_aws_iam_delete_access_key_for_artifact",
+            u"wf_aws_iam_delete_access_keys",
+            u"wf_aws_iam_delete_login_profile",
+            u"wf_aws_iam_delete_user",
+            u"wf_aws_iam_delete_user_for_artifact",
+            u"wf_aws_iam_detach_all_user_policies",
+            u"wf_aws_iam_get_access_key_for_artifact",
+            u"wf_aws_iam_get_access_keys",
+            u"wf_aws_iam_get_user",
+            u"wf_aws_iam_get_user_for_artifact",
+            u"wf_aws_iam_list_access_keys",
+            u"wf_aws_iam_list_users",
+            u"wf_aws_iam_refresh_access_key",
+            u"wf_aws_iam_refresh_user",
+            u"wf_aws_iam_remove_user_from_all_groups"
+        ],
+        "actions": [
+            u"Example: AWS IAM: Add Access Key As Artifact",
+            u"Example: AWS IAM: Add User As Artifact",
+            u"Example: AWS IAM: Add User To Group",
+            u"Example: AWS IAM: Attach User Policy",
+            u"Example: AWS IAM: Change Profile Password",
+            u"Example: AWS IAM: Deactivate Access Key",
+            u"Example: AWS IAM: Delete Access Key",
+            u"Example: AWS IAM: Delete Access Key For Artifact",
+            u"Example: AWS IAM: Delete Access Keys",
+            u"Example: AWS IAM: Delete Login Profile",
+            u"Example: AWS IAM: Delete User",
+            u"Example: AWS IAM: Delete User For Artifact",
+            u"Example: AWS IAM: Detach All User Policies",
+            u"Example: AWS IAM: Get access Key For Artifact",
+            u"Example: AWS IAM: Get Access Keys",
+            u"Example: AWS IAM: Get User",
+            u"Example: AWS IAM: Get User For Artifact",
+            u"Example: AWS IAM: List Access Keys",
+            u"Example: AWS IAM: List Users",
+            u"Example: AWS IAM: Refresh Access Key",
+            u"Example: AWS IAM: Refresh User",
+            u"Example: AWS IAM: Remove User From All Groups"
+        ],
+        "incident_fields": [],
+        "incident_artifact_types": [
+            u"aws_iam_access_key_id",
+            u"aws_iam_user_name"
+        ],
+        "incident_types": [],
+        "datatables": [
+            u"aws_iam_access_keys",
+            u"aws_iam_users"
+        ],
+        "automatic_tasks": [],
+        "scripts": [
+            u"scr_aws_iam_add_access_key_as_artifact",
+            u"scr_aws_iam_add_user_as_artifact"
+        ],
+        "playbooks": [],
+    }
+
+
+def customization_data(client=None):
+    """
+    Returns a Generator of ImportDefinitions (Customizations).
+    Install them using `resilient-circuits customize`
+
+    IBM SOAR Platform Version: 48.2.16
+
+    Contents:
+    - Message Destinations:
+        - fn_aws_iam
+    - Functions:
+        - fn_aws_iam_add_user_to_groups
+        - fn_aws_iam_attach_user_policies
+        - fn_aws_iam_deactivate_mfa_devices
+        - fn_aws_iam_delete_access_keys
+        - fn_aws_iam_delete_login_profile
+        - fn_aws_iam_delete_mfa_devices
+        - fn_aws_iam_delete_signing_certs
+        - fn_aws_iam_delete_ss_creds
+        - fn_aws_iam_delete_ssh_keys
+        - fn_aws_iam_delete_user
+        - fn_aws_iam_detach_user_policies
+        - fn_aws_iam_list_mfa_devices
+        - fn_aws_iam_list_signing_certs
+        - fn_aws_iam_list_ss_creds
+        - fn_aws_iam_list_ssh_keys
+        - fn_aws_iam_list_user_access_key_ids
+        - fn_aws_iam_list_user_groups
+        - fn_aws_iam_list_user_policies
+        - fn_aws_iam_list_users
+        - fn_aws_iam_remove_user_from_groups
+        - fn_aws_iam_update_access_key
+        - fn_aws_iam_update_login_profile
+    - Workflows:
+        - wf_aws_iam_add_user_to_group
+        - wf_aws_iam_attach_user_policy
+        - wf_aws_iam_change_profile_password
+        - wf_aws_iam_deactivate_access_key
+        - wf_aws_iam_delete_access_key
+        - wf_aws_iam_delete_access_key_for_artifact
+        - wf_aws_iam_delete_access_keys
+        - wf_aws_iam_delete_login_profile
+        - wf_aws_iam_delete_user
+        - wf_aws_iam_delete_user_for_artifact
+        - wf_aws_iam_detach_all_user_policies
+        - wf_aws_iam_get_access_key_for_artifact
+        - wf_aws_iam_get_access_keys
+        - wf_aws_iam_get_user
+        - wf_aws_iam_get_user_for_artifact
+        - wf_aws_iam_list_access_keys
+        - wf_aws_iam_list_users
+        - wf_aws_iam_refresh_access_key
+        - wf_aws_iam_refresh_user
+        - wf_aws_iam_remove_user_from_all_groups
+    - Rules:
+        - Example: AWS IAM: Add Access Key As Artifact
+        - Example: AWS IAM: Add User As Artifact
+        - Example: AWS IAM: Add User To Group
+        - Example: AWS IAM: Attach User Policy
+        - Example: AWS IAM: Change Profile Password
+        - Example: AWS IAM: Deactivate Access Key
+        - Example: AWS IAM: Delete Access Key
+        - Example: AWS IAM: Delete Access Key For Artifact
+        - Example: AWS IAM: Delete Access Keys
+        - Example: AWS IAM: Delete Login Profile
+        - Example: AWS IAM: Delete User
+        - Example: AWS IAM: Delete User For Artifact
+        - Example: AWS IAM: Detach All User Policies
+        - Example: AWS IAM: Get access Key For Artifact
+        - Example: AWS IAM: Get Access Keys
+        - Example: AWS IAM: Get User
+        - Example: AWS IAM: Get User For Artifact
+        - Example: AWS IAM: List Access Keys
+        - Example: AWS IAM: List Users
+        - Example: AWS IAM: Refresh Access Key
+        - Example: AWS IAM: Refresh User
+        - Example: AWS IAM: Remove User From All Groups
+    - Custom Artifact Types:
+        - aws_iam_access_key_id
+        - aws_iam_user_name
+    - Data Tables:
+        - aws_iam_access_keys
+        - aws_iam_users
+    - Scripts:
+        - scr_aws_iam_add_access_key_as_artifact
+        - scr_aws_iam_add_user_as_artifact
+    """
+
+    res_file = os.path.join(os.path.dirname(__file__), RES_FILE)
+    if not os.path.isfile(res_file):
+        raise FileNotFoundError("{} not found".format(RES_FILE))
+
+    with io.open(res_file, mode='rt') as f:
+        b64_data = base64.b64encode(f.read().encode('utf-8'))
+        yield ImportDefinition(b64_data)
---
+++ fn_rest_api/fn_rest_api/components/funct_rest_api.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+# (c) Copyright IBM Corp. 2010 to 2023. All Rights Reserved.
+# pragma pylint: disable=unused-argument, no-self-use
+
+"""AppFunction implementation """
+import json
+from logging import getLogger
+
+from requests.exceptions import HTTPError
+from resilient_lib import render, validate_fields, IntegrationError
+from resilient_circuits import AppFunctionComponent, app_function, FunctionResult
+
+from fn_rest_api.lib.helper import make_rest_call, dedup_dict, render_dict_components
+from fn_rest_api.lib import authentication_handler
+
+FN_NAME = "rest_api"
+PACKAGE_NAME = "fn_rest_api"
+
+LOG = getLogger(__name__)
+
+class FunctionComponent(AppFunctionComponent):
+    """Component that implements function 'rest_api'"""
+
+    def __init__(self, opts):
+        super(FunctionComponent, self).__init__(opts, PACKAGE_NAME)
+
+    @app_function(FN_NAME)
+    def _app_function(self, fn_inputs):
+        """
+        Function: This function calls a REST web service. It supports the standard REST methods: 
+            GET, HEAD, POST, PUT, DELETE, OPTIONS, and PATCH.
+
+        The function parameters determine the type of call, the URL, and optionally the headers 
+        and body. The results include the text or structured (JSON) result from the web service,
+        and additional information including the elapsed time.
+
+        Inputs:
+        -------
+            -   fn_inputs.rest_api_body
+            -   fn_inputs.rest_api_url
+            -   fn_inputs.rest_api_method
+            -   fn_inputs.rest_api_timeout
+            -   fn_inputs.rest_api_cookies
+            -   fn_inputs.rest_api_allowed_status_codes
+            -   fn_inputs.rest_api_verify
+            -   fn_inputs.rest_api_headers
+
+            -   fn_inputs.oauth_token_url
+            -   fn_inputs.oauth_client_id
+            -   fn_inputs.oauth_client_secret
+            -   fn_inputs.oauth_redirect_uri
+            -   fn_inputs.oauth_scope
+            -   fn_inputs.oauth_code
+            -   fn_inputs.oauth_access_token
+            -   fn_inputs.oauth_refresh_token
+            -   fn_inputs.oauth_token_type
+
+            -   fn_inputs.client_auth_pem
+            -   fn_inputs.client_auth_key
+            -   fn_inputs.client_auth_cert
+        """
+        yield self.status_message(f"Starting App Function: '{FN_NAME}'")
+        validate_fields([
+            "rest_api_method",
+            "rest_api_url",
+            "rest_api_verify"
+            ], fn_inputs)
+
+        rest_properties = oauth_properties = certs_path = {}
+
+        # Get the function parameters: 
+        # Select, values: "GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"
+        rest_method  = self.get_select_param(getattr(fn_inputs, "rest_api_method")) 
+        rest_url     = getattr(fn_inputs, "rest_api_url") # text
+        rest_verify  = getattr(fn_inputs, "rest_api_verify") # boolean
+        rest_timeout = getattr(fn_inputs, "rest_api_timeout", 600) # Default timeout to 600 seconds
+        allowed_status_codes = getattr(fn_inputs, "rest_api_allowed_status_codes", None) # text
+
+        rest_properties = {
+            "headers" : self.get_textarea_param(getattr(fn_inputs, "rest_api_headers", None)), # textarea
+            "cookies" : self.get_textarea_param(getattr(fn_inputs, "rest_api_cookies", None)), # textarea
+            "body"    : self.get_textarea_param(getattr(fn_inputs, "rest_api_body", None))}    # textarea
+
+        # Properties required for OAuth Authentication
+        oauth_properties = {
+            authentication_handler.TOKEN_URL     : getattr(fn_inputs, "oauth_token_url", None),
+            authentication_handler.CLIENT_ID     : getattr(fn_inputs, "oauth_client_id", None),
+            authentication_handler.CLIENT_SECRET : getattr(fn_inputs, "oauth_client_secret", None),
+            authentication_handler.REDIRECT_URI  : getattr(fn_inputs, "oauth_redirect_uri", None),
+            authentication_handler.SCOPE         : getattr(fn_inputs, "oauth_scope", None),
+            authentication_handler.CODE          : getattr(fn_inputs, "oauth_code", None),
+            authentication_handler.ACCESS_TOKEN  : getattr(fn_inputs, "oauth_access_token", None),
+            authentication_handler.REFRESH_TOKEN : getattr(fn_inputs, "oauth_refresh_token", None),
+            authentication_handler.TOKEN_TYPE    : getattr(fn_inputs, "oauth_token_type", None)}
+
+        # Properties required for JWT Authentication
+        jwt_properties = {
+            authentication_handler.JWT_TOKEN     : getattr(fn_inputs, "jwt_token", None),
+            authentication_handler.JWT_KEY       : getattr(fn_inputs, "jwt_key", None),
+            authentication_handler.JWT_HEADERS   : getattr(fn_inputs, "jwt_headers", None),
+            authentication_handler.JWT_PAYLOAD   : getattr(fn_inputs, "jwt_payload", None),
+            authentication_handler.JWT_ALGORITHM : getattr(fn_inputs, "jwt_algorithm", None) }
+
+        # Properties required for Client-Side Authentication
+        cert_properties = {
+            authentication_handler.CLIENT_AUTH_CERT : getattr(fn_inputs, authentication_handler.CLIENT_AUTH_CERT, None),
+            authentication_handler.CLIENT_AUTH_KEY  : getattr(fn_inputs, authentication_handler.CLIENT_AUTH_KEY, None),
+            authentication_handler.CLIENT_AUTH_PEM  : getattr(fn_inputs, authentication_handler.CLIENT_AUTH_PEM, None)}
+
+        LOG.info(f"rest_method          : {rest_method}")
+        LOG.info(f"rest_url             : {rest_url}")
+        LOG.info(f"rest_verify          : {rest_verify}")
+        LOG.info(f"rest_timeout         : {rest_timeout}")
+        LOG.info(f"allowed_status_codes : {allowed_status_codes}")
+        LOG.info(f"rest_properties      : {json.dumps(rest_properties, indent=2)}")
+
+        # Rendering rest url with values from app.conf
+        rest_url = render(rest_url, self.options)
+
+        # Rendering any OAuth property that has values from app.conf
+        LOG.info("Rendering OAuth properties")
+        for key, value in oauth_properties.items():
+            if value:
+                oauth_properties[key] = render(value, self.options)
+
+        LOG.info("Rendering rest properties. If values contain SECRETS, they are incorporated.")
+        render_dict_components(rest_properties, self.options)
+
+        LOG.info("Rendering JWT properties. If values contain SECRETS, they are incorporated.")
+        render_dict_components(jwt_properties, self.options)
+
+        # Converting allowed_status_codes to a list of integers
+        allowed_status_codes = [int(x) for x in allowed_status_codes.split(",")] if allowed_status_codes else []
+
+        # Initializing OAuth handler
+        oauth_client     = authentication_handler.OAuth2Authorization(self.rc, oauth_properties)
+
+        # Performing OAuth authentication if Oauth parameters are provided
+        rest_properties  = authentication_handler.add_oauth_headers(oauth_client, rest_properties)
+        
+        # Initializing JWT handler
+        jwt_client       = authentication_handler.JWTHandler(jwt_properties)
+
+        # Generating jwt headers if jwt attributes are provided
+        rest_properties  = jwt_client.add_jwt_headers(rest_properties)
+        
+        # Generating certificates for client side authentication, if certificates are provided
+        rest_certificate = authentication_handler.check_certificate(cert_properties, certs_path)
+
+        LOG.debug(f"Request Header : {json.dumps(rest_properties.get('headers'), indent=2)}")
+        LOG.debug(f"Request Body   : {json.dumps(rest_properties.get('body'), indent=2)}")
+
+        retry = False
+        try:
+            LOG.info("Attempting call")
+            response = make_rest_call(
+                self.opts, self.options,
+                rest_method, rest_url,
+                rest_properties.get("headers"),
+                rest_properties.get("cookies"),
+                rest_properties.get("body"),
+                rest_verify,
+                rest_timeout,
+                rest_certificate,
+                allowed_status_codes)
+
+        except (HTTPError, IntegrationError) as err:
+            # If the error raised is either HTTPError else IntegrationError, sets retry to True
+            LOG.warning(str(err))
+            if oauth_client.check_oauth_ready():
+                retry = True
+            else:
+                raise IntegrationError(str(err))
+
+        if retry:
+            # Retrying request by attempting to refresh_tokens
+            oauth_client.force_refresh_tokens()
+            rest_properties = authentication_handler.add_oauth_headers(oauth_client, rest_properties)
+            response = make_rest_call(
+                self.opts, self.options,
+                rest_method, rest_url,
+                rest_properties.get("headers"),
+                rest_properties.get("cookies"),
+                rest_properties.get("body"),
+                rest_verify,
+                rest_timeout,
+                rest_certificate,
+                allowed_status_codes)
+
+        try:
+            response_json = response.json()
+            LOG.debug(json.dumps(response_json, indent=2))
+        except:
+            response_json = None
+
+        results = {
+            "ok"                : response.ok,
+            "url"               : response.url,
+            "status_code"       : response.status_code,
+            "reason"            : response.reason,
+            "cookies"           : dedup_dict(response.cookies),
+            "headers"           : dedup_dict(response.headers),
+            "elapsed"           : int(response.elapsed.total_seconds() * 1000.0),
+            "apparent_encoding" : response.apparent_encoding,
+            "text"              : response.text,
+            "json"              : response_json,
+            "links"             : response.links}
+
+        if len(certs_path) == 0:
+            LOG.info("No client authentication certificates detected.")
+        elif authentication_handler.delete_certificates(certs_path):
+            LOG.info("Successfully deleted client side authentication certificates.")
+        else:
+            LOG.warning(f"Unable to clear all client side authentication certificates {certs_path.keys()}.")
+
+        # Produce a FunctionResult with the results
+        yield FunctionResult(results)
