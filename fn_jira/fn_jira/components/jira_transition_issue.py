b901a5e040a8a6d218917f550f69005bee4fa06f
Travis CI User <travis@example.org>
2024-01-04 13:56:39 +0000

    Syncing external repository on 2024_01_04

---
+++ fn_jira/fn_jira/components/jira_transition_issue.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# pragma pylint: disable=unused-argument, no-self-use
+# (c) Copyright IBM Corp. 2010, 2023. All Rights Reserved.
+"""Transition a Jira issue from IBM SOAR"""
+
+from json import loads
+from fn_jira.util.helper import (PACKAGE_NAME,
+                                 get_server_settings, to_markdown)
+from resilient_circuits import (AppFunctionComponent, FunctionResult,
+                                app_function)
+from resilient_lib import validate_fields, IntegrationError
+from fn_jira.lib.app_common import AppCommon
+from jira.exceptions import JIRAError
+
+FN_NAME = "jira_transition_issue"
+
+class FunctionComponent(AppFunctionComponent):
+    """Component that implements SOAR function 'jira_transition_issue"""
+
+    def __init__(self, opts):
+        super(FunctionComponent, self).__init__(opts, PACKAGE_NAME)
+
+    @app_function(FN_NAME)
+    def _app_function(self, fn_inputs):
+        """
+        Function: Transition a jira issue.
+        Inputs:
+            -   fn_inputs.jira_issue_id
+            -   fn_inputs.jira_transition_id
+            -   fn_inputs.jira_fields
+            -   fn_inputs.jira_comment
+            -   fn_inputs.jira_label
+        """
+        yield self.status_message(f"Starting App Function: '{FN_NAME}'")
+
+        # Get configuration for Jira server specified
+        options = get_server_settings(self.opts, getattr(fn_inputs, "jira_label", None))
+
+        # Connect to Jira
+        yield self.status_message("Connecting to JIRA")
+        jira_client = AppCommon(self.opts, options).get_jira_client()
+
+        # Get + validate the function parameters:
+        self.LOG.info("Validating function inputs")
+        validate_fields(["jira_issue_id", "jira_transition_id"], fn_inputs)
+        self.LOG.info(f"Validated function inputs: {fn_inputs._asdict()}")
+
+        jira_fields = loads(getattr(fn_inputs, "jira_fields", "{}"))
+        jira_comment = to_markdown(getattr(fn_inputs, "jira_comment", None))
+        jira_issue_id = fn_inputs.jira_issue_id
+        jira_transition_id = fn_inputs.jira_transition_id
+
+        yield self.status_message(f"Transition issue {jira_issue_id} to '{jira_transition_id}'")
+
+        try:
+            # Transition Jira issue, update fields, and add comment
+            jira_client.transition_issue(issue=jira_issue_id, transition=jira_transition_id, fields=jira_fields, comment=jira_comment)
+        except JIRAError as err:
+            if err.status_code == 400 or "Invalid transition name." in err.text:
+                # Update fields of the Jira issue
+                jira_client.issue(jira_issue_id).update(comment=jira_comment, fields=jira_fields)
+                # Transition Jira Issue
+                jira_client.transition_issue(issue=jira_issue_id, transition=jira_transition_id)
+            else:
+                raise IntegrationError(str(err))
+        except Exception as er:
+            raise IntegrationError(str(er))
+
+        yield self.status_message(f"Finished running App Function: '{FN_NAME}'")
+
+        # Produce a FunctionResult with the results
+        yield FunctionResult("Done")
---
+++ fn_pagerduty/fn_pagerduty/util/config.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""Generate a default configuration-file section for fn_pagerduty"""
+
+from __future__ import print_function
+
+
+def config_section_data():
+    """Produce the default configuration section for app.config,
+       when called by `resilient-circuits config [-c|-u]`
+    """
+    config_data = u"""[pagerduty]
+api_token=<api_token>
+from_email=<from_email_address>
+# bypass https certificate validation (only set to False for testing purposes)
+verifyFlag=False
+resilient_client=IBM Resilient
+"""
+
+    return config_data
