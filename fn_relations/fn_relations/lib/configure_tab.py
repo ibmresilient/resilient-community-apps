961ac58f389398f46667b1d2c1b5a9111be7aa9c
Mark Scherfling <mark.scherfling@ibm.com>
2024-01-17 10:48:46 -0500

    SOARAPPS-7609 nested ChildIncidentTab

--- fn_relations/fn_relations/lib/configure_tab.py
+++ fn_relations/fn_relations/lib/configure_tab.py
@@ -4,40 +4,46 @@
 
 import logging
 
-from resilient_circuits.app import AppArgumentParser
 from resilient_lib.ui import Datatable, Field, Tab, create_tab, Section, HTMLBlock
 from resilient_lib.ui.common import update_summary_layout
 LOG = logging.getLogger(__name__)
 
 PACKAGE = "fn_relations"
 
-class ChildIncidentTab(Tab):
-    SECTION = PACKAGE
-    NAME = "Child Incidents"
-    UUID = "f831db07-27a0-b94e-aec7-2d599c68ad48"
-
-    CONTAINS = [
-        Datatable("dt_relations_child_incidents")
-    ]
-
-    SHOW_IF = [
-        Field("relations_level").conditions.equals("Parent")
-    ]
-
 # Continue if exception is thrown
 def init_relations_layout(opts):
+    
+    # this needs to be a nested class in order to use 'opts'
+    class ChildIncidentTab(Tab):
+      SECTION = PACKAGE
+      NAME = "Child Incidents"
+      UUID = "f831db07-27a0-b94e-aec7-2d599c68ad48"
+
+      CONTAINS = [
+          Datatable("dt_relations_child_incidents")
+      ]
+
+      SHOW_IF = [
+          SelectField("relations_level", opts).conditions.equals("Parent")
+      ]
+
+
     try:
         create_tab(ChildIncidentTab, opts, update_existing=True)
         LOG.info("Tab created: %s", ChildIncidentTab.NAME)
 
         section_list = [
-            Section([Field("relations_level")], 
-                    [Field("relations_level").conditions.equals("Parent")]),
             Section([
+                      HTMLBlock("<h3>Relations</h3>"),
+                      Field("relations_level")
+                    ], 
+                    [SelectField("relations_level", opts).conditions.equals("Parent")]),
+            Section([
+                      HTMLBlock("<h4>Relations</h4>"),
                       Field("relations_level"),
                       Field("relations_parent_id")
                     ],
-                    [Field("relations_level").conditions.equals("Child")])
+                    [SelectField("relations_level", opts).conditions.equals("Child")])
         ]
 
         update_summary_layout(opts,
@@ -48,56 +54,78 @@ def init_relations_layout(opts):
     except SystemExit as e:
         LOG.warning("Failed to create tab: %s.\nERROR: %s", ChildIncidentTab.NAME, e)
 
-# extra UUIDs if needed. found by running:
-# >>> import uuid
-# >>> uuid.uuid4()
-#
-# UUID('b27793a5-7a40-407f-a8a6-ab0376875813')
-# UUID('f986581c-93e7-4c71-a6fb-a0dd01993ff1')
+## temporary definitions until code in resilient-lib
+from resilient import get_client
+from resilient_lib.ui.conditions import FieldConditions
 
-    """
-    {
-      "element" : "section",
-      "field_type" : null,
-      "fields" : [ {
-        "content" : "Incident Relations",
-        "element" : "html",
-        "field_type" : null,
-        "show_if" : null,
-        "show_link_header" : false,
-        "step_label" : null
-      }, {
-        "content" : "relations_level",
-        "element" : "field",
-        "field_type" : "incident",
-        "show_if" : null,
-        "show_link_header" : false,
-        "step_label" : null
-      } ],
-      "show_if" : [ {
-        "condition" : "has_a_value",
-        "field" : "incident.relations_level",
-        "value" : null
-      } ],
-      "show_link_header" : false,
-      "step_label" : null
-    }, {
-      "element" : "section",
-      "field_type" : null,
-      "fields" : [ {
-        "content" : "relations_parent_id",
-        "element" : "field",
-        "field_type" : "incident",
-        "show_if" : null,
-        "show_link_header" : false,
-        "step_label" : null
-      } ],
-      "show_if" : [ {
-        "condition" : "has_a_value",
-        "field" : "incident.relations_parent_id",
-        "value" : null
-      } ],
-      "show_link_header" : false,
-      "step_label" : null
-    }
-    """
+class SelectFieldCondition(FieldConditions):
+    def __init__(self, api_name, res_client):
+        super(SelectFieldCondition, self).__init__(api_name)
+        self.client = res_client
+
+    def _get_value_for_select_field(self, value):
+        types = self.client.cached_get("/types/incident")
+        for value_mapping in types.get("fields", {}).get(self.api_name, {}).get("values", {}):
+            if value_mapping.get("label") == value:
+                return value_mapping.get("value")
+        raise ValueError("Couldn't find value {0} for select field {1}".format(value, self.api_name))
+
+    def has_value(self):
+        return {
+            "field": "incident.{}".format(self.api_name),
+            "condition": "has_a_value"
+        }
+
+    def equals(self, value):
+        return {
+            "field": "incident.{}".format(self.api_name),
+            "condition": "equals",
+            "value": self._get_value_for_select_field(value)
+        }
+
+    def contains(self, value):
+        return {
+            "field": "incident.{}".format(self.api_name),
+            "condition": "contains",
+            "value": self._get_value_for_select_field(value)
+        }
+
+    def doesnt_contain(self, value):
+        return {
+            "field": "incident.{}".format(self.api_name),
+            "condition": "not_contains",
+            "value": self._get_value_for_select_field(value)
+        }
+
+    def doesnt_equal(self, value):
+        return {
+            "field": "incident.{}".format(self.api_name),
+            "condition": "not_equals",
+            "value": self._get_value_for_select_field(value)
+        }
+
+    def doesnt_have_value(self):
+        return {
+            "field": "incident.{}".format(self.api_name),
+            "condition": "not_has_a_value"
+        }
+
+    def has_one_of(self, values):
+        return {
+            "field": "incident.{}".format(self.api_name),
+            "condition": "in",
+            "value": [self._get_value_for_select_field(value) for value in values]
+        }
+
+    def doesnt_have_one_of(self, values):
+        return {
+            "field": "incident.{}".format(self.api_name),
+            "condition": "not_in",
+            "value": [self._get_value_for_select_field(value) for value in values]
+        }
+
+class SelectField(Field):
+    def __init__(self, api_name, opts):
+        super(SelectField, self).__init__(api_name)
+        res_client = get_client(opts)
+        self.conditions = SelectFieldCondition(api_name=api_name, res_client=res_client)
