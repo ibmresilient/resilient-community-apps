86a255bf3a566fea749c1b909c04c8e8d572f0aa
Mark Scherfling <mark.scherfling@ibm.com>
2024-01-26 16:44:01 -0500

    SOARAPPS-6759 unit test fixes

--- rc_data_feed/tests/test_sync_data.py
+++ rc_data_feed/tests/test_sync_data.py
@@ -13,45 +13,48 @@ WORKSPACE_MAP_B = {
     "Default Workspace": "elastic_feed"
 }
 
-WORKSPACE_VERIZON = { 
-    "TMC workspace": ["postgres_feed:label1"], 
-    "RMC VCG workspace": ["postgres_feed:label2"], 
-    "RED workspace": ["postgres_feed:label3"], 
-    "Legal workspace": ["postgres_feed:label4"], 
-    "GSOC workspace": ["postgres_feed:label5"], 
-    "EVM workspace": ["postgres_feed:label6"], 
-    "ASIRT workspace": ["postgres_feed:label7"], 
-    "ITO workspace": ["postgres_feed:label8"], 
-    "TF Workspace": ["postgres_feed:label9"], 
-    "NCC workspace": ["postgres_feed:label10"], 
+WORKSPACE_VERIZON = {
+    "TMC workspace": ["postgres_feed:label1"],
+    "RMC VCG workspace": ["postgres_feed:label2"],
+    "RED workspace": ["postgres_feed:label3"],
+    "Legal workspace": ["postgres_feed:label4"],
+    "GSOC workspace": ["postgres_feed:label5"],
+    "EVM workspace": ["postgres_feed:label6"],
+    "ASIRT workspace": ["postgres_feed:label7"],
+    "ITO workspace": ["postgres_feed:label8"],
+    "TF Workspace": ["postgres_feed:label9"],
+    "NCC workspace": ["postgres_feed:label10"],
     "DF Workspace": ["postgres_feed:label11"]
 }
 
-VERIZON_FEEDS = ['postgres_feed:label1', 'postgres_feed:label2', 'postgres_feed:label3', 'postgres_feed:label4', 'postgres_feed:label5', 'postgres_feed:label6', 'postgres_feed:label7', 'postgres_feed:label8', 'postgres_feed:label9', 'postgres_feed:label10', 'postgres_feed:label11']
+VERIZON_FEEDS = ','.join(['postgres_feed:label1', 'postgres_feed:label2', 'postgres_feed:label3', 'postgres_feed:label4', 'postgres_feed:label5', 'postgres_feed:label6', 'postgres_feed:label7', 'postgres_feed:label8', 'postgres_feed:label9', 'postgres_feed:label10', 'postgres_feed:label11'])
 
 def test_is_workspace_valid_success():
 
-    pool1 = PluginPool(None, 
-                      None, 
-                      ["elastic_feed"],
+    pool1 = PluginPool(None,
+                      None,
+                      "",
+                      {},
                       WORKSPACE_MAP,
                       False)
 
     assert pool1._is_workspace_valid("Default Workspace", "elastic_feed")
     assert pool1._is_workspace_valid("WorkspaceB", "elastic_feed")
 
-    pool2 = PluginPool(None, 
-                      None, 
-                      [],
+    pool2 = PluginPool(None,
+                      None,
+                      "",
+                      {},
                       None,
                       False)
 
     assert pool2._is_workspace_valid("Default Workspace", "elastic_feed")
     assert pool2._is_workspace_valid("WorkspaceX", "elastic_feed")
 
-    pool3 = PluginPool(None, 
-                      None, 
-                      ["elastic_feed"],
+    pool3 = PluginPool(None,
+                      None,
+                      "",
+                      {},
                       WORKSPACE_MAP_B,
                       False)
 
@@ -60,9 +63,10 @@ def test_is_workspace_valid_success():
 
 def test_is_workspace_valid_fail():
 
-    pool1 = PluginPool(None, 
-                      None, 
-                      ["elastic_feed"],
+    pool1 = PluginPool(None,
+                      None,
+                      "",
+                      {},
                       WORKSPACE_MAP,
                       False)
 
@@ -70,9 +74,10 @@ def test_is_workspace_valid_fail():
     assert not pool1._is_workspace_valid("WorkspaceA", "feed_not_found")
     assert not pool1._is_workspace_valid("WorkspaceB", "feed_not_found")
 
-    pool1 = PluginPool(None, 
-                      None, 
-                      ["elastic_feed"],
+    pool1 = PluginPool(None,
+                      None,
+                      "",
+                      {},
                       WORKSPACE_MAP_B,
                       False)
 
@@ -80,18 +85,20 @@ def test_is_workspace_valid_fail():
 
 def test_is_verizon_valid_success():
 
-    pool1 = PluginPool(None, 
-                      None, 
-                      VERIZON_FEEDS,
+    pool1 = PluginPool(None,
+                      None,
+                      "",
+                      {},
                       WORKSPACE_VERIZON,
                       False)
 
     assert pool1._is_workspace_valid("TMC workspace", "postgres_feed:label1")
     assert pool1._is_workspace_valid("DF Workspace", "postgres_feed:label11")
 
-    pool2 = PluginPool(None, 
-                      None, 
-                      [],
+    pool2 = PluginPool(None,
+                      None,
+                      "",
+                      {},
                       None,
                       False)
 
@@ -101,9 +108,10 @@ def test_is_verizon_valid_success():
 
 def test_is_verizon_valid_fail():
 
-    pool1 = PluginPool(None, 
-                      None, 
-                      VERIZON_FEEDS,
+    pool1 = PluginPool(None,
+                      None,
+                      "",
+                      {},
                       WORKSPACE_VERIZON,
                       False)
 
@@ -111,9 +119,10 @@ def test_is_verizon_valid_fail():
     assert not pool1._is_workspace_valid("RMC VCG workspace", "postgres_feed:label1")
     assert not pool1._is_workspace_valid("DF Workspace", "feed_not_found")
 
-    pool1 = PluginPool(None, 
-                      None, 
-                      VERIZON_FEEDS,
+    pool1 = PluginPool(None,
+                      None,
+                      "",
+                      {},
                       WORKSPACE_VERIZON,
                       False)
 
