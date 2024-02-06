bd60728dcb9a9d6d094bad313aa6a3ca7f0c476a
Travis CI User <travis@example.org>
2024-02-06 16:43:30 +0000

    Syncing external repository on 2024_02_06

---
+++ fn_sep/fn_sep/components/funct_sep_cancel_a_command.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+"""AppFunction implementation"""
+
+import json
+from resilient_circuits import AppFunctionComponent, app_function, FunctionResult
+from resilient_lib import validate_fields
+from fn_sep.lib.symantec_sep_client import Symantec_SEP
+
+PACKAGE_NAME = "fn_sep"
+FN_NAME = "sep_cancel_a_command"
+
+
+class FunctionComponent(AppFunctionComponent):
+    """Component that implements function 'sep_cancel_a_command'"""
+
+    def __init__(self, opts):
+        super(FunctionComponent, self).__init__(opts, PACKAGE_NAME)
+
+    @app_function(FN_NAME)
+    def _app_function(self, fn_inputs):
+        """
+        Function: Cancels an existing command by creating a new cancel command for clients for which the command is still pending.
+        Inputs:
+            -   fn_inputs.sep_command_id
+        """
+
+        yield self.status_message(f"Starting App Function: '{FN_NAME}'")
+
+        try:
+            validate_fields(['sep_command_id'], fn_inputs)
+
+            sep_client = Symantec_SEP(self.options, self.rc)
+
+            input_data = fn_inputs._asdict()
+
+            url = f"/command-queue/{input_data['sep_command_id']}/cancel"
+            method = "POST"
+                
+            response,err = sep_client.make_api_call(method, url,use_callback=True)
+
+            yield self.status_message(f"Finished running App Function: '{FN_NAME}'")
+            if err == None:
+                yield FunctionResult(response)
+            else:
+                raise ValueError(err)
+
+        except Exception as err_message:
+            response = {}
+            yield self.status_message("Error in Symantec SEP integration: {err}.".format(err=err_message))
+            err_message = json.dumps(err_message, default=str)
+            yield FunctionResult(response, success=False, reason=err_message)
