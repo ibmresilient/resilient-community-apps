e4301471ad07f6b00656919c0c0d16ad11643bfc
Travis CI User <travis@example.org>
2025-02-28 19:22:16 +0000

    Syncing external repository on 2025_02_28

---
+++ fn_log_capture/fn_log_capture/util/config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Generate a default configuration-file section for fn_log_capture"""
+
+from __future__ import print_function
+
+
+def config_section_data():
+    """Produce the default configuration section for app.config,
+       when called by `resilient-circuits config [-c|-u]`
+    """
+    return None
