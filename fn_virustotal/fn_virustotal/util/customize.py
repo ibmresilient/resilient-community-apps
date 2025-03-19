827f9ba0aa078b69812b5b7773a2ed843c59bd7c
Travis CI User <travis@example.org>
2025-03-19 18:25:57 +0000

    Syncing external repository on 2025_03_19

---
+++ fn_virustotal/fn_virustotal/util/customize.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+# <<PUT YOUR COPYRIGHT TEXT HERE>>
+# Generated with resilient-sdk v51.0.1.1.824
+
+"""Generate the SOAR customizations required for fn_virustotal"""
+
+import base64
+import os
+import io
+try:
+    from resilient import ImportDefinition
+except ImportError:
+    # Support Apps running on resilient-circuits < v35.0.195
+    from resilient_circuits.util import ImportDefinition
+
+RES_FILE = "data/export.res"
+
+
+def codegen_reload_data():
+    """
+    Parameters required reload codegen for the fn_virustotal package
+    """
+    return {
+        "package": u"fn_virustotal",
+        "message_destinations": [
+            u"fn_virustotal"
+        ],
+        "functions": [
+            u"virustotal"
+        ],
+        "workflows": [],
+        "actions": [],
+        "incident_fields": [],
+        "incident_artifact_types": [],
+        "incident_types": [],
+        "datatables": [],
+        "automatic_tasks": [],
+        "scripts": [],
+        "playbooks": [
+            u"virustotal_scan_artifact",
+            u"virustotal_scan_attachment"
+        ]
+    }
+
+
+def customization_data(client=None):
+    """
+    Returns a Generator of ImportDefinitions (Customizations).
+    Install them using `resilient-circuits customize`
+
+    IBM SOAR Platform Version: 50.0.9097
+
+    Contents:
+    - Message Destinations:
+        - fn_virustotal
+    - Functions:
+        - virustotal
+    - Playbooks:
+        - virustotal_scan_artifact
+        - virustotal_scan_attachment
+    """
+
+    res_file = os.path.join(os.path.dirname(__file__), RES_FILE)
+    if not os.path.isfile(res_file):
+        raise FileNotFoundError("{} not found".format(RES_FILE))
+
+    with io.open(res_file, mode='rt') as f:
+        b64_data = base64.b64encode(f.read().encode('utf-8'))
+        yield ImportDefinition(b64_data)
