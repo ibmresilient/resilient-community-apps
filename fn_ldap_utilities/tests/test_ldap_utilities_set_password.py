bf07f2fbdb6767570c34b12d791ce3f6e6b2d1d5
Travis CI User <travis@example.org>
2025-04-21 17:35:33 +0000

    Syncing external repository on 2025_04_21

---
+++ fn_ldap_utilities/tests/test_ldap_utilities_set_password.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# (c) Copyright IBM Corp. 2010, 2024. All Rights Reserved.
+"""Tests using pytest_resilient_circuits"""
+
+from __future__ import print_function
+import pytest
+from mock import patch
+from fn_ldap_utilities.util.helper import PACKAGE_NAME
+from resilient_circuits.util import get_function_definition
+from helper import TestingHelper, get_mock_config_data
+from resilient_circuits import SubmitTestFunction, FunctionResult
+
+FUNCTION_NAME = "ldap_utilities_set_password"
+
+# Read the default configuration-data section from the package
+config_data = get_mock_config_data()
+
+# Provide a simulation of the SOAR REST API (uncomment to connect to a real appliance)
+resilient_mock = "pytest_resilient_circuits.BasicResilientMock"
+
+def call_ldap_utilities_set_password_function(circuits, function_params, timeout=10):
+    # Fire a message to the function
+    evt = SubmitTestFunction(FUNCTION_NAME, function_params)
+    circuits.manager.fire(evt)
+    event = circuits.watcher.wait(FUNCTION_NAME+"_result", parent=evt, timeout=timeout)
+    assert event
+    assert isinstance(event.kwargs["result"], FunctionResult)
+    pytest.wait_for(event, "complete", True)
+    return event.kwargs["result"].value
+
+class TestLdapUtilitiesSetPassword:
+    """ Tests for the ldap_utilities_set_password function"""
+
+    helper = TestingHelper()
+
+    def test_function_definition(self):
+        """ Test that the package provides customization_data that defines the function """
+        func = get_function_definition(PACKAGE_NAME, FUNCTION_NAME)
+        assert func
+
+    inputs = {
+        "ldap_dn": "CN=Test User8,CN=Users,dc=example,DC=com",
+        "ldap_new_password": "Passw8rd!",
+        "ldap_return_new_password": False
+    }
+
+    outputs = {
+        "success": True,
+        "content": {
+            "user_dn": "CN=Test User8,CN=Users,dc=example,DC=com"
+        }
+    }
+
+    @patch('fn_ldap_utilities.util.helper.Connection', helper.mocked_connection())
+    @patch('fn_ldap_utilities.util.helper.Server', helper.mocked_server())
+    @pytest.mark.parametrize("mock_inputs, expected_results", [(inputs, outputs)])
+    def test_success(self, circuits_app, mock_inputs, expected_results):
+        """ Test calling with sample values for the parameters """
+        results = call_ldap_utilities_set_password_function(circuits_app, mock_inputs)
+        for expected_result in expected_results:
+            assert expected_result in results
---
+++ fn_mxtoolbox/fn_mxtoolbox/util/customize.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+# <<PUT YOUR COPYRIGHT TEXT HERE>>
+# Generated with resilient-sdk v51.0.5.0.1475
+
+"""Generate the SOAR customizations required for fn_mxtoolbox"""
+
+import base64
+import os
+import io
+try:
+    from resilient import ImportDefinition
+except ImportError:
+    # Support Apps running on resilient-circuits < v35.0.195
+    from resilient_circuits.util import ImportDefinition
+
+RES_FILE = "data/export.res"
+
+
+def codegen_reload_data():
+    """
+    Parameters required reload codegen for the fn_mxtoolbox package
+    """
+    return {
+        "package": u"fn_mxtoolbox",
+        "message_destinations": [
+            u"fn_mxtoolbox"
+        ],
+        "functions": [
+            u"fn_mxtoolbox"
+        ],
+        "workflows": [
+            u"example_mxtoolbox_mx_query"
+        ],
+        "actions": [
+            u"Example: MXToolbox MX query"
+        ],
+        "incident_fields": [],
+        "incident_artifact_types": [],
+        "incident_types": [],
+        "datatables": [],
+        "automatic_tasks": [],
+        "scripts": [],
+        "playbooks": [],
+    }
+
+
+def customization_data(client=None):
+    """
+    Returns a Generator of ImportDefinitions (Customizations).
+    Install them using `resilient-circuits customize`
+
+    IBM SOAR Platform Version: 51.0.0.0.9339
+
+    Contents:
+    - Message Destinations:
+        - fn_mxtoolbox
+    - Functions:
+        - fn_mxtoolbox
+    - Workflows:
+        - example_mxtoolbox_mx_query
+    - Rules:
+        - Example: MXToolbox MX query
+    """
+
+    res_file = os.path.join(os.path.dirname(__file__), RES_FILE)
+    if not os.path.isfile(res_file):
+        raise FileNotFoundError("{} not found".format(RES_FILE))
+
+    with io.open(res_file, mode='rt') as f:
+        b64_data = base64.b64encode(f.read().encode('utf-8'))
+        yield ImportDefinition(b64_data)
