3339fa59305229009b15acb9968c2974b36c6baf
Travis CI User <travis@example.org>
2025-06-26 13:03:48 +0000

    Syncing external repository on 2025_06_26

---
+++ fn_google_maps_directions/fn_google_maps_directions/util/config.py
@@ -0,0 +1,13 @@
+# (c) Copyright IBM Corp. 2018. All Rights Reserved.
+# -*- coding: utf-8 -*-
+
+"""Generate a default configuration-file section for fn_google_maps_directions"""
+
+from __future__ import print_function
+
+
+def config_section_data():
+  """Produce the default configuration section for app.config,
+      when called by `resilient-circuits config [-c|-u]`
+  """
+  return None
---
+++ fn_phish_tank/setup.py
@@ -0,0 +1,42 @@
+# (c) Copyright IBM Corp. 2010, 2019. All Rights Reserved.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='fn_phish_tank',
+    display_name='Phish Tank for SOAR',
+    version='1.0.3',
+    license='MIT',
+    author='IBM SOAR',
+    author_email='',
+    url='http://ibm.biz/soarcommunity',
+    description="PhishTank Lookup URL Function for IBM QRadar SOAR",
+    long_description="""Searches the PhishTank database (https://www.phishtank.com/) to determine if a URL is a phishing URL or not. 
+    The information returned from PhishTank is used to update the Artifacts description and add a note to the incident.
+<br>
+Links:
+<ul><a target='blank' href='https://ibm.com/mysupport'>Support</a></ul>
+<ul><a target='blank' href='https://ideas.ibm.com/'>Enhancement Requests</a></ul>""",
+    install_requires=[
+        'resilient_circuits>=30.0.0',
+        'python-dateutil>=2.8.0',
+        'requests>=2.21.0',
+        'resilient-lib>=32.0.140',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    classifiers=[
+        'Programming Language :: Python',
+    ],
+    entry_points={
+        "resilient.circuits.components": [
+            "FnPhishTankSubmitUrlFunctionComponent = fn_phish_tank.components.fn_phish_tank_submit_url:FunctionComponent"
+        ],
+        "resilient.circuits.configsection": ["gen_config = fn_phish_tank.util.config:config_section_data"],
+        "resilient.circuits.customize": ["customize = fn_phish_tank.util.customize:customization_data"],
+        "resilient.circuits.selftest": ["selftest = fn_phish_tank.util.selftest:selftest_function"]
+    }
+)
